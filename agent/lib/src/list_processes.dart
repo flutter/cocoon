8e71dc3b34c1f5e37f906636cf05c3526513133d
Michael Goderbauer <goderbauer@google.com>
2017-03-16 14:37:09 -0700

    add comment

--- agent/lib/src/list_processes.dart
+++ agent/lib/src/list_processes.dart
@@ -70,6 +70,7 @@ Future<List<int>> _listFlutterProcessesWindows(Directory flutterDirectory) async
   if (!canRun(_handleUtil))
     throw 'Please add Microsoft\'s Handle utility to your PATH: '
           'https://technet.microsoft.com/en-us/sysinternals/handle.aspx';
+  // `handle` return non-zero exit code when no process is found.
   String result = await eval(_handleUtil, [path.canonicalize(flutterDirectory.absolute.path)], canFail: true);
 
   return _pid.allMatches(result)
