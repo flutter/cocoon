d694be5c38330d36578fc5b16390220058cb9e48
Jenn Magder <magder@google.com>
2019-07-11 15:41:46 -0700

    Tests for GitHub status service

---
+++ app/test/github_status_service_test.dart
@@ -0,0 +1,50 @@
+// Copyright (c) 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:convert';
+
+import 'package:test/test.dart';
+import 'package:http/testing.dart';
+import 'package:http/http.dart' as http;
+
+import '../lib/repository/models/github_status.dart';
+import '../lib/repository/services/github_status_service.dart';
+
+void main() {
+  group('GitHub status fetch', () {
+    test('Successful fetch', () async {
+      final MockClient client = MockClient((http.Request request) async {
+        final Map<String, dynamic> mapJson = <String, dynamic>{
+          'status': <String, dynamic>{
+            'description': 'Failure',
+            'indicator': 'minor'}
+        };
+        return http.Response(json.encode(mapJson), 200);
+      });
+      final GithubStatus status = await fetchGithubStatus(client: client);
+
+      expect(status.status, 'Failure');
+      expect(status.indicator, 'minor');
+    });
+
+    test('Unexpected fetch', () async {
+      final MockClient client = MockClient((http.Request request) async {
+        final Map<String, dynamic> mapJson = <String, dynamic>{'bogus': 'Failure'};
+        return http.Response(json.encode(mapJson), 200);
+      });
+      final GithubStatus status = await fetchGithubStatus(client: client);
+
+      expect(status, isNull);
+    });
+
+    test('Failed fetch', () async {
+      final MockClient client = MockClient((http.Request request) async {
+        return http.Response(null, 404);
+      });
+      final GithubStatus status = await fetchGithubStatus(client: client);
+
+      expect(status, isNull);
+    });
+  });
+}
