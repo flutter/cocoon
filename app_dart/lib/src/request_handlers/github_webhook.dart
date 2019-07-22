f097c15a797e4b1d7f37382024c5ab70704e9987
Ian Hickson <ian@hixie.ch>
2019-07-22 11:37:05 -0700

    Update github_webhook.dart

--- app_dart/lib/src/request_handlers/github_webhook.dart
+++ app_dart/lib/src/request_handlers/github_webhook.dart
@@ -140,6 +140,7 @@ Future<void> _applyLabels(Config config, GitHub gitHubClient, PullRequestEvent e
     );
   }
   if (!hasTests && needsTests) {
+    // https://pantheon.corp.google.com/datastore/entities;kind=CocoonConfig;ns=__$DEFAULT$__/edit;key=0%2F%257C12%2FCocoonConfig%257C35%2Fname%253AMissingTestsPullRequestMessage?project=flutter-dashboard
     final String body = await config.missingTestsPullRequestMessage;
     await gitHubClient.issues.createComment(slug, event.number, body);
   }
