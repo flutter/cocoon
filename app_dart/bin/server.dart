c4d4a15390d13668bf1b2a83e4768cabaa7c37bb
Todd Volkert <tvolkert@google.com>
2019-08-03 15:28:52 -0700

    Add placeholder map entries for handlers still to migrate.

--- app_dart/bin/server.dart
+++ app_dart/bin/server.dart
@@ -15,20 +15,33 @@ Future<void> main() async {
   await withAppEngineServices(() async {
     final Config config = Config(dbService);
     final AuthenticationProvider authProvider = AuthenticationProvider(config, () => gae.context);
+    final BuildBucketClient buildBucketClient = BuildBucketClient(
+      accessTokenProvider: AccessTokenProvider(config),
+    );
 
     final Map<String, RequestHandler<dynamic>> handlers = <String, RequestHandler<dynamic>>{
       '/api/append-log': AppendLog(config, authProvider),
-      '/api/github-webhook-pullrequest': GithubWebhook(
-          config,
-          BuildBucketClient(
-            accessTokenProvider: AccessTokenProvider(config),
-          )),
+      '/api/authorize-agent': null,
+      '/api/check-build-status': null,
+      '/api/create-agent': null,
+      '/api/get-authentication-status': null,
+      '/api/get-log': null,
+      '/api/github-webhook-pullrequest': GithubWebhook(config, buildBucketClient),
+      '/api/public/build-status': null,
+      '/api/public/get-benchmarks': null,
+      '/api/public/get-status': null,
+      '/api/public/get-timeseries-history': null,
       '/api/push-build-status-to-github': PushBuildStatusToGithub(config, authProvider),
+      '/api/push-engine-build-status-to-github': null,
       '/api/refresh-chromebot-status': RefreshChromebotStatus(config, authProvider),
       '/api/refresh-github-commits': RefreshGithubCommits(config, authProvider),
       '/api/refresh-cirrus-status': RefreshCirrusStatus(config, authProvider),
       '/api/reserve-task': ReserveTask(config, authProvider),
+      '/api/reset-devicelab-task': null,
+      '/api/update-agent-health': null,
+      '/api/update-benchmark-targets': null,
       '/api/update-task-status': UpdateTaskStatus(config, authProvider),
+      '/api/update-timeseries': null,
       '/api/vacuum-clean': VacuumClean(config, authProvider),
       '/api/debug/get-task-by-id': DebugGetTaskById(config, authProvider),
       '/api/debug/reset-pending-tasks': DebugResetPendingTasks(config, authProvider),
