afe886d077a8aea24b850c0270dd275e2a42eb56
Dan Field <dnfield@google.com>
2019-09-07 14:18:05 -0700

    Make test actually test (#405)
    * start
    
    * ..
    
    * :wq
    
    * cwd
    
    * analysis
    
    * ..
    
    * ..
    
    * ..
    
    * app
    
    * agent
    
    * ..
    
    * ..
    
    * ..
    
    * fixes
    
    * fixes
    
    * ..
    
    * exclude web tests
    
    * fix test script and update deps for agent

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-if [[ $1 -eq 'app' ]]; then
+if [[ "$1" == "app" ]]; then
   echo "Testing the web is not yet supported"
   exit 0
 fi
