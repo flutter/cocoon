4a45a6558522788efd584eb4a8663935e24d9374
Casey Hillers <chillers@google.com>
2019-09-11 16:01:07 -0700

    copyright header

--- app_flutter/test/widget_test.dart
+++ app_flutter/test/widget_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
 // This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
