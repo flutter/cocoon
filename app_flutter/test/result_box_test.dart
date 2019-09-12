9f6703c09ba84f089dd091462a6bcd27c58f1b82
Casey Hillers <chillers@google.com>
2019-09-12 16:42:40 -0700

    added tests for all the colors!

--- app_flutter/test/result_box_test.dart
+++ app_flutter/test/result_box_test.dart
@@ -8,21 +8,49 @@ import 'package:flutter/material.dart';
 import 'package:app_flutter/result_box.dart';
 
 void main() {
+  testWidgets('ResultBox is the color red when given the message Failed',
+      (WidgetTester tester) async {
+    expectResultBoxColorWithMessage(tester, 'Failed', Colors.red);
+  });
+
+  testWidgets(
+      'ResultBox is the color purple when given the message In Progress',
+      (WidgetTester tester) async {
+    expectResultBoxColorWithMessage(tester, 'In Progress', Colors.purple);
+  });
+
+  testWidgets('ResultBox is the color blue when given the message New',
+      (WidgetTester tester) async {
+    expectResultBoxColorWithMessage(tester, 'New', Colors.blue);
+  });
+
+  testWidgets('ResultBox is the color white when given the message Skipped',
+      (WidgetTester tester) async {
+    expectResultBoxColorWithMessage(tester, 'Skipped', Colors.white);
+  });
+
   testWidgets('ResultBox is the color green when given the message Succeeded',
       (WidgetTester tester) async {
-    await tester.pumpWidget(ResultBox(message: 'Succeeded'));
+    expectResultBoxColorWithMessage(tester, 'Succeeded', Colors.green);
+  });
 
-    Container resultBoxWidget = find.byType(Container).evaluate().first.widget;
-    BoxDecoration decoration = resultBoxWidget.decoration;
-    expect(decoration.color, Colors.green);
+  testWidgets(
+      'ResultBox is the color orange when given the message Underperformed',
+      (WidgetTester tester) async {
+    expectResultBoxColorWithMessage(tester, 'Underperformed', Colors.orange);
   });
 
   testWidgets('ResultBox is the color black when given an unknown message',
       (WidgetTester tester) async {
-    await tester.pumpWidget(ResultBox(message: '404'));
-
-    Container resultBoxWidget = find.byType(Container).evaluate().first.widget;
-    BoxDecoration decoration = resultBoxWidget.decoration;
-    expect(decoration.color, Colors.black);
+    expectResultBoxColorWithMessage(tester, '404', Colors.black);
   });
+}
+
+void expectResultBoxColorWithMessage(
+    WidgetTester tester, String message, Color expectedColor) async {
+  await tester.pumpWidget(ResultBox(message: message));
+
+  Container resultBoxWidget = find.byType(Container).evaluate().first.widget;
+  BoxDecoration decoration = resultBoxWidget.decoration;
+  expect(decoration.color, expectedColor);
 }
