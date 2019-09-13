6b2217da34425876d9dae00c034e4190d83b16d7
Casey Hillers <chillers@google.com>
2019-09-13 13:31:36 -0700

    slivergrid

--- app_flutter/lib/status_grid.dart
+++ app_flutter/lib/status_grid.dart
@@ -9,15 +9,26 @@ import 'result_box.dart';
 class StatusGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: 100,
-      gridDelegate:
-          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 20),
-      itemBuilder: (BuildContext context, int index) {
-        String randomMessage = ResultBox.resultColor.keys
-            .toList()[index % ResultBox.resultColor.keys.length];
-        return ResultBox(message: randomMessage);
-      },
+    /// SliverGrid only renders the cells that are visible to the screen. Additionally, it offers support for scrolling.
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverGrid(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 50.0,
+            mainAxisSpacing: 1.0,
+            crossAxisSpacing: 1.0,
+          ),
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              // TODO(chillers): use a data source
+              String randomMessage =
+                  ResultBox.resultColor.keys.toList()[4 * (index & 1)];
+              return ResultBox(message: randomMessage);
+            },
+            childCount: 2000,
+          ),
+        ),
+      ],
     );
   }
 }
