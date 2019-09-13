7f26cebe876a25cbe3245839e5a8a2be57c6a3b6
Casey Hillers <chillers@google.com>
2019-09-13 15:24:27 -0700

    another way

--- app_flutter/lib/status_grid.dart
+++ app_flutter/lib/status_grid.dart
@@ -7,27 +7,43 @@ import 'package:flutter/material.dart';
 import 'result_box.dart';
 
 class StatusGrid extends StatelessWidget {
+  static const int taskCount = 80; // rough estimate based on existing dashboard
+  static const int commitCount = 200; // based on existing dashboard
+
   @override
   Widget build(BuildContext context) {
-    /// SliverGrid only renders the cells that are visible to the screen. Additionally, it offers support for scrolling.
+    /// There are too many columns to fit in one screen width
     return CustomScrollView(
       slivers: <Widget>[
-        SliverGrid(
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 50.0,
-            mainAxisSpacing: 1.0,
-            crossAxisSpacing: 1.0,
-          ),
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-              // TODO(chillers): use a data source
-              String randomMessage =
-                  ResultBox.resultColor.keys.toList()[4 * (index & 1)];
-              return ResultBox(message: randomMessage);
-            },
-            childCount: 2000,
-          ),
+        /// SliverGrid only renders the cells that are visible to the screen.
+        SliverGrid.extent(
+          children: List.generate(taskCount * commitCount, (index) {
+            String randomMessage =
+                ResultBox.resultColor.keys.toList()[4 * (index & 1)];
+            return ResultBox(message: randomMessage);
+          }),
+          maxCrossAxisExtent: 40,
         ),
+        // SliverGrid(
+        //   // this technique was good for defining a size they should match
+        //   // gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        //   //   maxCrossAxisExtent: 50.0,
+        //   //   mainAxisSpacing: 1.0,
+        //   //   crossAxisSpacing: 1.0,
+        //   // ),
+        //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        //       mainAxisSpacing: 4, crossAxisCount: taskCount),
+        //   delegate: SliverChildBuilderDelegate(
+        //     (BuildContext context, int index) {
+        //       // TODO(chillers): use a data source
+        //       String randomMessage =
+        //           ResultBox.resultColor.keys.toList()[4 * (index & 1)];
+        //       return ResultBox(message: randomMessage);
+        //     },
+        //     // TODO(chillers): use a data source
+        //     childCount: commitCount * taskCount,
+        //   ),
+        // ),
       ],
     );
   }
