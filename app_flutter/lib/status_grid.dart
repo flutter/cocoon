347c9c3ca75295605d578742fb35cab9990970cd
Casey Hillers <chillers@google.com>
2019-09-16 11:40:53 -0700

    more playing around

--- app_flutter/lib/status_grid.dart
+++ app_flutter/lib/status_grid.dart
@@ -7,54 +7,67 @@ import 'package:flutter/material.dart';
 import 'result_box.dart';
 
 class StatusGrid extends StatelessWidget {
-  static const int taskCount = 80; // rough estimate based on existing dashboard
+  static const int taskCount = 20; // rough estimate based on existing dashboard
   static const int commitCount = 200; // based on existing dashboard
 
   @override
   Widget build(BuildContext context) {
     /// There are too many columns to fit in one screen width
-    return Container(
-      height: 20,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: List.generate(taskCount, (index) {
-          return ResultBox(
-            message: 'Succeeded',
-          );
-        }),
-      ),
-      // return CustomScrollView(
-      //   slivers: <Widget>[
-      /// SliverGrid only renders the cells that are visible to the screen.
-      // SliverGrid.extent(
-      //   children: List.generate(taskCount * commitCount, (index) {
-      //     String randomMessage =
-      //         ResultBox.resultColor.keys.toList()[4 * (index & 1)];
-      //     return ResultBox(message: randomMessage);
-      //   }),
-      //   maxCrossAxisExtent: 40,
-      // ),
-      // SliverGrid(
-      //   // this technique was good for defining a size they should match
-      //   // gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-      //   //   maxCrossAxisExtent: 50.0,
-      //   //   mainAxisSpacing: 1.0,
-      //   //   crossAxisSpacing: 1.0,
-      //   // ),
-      //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-      //       mainAxisSpacing: 4, crossAxisCount: taskCount),
-      //   delegate: SliverChildBuilderDelegate(
-      //     (BuildContext context, int index) {
-      //       // TODO(chillers): use a data source
-      //       String randomMessage =
-      //           ResultBox.resultColor.keys.toList()[4 * (index & 1)];
-      //       return ResultBox(message: randomMessage);
-      //     },
-      //     // TODO(chillers): use a data source
-      //     childCount: commitCount * taskCount,
-      //   ),
-      // ),
-      // ],
+    // return ListView(
+    //   children: List.generate(commitCount, (index) {
+    //     return Container(
+    //       height: 20,
+    //       child: ListView(
+    //         scrollDirection: Axis.horizontal,
+    //         children: List.generate(taskCount, (index) {
+    //           return ResultBox(
+    //             message: 'Succeeded',
+    //           );
+    //         }),
+    //       ),
+    //     );
+    //   }),
+    // );
+    return CustomScrollView(
+      slivers: <Widget>[
+        /// SliverGrid only renders the cells that are visible to the screen.
+        // SliverGrid.extent(
+        //   children: List.generate(taskCount * commitCount, (index) {
+        //     String randomMessage =
+        //         ResultBox.resultColor.keys.toList()[4 * (index & 1)];
+        //     return ResultBox(message: randomMessage);
+        //   }),
+        //   maxCrossAxisExtent: 40,
+        // ),
+        // SliverGrid(
+        //   // this technique was good for defining a size they should match
+        //   gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        //     maxCrossAxisExtent: 50.0,
+        //     mainAxisSpacing: 1.0,
+        //     crossAxisSpacing: 1.0,
+        //   ),
+        //   // gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        //   //     mainAxisSpacing: 4, crossAxisCount: taskCount),
+        //   delegate: SliverChildBuilderDelegate(
+        //     (BuildContext context, int index) {
+        //       // TODO(chillers): use a data source
+        //       String randomMessage =
+        //           ResultBox.resultColor.keys.toList()[4 * (index & 1)];
+        //       return ResultBox(message: randomMessage);
+        //     },
+        //     // TODO(chillers): use a data source
+        //     childCount: commitCount * taskCount,
+        //   ),
+        // ),
+        SliverGrid.count(
+          crossAxisCount: taskCount,
+          children: List.generate(taskCount * commitCount, (index) {
+            String randomMessage =
+                ResultBox.resultColor.keys.toList()[4 * (index & 1)];
+            return ResultBox(message: randomMessage);
+          }),
+        ),
+      ],
     );
   }
 }
