a1f8db3a6c498c2e5e06e74f087870c971d85b2f
Casey Hillers <chillers@google.com>
2019-09-16 17:21:35 -0700

    scroll working on both axis

--- app_flutter/lib/status_grid.dart
+++ app_flutter/lib/status_grid.dart
@@ -7,69 +7,26 @@ import 'package:flutter/material.dart';
 import 'result_box.dart';
 
 class StatusGrid extends StatelessWidget {
-  static const int taskCount = 10; // rough estimate based on existing dashboard
-  static const int commitCount = 100; // based on existing dashboard
+  static const int taskCount = 80; // rough estimate based on existing dashboard
+  static const int commitCount = 200; // based on existing dashboard
 
   @override
   Widget build(BuildContext context) {
-    // SingleChildScrollView (basic)
+    // The grid is wrapped with SingleChildScrollView to enable scrolling both
+    // horizontally and vertically
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
-      child: new Row(
-        children: List.generate(200, (index) {
-          return Text('hi ');
-        }),
+      child: Container(
+        width: taskCount * 50.0,
+        height: 500.0,
+        child: GridView.count(
+          shrinkWrap: true,
+          crossAxisCount: taskCount,
+          children: List.generate(taskCount * commitCount, (index) {
+            return ResultBox(message: 'Succeeded');
+          }),
+        ),
       ),
     );
-
-    // SingleChildScrollView + GridView
-    // Assertion failed: org-dartlang-app:///packages/flutter/src/rendering/viewport.dart:1632:16
-    // constraints.hasBoundedWidth
-    // is not true
-    // return SingleChildScrollView(
-    //   scrollDirection: Axis.horizontal,
-    //   child: GridView.count(
-    //     shrinkWrap: true,
-    //     crossAxisCount: taskCount,
-    //     children: List.generate(taskCount * commitCount, (index) {
-    //       return Text('#$index ');
-    //     }),
-    //   ),
-    // );
-
-    // CUSTOM SCROLL VIEW + SLIVER GRID
-    // return CustomScrollView(
-    //   slivers: <Widget>[
-    //     /// SliverGrid only renders the cells that are visible to the screen.
-    //     // SliverGrid(
-    //     //   // this technique was good for defining a size they should match
-    //     //   gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-    //     //     maxCrossAxisExtent: 50.0,
-    //     //     mainAxisSpacing: 1.0,
-    //     //     crossAxisSpacing: 1.0,
-    //     //   ),
-    //     //   // gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-    //     //   //     mainAxisSpacing: 4, crossAxisCount: taskCount),
-    //     //   delegate: SliverChildBuilderDelegate(
-    //     //     (BuildContext context, int index) {
-    //     //       // TODO(chillers): use a data source
-    //     //       String randomMessage =
-    //     //           ResultBox.resultColor.keys.toList()[4 * (index & 1)];
-    //     //       return ResultBox(message: randomMessage);
-    //     //     },
-    //     //     // TODO(chillers): use a data source
-    //     //     childCount: commitCount * taskCount,
-    //     //   ),
-    //     // ),
-    //     SliverGrid.count(
-    //       crossAxisCount: taskCount,
-    //       children: List.generate(taskCount * commitCount, (index) {
-    //         String randomMessage =
-    //             ResultBox.resultColor.keys.toList()[4 * (index & 1)];
-    //         return ResultBox(message: randomMessage);
-    //       }),
-    //     ),
-    //   ],
-    // );
   }
 }
