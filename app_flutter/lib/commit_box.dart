37ac9fc466d8652e49a8bc3c755271612c83b77b
Casey Hillers <caseyhillers@gmail.com>
2019-09-17 16:18:46 -0700

    Commit Widget (#427)
    * commit box
    
    * switched to stateless
    
    * Update app_flutter/lib/commit_box.dart
    
    Co-Authored-By: Jonah Williams <jonahwilliams@google.com>
    
    * documented commit box fields

---
+++ app_flutter/lib/commit_box.dart
@@ -0,0 +1,37 @@
+// Copyright (c) 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+/// Displays Git commit information.
+class CommitBox extends StatelessWidget {
+  // TODO(chillers): convert to use commit model
+  const CommitBox({
+    Key key,
+    @required this.message,
+    @required this.avatarUrl,
+  }) : super(key: key);
+
+  /// Commit message that summarizes the change made.
+  final String message;
+
+  /// Image URL to the avatar of the author of this commit.
+  final String avatarUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Image.network(
+          avatarUrl,
+          height: 40,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(message),
+        ),
+      ],
+    );
+  }
+}
---
+++ app_flutter/test/commit_box_test.dart
@@ -0,0 +1,32 @@
+// Copyright (c) 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:app_flutter/commit_box.dart';
+
+void main() {
+  group('CommitBox', () {
+    testWidgets('shows information correctly', (WidgetTester tester) async {
+      const String message = 'message message';
+      const String avatarUrl =
+          'https://avatars2.githubusercontent.com/u/2148558?v=4';
+
+      await tester.pumpWidget(Directionality(
+        child: CommitBox(
+          message: message,
+          avatarUrl: avatarUrl,
+        ),
+        textDirection: TextDirection.ltr,
+      ));
+
+      expect(find.text(message), findsOneWidget);
+      expect(find.byType(Image), findsOneWidget);
+
+      // Image.Network throws a 400 exception in tests
+      tester.takeException();
+    });
+  });
+}
