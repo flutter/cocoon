19c13a18d0295d08cef7955167b479fdfbb07ced
Casey Hillers <chillers@google.com>
2019-09-26 11:36:51 -0700

    switched map to brute force

--- app_flutter/lib/status_grid.dart
+++ app_flutter/lib/status_grid.dart
@@ -36,14 +36,6 @@ class StatusGrid extends StatelessWidget {
     // how many tasks they each run.
     int columnCount = _getColumnCount(statuses.first);
 
-    /// This is a list of total number of tasks at the end of each stage.
-    ///
-    /// This allows us to lookup a task based on the number of stages instead of
-    /// bruteforcing and looping through all stages and their tasks.
-    ///
-    /// We calculate this once as it is used when retrieving each [Task].
-    List<int> taskIndexBoundaries = _getTaskIndexBoundaries(statuses.first);
-
     // The grid is wrapped with SingleChildScrollView to enable scrolling both
     // horizontally and vertically
     return Expanded(
@@ -62,9 +54,16 @@ class StatusGrid extends StatelessWidget {
                 return CommitBox(commit: statuses[statusIndex].commit);
               }
 
-              return TaskBox(
-                  task: _mapGridIndexToTask(
-                      gridIndex, columnCount, taskIndexBoundaries));
+              return GestureDetector(
+                  onTap: () {
+                    Task task =
+                        _mapGridIndexToTaskBruteForce(gridIndex, columnCount);
+                    print(
+                        'statusI: $statusIndex\t\ttaskI: ${task.createTimestamp}\t\tgridI: $gridIndex');
+                  },
+                  child: TaskBox(
+                    task: _mapGridIndexToTaskBruteForce(gridIndex, columnCount),
+                  ));
             },
           ),
         ),
@@ -89,53 +88,24 @@ class StatusGrid extends StatelessWidget {
     return columnCount;
   }
 
-  /// Returns a list of the total number of tasks at the end of each stage.
-  ///
-  /// This is useful for quickly looking up what [Stage] a [Task] is in given
-  /// just an index.
-  ///
-  /// Additionally, storing the total number of tasks at the end of a [Stage]
-  /// allows for quickly getting the relative index for a task in that [Stage].
-  ///
-  /// The last boundary is 1 above the total number of tasks, thus this
-  /// list is inclusive of all tasks (assuming the first [CommitStatus] is
-  /// representative of the data).
-  List<int> _getTaskIndexBoundaries(CommitStatus status) {
-    List<int> indices = <int>[];
-
-    for (int i = 0; i < status.stages.length; i++) {
-      Stage stage = status.stages[i];
-      if (i == 0) {
-        indices.add(stage.tasks.length);
-      } else {
-        indices.add(indices[i - 1] + stage.tasks.length);
-      }
-    }
-
-    return indices;
-  }
-
-  /// Returns [Task] associated with an overall index in [List<CommitStatus>]
-  Task _mapGridIndexToTask(
-      int gridIndex, int columnCount, List<int> taskIndexBoundaries) {
+  /// Maps a [gridIndex] to a specific [Task] in [List<CommitStatus>]
+  Task _mapGridIndexToTaskBruteForce(int gridIndex, int columnCount) {
     int commitStatusIndex = gridIndex ~/ columnCount;
     CommitStatus status = statuses[commitStatusIndex];
 
-    // set index to be relative to this status (and remove the commit index)
-    int indexOffset = columnCount * commitStatusIndex - 1;
-    int index = gridIndex - indexOffset;
+    int taskIndex = (gridIndex % columnCount) - 1;
 
-    int stageIndex = 0;
-    int taskIndex = 0;
-    for (int taskIndexBoundary in taskIndexBoundaries) {
-      if (index < taskIndexBoundary) {
-        break;
+    int currentStageIndex = 0;
+    while (taskIndex >= 0 && currentStageIndex < status.stages.length) {
+      Stage currentStage = status.stages[currentStageIndex];
+      if (taskIndex >= currentStage.tasks.length) {
+        taskIndex = taskIndex - currentStage.tasks.length;
+        currentStageIndex++;
+      } else {
+        return currentStage.tasks[taskIndex];
       }
-
-      taskIndex = index - taskIndexBoundary;
-      stageIndex++;
     }
 
-    return status.stages[stageIndex].tasks[taskIndex];
+    throw Exception('Could not find Task for gridIndex=$gridIndex');
   }
 }
