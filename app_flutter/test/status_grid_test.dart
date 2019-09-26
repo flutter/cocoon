19f3d0cc49579aa3506e08198933b3ad114254c0
Casey Hillers <chillers@google.com>
2019-09-26 12:28:42 -0700

    first task last task test

--- app_flutter/test/status_grid_test.dart
+++ app_flutter/test/status_grid_test.dart
@@ -2,12 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:app_flutter/task_box.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:cocoon_service/protos.dart' show CommitStatus;
 
 import 'package:app_flutter/service/fake_cocoon.dart';
+import 'package:app_flutter/commit_box.dart';
 import 'package:app_flutter/status_grid.dart';
 
 void main() {
@@ -35,43 +37,68 @@ void main() {
       tester.takeException();
     });
 
-    testWidgets('has correct width', (WidgetTester tester) async {
-      await tester.pumpWidget(Column(
-        children: [
-          StatusGrid(
-            statuses: statuses,
+    testWidgets('commits show in the same column', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Column(
+            children: [
+              StatusGrid(
+                statuses: statuses,
+              ),
+            ],
           ),
-        ],
-      ));
+        ),
+      );
 
-      // expect(find.byType(GridView).evaluate()., 200);
+      List<Element> commits = find.byType(CommitBox).evaluate().toList();
+
+      double xPosition = commits.first.size.topLeft(Offset.zero).dx;
+
+      for (Element commit in commits) {
+        // All the x positions should match the first instance if they're all in the same column
+        expect(commit.size.topLeft(Offset.zero).dx, xPosition);
+        tester.takeException();
+      }
     });
 
-    testWidgets('has correct height', (WidgetTester tester) async {
-            await tester.pumpWidget(Column(
-        children: [
-          StatusGrid(
-            statuses: <CommitStatus>[],
-          ),
-        ],
-      ));
-
-      GridView grid = find.byType(GridView).evaluate().first.widget;
-
-      // height should be 50.0 * statuses.length
-    });
-
-    testWidgets('commits only show in left most column',
+    testWidgets('first task in grid is the first task given',
         (WidgetTester tester) async {
-      await tester.pumpWidget(Column(
-        children: [
-          StatusGrid(
-            statuses: <CommitStatus>[],
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Column(
+            children: [
+              StatusGrid(
+                statuses: statuses,
+              ),
+            ],
           ),
-        ],
-      ));
+        ),
+      );
 
-      GridView grid = find.byType(GridView).evaluate().first.widget;
+      TaskBox firstTask = find.byType(TaskBox).evaluate().first.widget;
+      expect(firstTask.task, statuses[0].stages[0].tasks[0]);
+
+      tester.takeException();
+    });
+
+    testWidgets('last task in grid is the last task given',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Column(
+            children: [
+              StatusGrid(
+                statuses: statuses,
+              ),
+            ],
+          ),
+        ),
+      );
+
+      TaskBox lastTask = find.byType(TaskBox).evaluate().last.widget;
+      expect(lastTask.task, statuses.last.stages.last.tasks.last);
+
+      tester.takeException();
     });
   });
 }
