8a2ec4944218d066dfa311dba9f744e635ad96b5
Casey Hillers <caseyhillers@gmail.com>
2019-09-30 18:09:06 -0700

    theme as a final var

--- app_flutter/lib/main.dart
+++ app_flutter/lib/main.dart
@@ -40,12 +40,12 @@ class _BuildDashboardPageState extends State<BuildDashboardPage> {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Build Dashboard v2'),
-        backgroundColor: buildState.isTreeBuilding
-            ? Theme.of(context).primaryColor
-            : Theme.of(context).errorColor,
+        backgroundColor:
+            buildState.isTreeBuilding ? theme.primaryColor : theme.errorColor,
       ),
       body: Column(
         children: [
