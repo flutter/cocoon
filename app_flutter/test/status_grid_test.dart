96cd4d16c02d3f9d06a0f1aaa2bdfe2f681f5716
Casey Hillers <chillers@google.com>
2019-10-07 12:13:09 -0700

    removed old dep

--- app_flutter/test/status_grid_test.dart
+++ app_flutter/test/status_grid_test.dart
@@ -4,7 +4,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
 
 import 'package:cocoon_service/protos.dart' show CommitStatus, Task;
