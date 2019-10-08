edd0cb00f5a246e4d198acbe25c5a0877409b2c9
Casey Hillers <caseyhillers@gmail.com>
2019-10-08 16:32:44 -0700

    Task Overlay UI (#467)
    * task status icon in overlay + tooltip
    
    * made UI show more info
    
    * task overlay indicates task being viewed

--- app_flutter/lib/task_box.dart
+++ app_flutter/lib/task_box.dart
@@ -11,7 +11,7 @@ import 'package:cocoon_service/protos.dart' show Task;
 /// If [Task.status] is "In Progress", it will show as a "New" task
 /// with a [CircularProgressIndicator] in the box.
 /// Shows a black box for unknown statuses.
-class TaskBox extends StatelessWidget {
+class TaskBox extends StatefulWidget {
   const TaskBox({Key key, @required this.task}) : super(key: key);
 
   /// [Task] to show information from.
@@ -44,35 +44,188 @@ class TaskBox extends StatelessWidget {
   };
 
   @override
+  _TaskBoxState createState() => _TaskBoxState();
+}
+
+class _TaskBoxState extends State<TaskBox> {
+  OverlayEntry _taskOverlay;
+
+  /// [Task.status] modified to take into account [Task.attempts] to create
+  /// a more descriptive status.
+  ///
+  /// For example, [Task.status] = "In Progress" and [Task.attempts] > 1 results
+  /// in the status of [statusUnderperformedInProgress].
+  String status;
+
+  @override
   Widget build(BuildContext context) {
-    final bool attempted = task.attempts > 1;
-    String status = task.status;
+    final bool attempted = widget.task.attempts > 1;
+
+    status = widget.task.status;
     if (attempted) {
-      if (status == statusSucceeded) {
-        status = statusSucceededButFlaky;
-      } else if (status == statusNew) {
-        status = statusUnderperformed;
-      } else if (status == statusInProgress) {
-        status = statusUnderperformedInProgress;
+      if (status == TaskBox.statusSucceeded) {
+        status = TaskBox.statusSucceededButFlaky;
+      } else if (status == TaskBox.statusNew) {
+        status = TaskBox.statusUnderperformed;
+      } else if (status == TaskBox.statusInProgress) {
+        status = TaskBox.statusUnderperformedInProgress;
       }
     }
 
-    return Container(
-      margin: const EdgeInsets.all(1.0),
-      color:
-          statusColor.containsKey(status) ? statusColor[status] : Colors.black,
-      child: (status == statusInProgress ||
-              status == statusUnderperformedInProgress)
-          ? const Padding(
-              padding: EdgeInsets.all(15.0),
-              child: CircularProgressIndicator(
-                strokeWidth: 3.0,
-                backgroundColor: Colors.white70,
-              ),
-            )
-          : null,
-      width: 20,
-      height: 20,
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        margin: const EdgeInsets.all(1.0),
+        color: TaskBox.statusColor.containsKey(status)
+            ? TaskBox.statusColor[status]
+            : Colors.black,
+        child: (status == TaskBox.statusInProgress ||
+                status == TaskBox.statusUnderperformedInProgress)
+            ? const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: CircularProgressIndicator(
+                  strokeWidth: 3.0,
+                  backgroundColor: Colors.white70,
+                ),
+              )
+            : null,
+        width: 20,
+        height: 20,
+      ),
+    );
+  }
+
+  void _handleTap() {
+    _taskOverlay = OverlayEntry(
+      builder: (_) => TaskOverlayContents(
+          parentContext: context,
+          task: widget.task,
+          taskStatus: status,
+          closeCallback: _closeOverlay),
+    );
+
+    Overlay.of(context).insert(_taskOverlay);
+  }
+
+  void _closeOverlay() => _taskOverlay.remove();
+}
+
+/// Displays the information from [Task] and allows interacting with a [Task].
+///
+/// This is intended to be inserted in an [OverlayEntry] as it requires
+/// [closeCallback] that will remove the widget from the tree.
+///
+/// Offers the functionality of opening the log for this [Task] and rerunning
+/// this [Task] through the build system.
+class TaskOverlayContents extends StatelessWidget {
+  const TaskOverlayContents({
+    Key key,
+    @required this.parentContext,
+    @required this.task,
+    @required this.taskStatus,
+    @required this.closeCallback,
+  })  : assert(parentContext != null),
+        assert(task != null),
+        assert(closeCallback != null),
+        super(key: key);
+
+  /// The parent context that has the size of the whole screen
+  final BuildContext parentContext;
+
+  /// The [Task] to display in the overlay
+  final Task task;
+
+  /// [Task.status] modified to take into account [Task.attempts] to create
+  /// a more descriptive status.
+  final String taskStatus;
+
+  /// This callback removes the parent overlay from the widget tree.
+  ///
+  /// On a click that is outside the area of the overlay (the rest of the screen),
+  /// this callback is called closing the overlay.
+  final void Function() closeCallback;
+
+  /// A lookup table to define the [Icon] for this [Overlay].
+  static const Map<String, Icon> statusIcon = <String, Icon>{
+    TaskBox.statusFailed: Icon(Icons.clear, color: Colors.red, size: 32),
+    TaskBox.statusNew: Icon(Icons.new_releases, color: Colors.blue, size: 32),
+    TaskBox.statusInProgress:
+        Icon(Icons.autorenew, color: Colors.blue, size: 32),
+    TaskBox.statusSucceeded:
+        Icon(Icons.check_circle, color: Colors.green, size: 32),
+    TaskBox.statusSucceededButFlaky: Icon(Icons.check_circle_outline, size: 32),
+    TaskBox.statusUnderperformed:
+        Icon(Icons.new_releases, color: Colors.orange, size: 32),
+    TaskBox.statusUnderperformedInProgress:
+        Icon(Icons.autorenew, color: Colors.orange, size: 32),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    final RenderBox renderBox = parentContext.findRenderObject();
+    final Offset offsetLeft = renderBox.localToGlobal(Offset.zero);
+
+    return Stack(
+      children: <Widget>[
+        /// This is a focus container to emphasize the [TaskBox] that this
+        /// [Overlay] is currently showing information from.
+        Positioned(
+          top: offsetLeft.dy,
+          left: offsetLeft.dx,
+          width: renderBox.size.width,
+          height: renderBox.size.height,
+          child: Container(
+            color: Colors.white70,
+            key: const Key('task-overlay-key'),
+          ),
+        ),
+        // This is the area a user can click (the rest of the screen) to close the overlay.
+        GestureDetector(
+          onTap: closeCallback,
+          child: Container(
+            width: MediaQuery.of(parentContext).size.width,
+            height: MediaQuery.of(parentContext).size.height,
+            // Color must be defined otherwise the container can't be clicked on
+            color: Colors.transparent,
+          ),
+        ),
+        Positioned(
+          width: 350,
+          // Move this overlay to be where the parent is
+          top: offsetLeft.dy + (renderBox.size.height / 2),
+          left: offsetLeft.dx + (renderBox.size.width / 2),
+          child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                ListTile(
+                  leading: Tooltip(
+                      message: taskStatus, child: statusIcon[taskStatus]),
+                  title: Text(task.name),
+                  subtitle: Text(
+                      'Attempts: ${task.attempts}\nDuration: ${task.endTimestamp - task.startTimestamp} seconds\nAgent: ${task.reservedForAgentId}'),
+                ),
+                ButtonBar(
+                  children: <Widget>[
+                    IconButton(
+                      icon: const Icon(Icons.receipt),
+                      onPressed: () {
+                        // TODO(chillers): Open log in new window. https://github.com/flutter/cocoon/issues/436
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.redo),
+                      onPressed: () {
+                        // TODO(chillers): Rerun task. https://github.com/flutter/cocoon/issues/424
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
