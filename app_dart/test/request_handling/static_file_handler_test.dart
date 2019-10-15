d3cf435a967e35a867dc168cfc78154ffd223789
Casey Hillers <caseyhillers@gmail.com>
2019-10-15 14:58:26 -0700

    added test to check for file respones returning normally

--- app_dart/test/request_handling/static_file_handler_test.dart
+++ app_dart/test/request_handling/static_file_handler_test.dart
@@ -2,9 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:convert' show utf8;
 import 'dart:io';
 
 import 'package:cocoon_service/cocoon_service.dart';
+import 'package:file/file.dart';
+import 'package:file/memory.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
@@ -17,19 +20,40 @@ import '../src/request_handling/request_handler_tester.dart';
 void main() {
   group('StaticFileHandler', () {
     RequestHandlerTester tester;
-    StaticFileHandler<Body> staticFileHandler;
+    FileSystem fs;
 
     final FakeConfig config = FakeConfig();
 
+    const String indexFileName = 'index.html';
+    const String indexFileContent = 'some html';
+
     setUp(() {
       tester = RequestHandlerTester();
+      fs = MemoryFileSystem();
+      fs.file('build/web/$indexFileName').createSync(recursive: true);
+      fs.file('build/web/$indexFileName').writeAsString(indexFileContent);
     });
 
+    Future<String> _decodeHandlerBody(Body body) {
+      return utf8.decoder.bind(body.serialize()).first;
+    }
+
     test('returns 404 response when file does not exist', () async {
-      staticFileHandler = StaticFileHandler<Body>('null', config: config);
+      final StaticFileHandler<Body> staticFileHandler =
+          StaticFileHandler<Body>('i do not exist as a file', config: config, fs: fs);
 
       expect(tester.get(staticFileHandler),
           throwsA(const TypeMatcher<NotFoundException>()));
+    });
+
+    test('returns body when file does exist', () async {
+      final StaticFileHandler<Body> staticFileHandler =
+          StaticFileHandler<Body>('/$indexFileName', config: config, fs: fs);
+
+      final Body body = await tester.get(staticFileHandler);
+      expect(body, isNotNull);
+      final String response = await _decodeHandlerBody(body);
+      expect(response, indexFileContent);
     });
   });
 }
