8094f53cc5ab3df0509f8baa93e15ac89aba8c1c
Tong Wu <wutong@google.com>
2019-10-17 12:33:33 -0700

    Resolve commits.

--- agent/bin/ci.sh
+++ agent/bin/ci.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
+# Starts a Cocoon agent to run in continuous integration (CI) mode.
+#
+# Expectes current directory to be cocoon/agent.
+# Requires $HOME environment variable.
 set -e
-
-# Starts a Cocoon agent.
 
 USER_NAME="flutter"
 CONFIG_FILE="config.yaml"
+LOG_FILE="$HOME/agent.log"
 
 if [ "$(whoami)" != "$USER_NAME" ]; then
-  echo "Should run under user '$USER_NAME'."
+  echo "Should run under user: $USER_NAME."
   exit 1
 fi
-
-pushd "$HOME/cocoon/agent"
 
 if [ ! -f "$CONFIG_FILE" ]; then
   echo "Unable to find $CONFIG_FILE."
   exit 1
 fi
 
-# Unlocks the login keychain, otherwise the error of "Your session has expired" occurs shortly.
+# Unlocks the login keychain, otherwise Xcode build reports the error of
+# "Your session has expired" shortly.
 if [ "$(uname)" == "Darwin" ]; then
   security unlock-keychain login.keychain
 fi
 
 pub get
 
-dart bin/agent.dart ci -c "$CONFIG_FILE" 2>&1 >> "$HOME/agent.log" & disown
+dart bin/agent.dart ci -c "$CONFIG_FILE" 2>&1 >> "$LOG_FILE" &
 
-tail -f "$HOME/agent.log"
+pid="$(pgrep -f bin/agent.dart)" || true
 
-popd
+if [ -z "$pid" ]; then
+  echo "Unable to start agent, please check the log at $LOG_FILE"
+else
+  echo "Started agent with PID: $pid"
+fi
--- agent/bin/smoke_test.sh
+++ agent/bin/smoke_test.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
+# Runs a smoke test.
+#
+# This is useful to validate the runtime requirements of a Cocoon agent. It's
+# intended to run manually after provision.
+#
+# Expectes current directory to be cocoon/agent.
 set -e
 
-# Runs a smoke test in order to validate the runtime environment for Cocoon agent.
-
-LOG=$(mktemp)
+LOG_FILE=$(mktemp)
 
 case "$1" in
   ios)
     security unlock-keychain login.keychain
-    dart bin/agent.dart run -r HEAD -t flutter_gallery_ios__start_up | tee $LOG
+    dart bin/agent.dart run -r HEAD -t flutter_gallery_ios__start_up 2>&1 | \
+      tee "$LOG_FILE"
   ;;
 
   ios32)
     security unlock-keychain login.keychain
-    dart bin/agent.dart run -r HEAD -t flutter_gallery_ios32__start_up | tee $LOG
+    dart bin/agent.dart run -r HEAD -t flutter_gallery_ios32__start_up 2>&1 | \
+      tee "$LOG_FILE"
   ;;
 
   android)
-    dart bin/agent.dart run -r HEAD -t flutter_gallery__start_up | tee $LOG
+    dart bin/agent.dart run -r HEAD -t flutter_gallery__start_up 2>&1 | \
+      tee "$LOG_FILE"
   ;;
 
   *)
-    echo "Usage: ./smoke_test.sh (ios | ios32 | android)"
+    echo "Usage: .bin/smoke_test.sh (ios | ios32 | android)"
     exit 1
   ;;
 esac
 
-echo "See logs at $LOG"
+echo "Please see logs at $LOG_FILE"
