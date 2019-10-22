3085422ec020b4b948cb6b2847d3c56e41f044a7
Casey Hillers <chillers@google.com>
2019-10-22 10:49:00 -0700

    create matrix works
    so .filled() creates object of same reference. That took way too long...

--- app_flutter/lib/task_matrix.dart
+++ app_flutter/lib/task_matrix.dart
@@ -2,21 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:collection';
-
 import 'package:flutter/foundation.dart';
 
 import 'package:cocoon_service/protos.dart' show CommitStatus, Stage, Task;
 
 class TaskMatrix {
   TaskMatrix({@required this.statuses}) {
-    _columnKeyIndex = _createColumnKeyIndex();
-    _columnMap = <int, int>{};
-    for (int i = 0; i < _columnKeyIndex.length; i++) {
-      _columnMap[i] = i;
-    }
+    _columnKeyIndex = createColumnKeyIndex();
+    _columnMap = List<int>.generate(_columnKeyIndex.length, (int i) => i);
 
-    _matrix = _createTaskMatrix(statuses);
+    _matrix = createTaskMatrix(statuses);
   }
 
   final List<CommitStatus> statuses;
@@ -25,9 +20,11 @@ class TaskMatrix {
 
   /// A map for taking in the corresponding column in [StatusGrid] and
   /// where it maps to in [_taskMatrix].
-  Map<int, int> _columnMap;
+  List<int> _columnMap;
 
   /// A key, value table to find what column a [Task] is in.
+  /// 
+  /// This is necessary to ensure every possible task has a column in the grid.
   Map<String, int> _columnKeyIndex;
 
   int get columns => _matrix.length;
@@ -51,17 +48,19 @@ class TaskMatrix {
   ///
   /// [task.stageName] and [task.name] are not unique. However, they
   /// are unique when combined together.
-  String _taskColumnKey(Task task) {
+  @visibleForTesting
+  String taskColumnKey(Task task) {
     return '${task.stageName}:${task.name}';
   }
 
   /// A map of all [taskColumnKey] to a unique index from [List<CommitStatus>].
   ///
   /// Scans through all [Task] in [List<CommitStatus>] to find all unique [taskColumnKey].
-  /// This ensures a column is allocated in [_createTaskMatrix].
+  /// This ensures a column is allocated in [createTaskMatrix].
   ///
   /// When a new [taskColumnKey] is found, it is inserted and the index is incremeted.
-  Map<String, int> _createColumnKeyIndex({List<CommitStatus> statuses}) {
+  @visibleForTesting
+  Map<String, int> createColumnKeyIndex({List<CommitStatus> statuses}) {
     statuses ??= this.statuses;
 
     final Map<String, int> taskColumnKeyIndex = <String, int>{};
@@ -72,7 +71,7 @@ class TaskMatrix {
     for (CommitStatus status in statuses) {
       for (Stage stage in status.stages) {
         for (Task task in stage.tasks) {
-          final String key = _taskColumnKey(task);
+          final String key = taskColumnKey(task);
           if (taskColumnKeyIndex.containsKey(key)) {
             continue;
           }
@@ -85,53 +84,17 @@ class TaskMatrix {
     return taskColumnKeyIndex;
   }
 
-  /// Sort [columnKeyIndex] based on a list of weights.
-  ///
-  /// For duplicate weights, order is assumed to not matter.
-  @visibleForTesting
-  Map<String, int> sortColumnKeyIndex(
-      Map<String, int> columnKeyIndex, List<int> weights) {
-    final Map<String, int> sortedColumnKeyIndex = <String, int>{};
-
-    // 1. Create a map that maps the current index to its given weight
-    final Map<int, int> weightIndex = <int, int>{};
-    for (int i = 0; i < weights.length; i++) {
-      weightIndex[i] = weights[i];
-    }
-
-    // 2. Sort (1) by value (weight) in ascending order.
-    final List<int> sortedWeightKeys = weightIndex.keys.toList()
-      ..sort((int k1, int k2) => weightIndex[k1].compareTo(weightIndex[k2]));
-
-    // Store in a LinkedHashMap to preserve the order
-    final LinkedHashMap<int, int> sortedWeights = LinkedHashMap<int, int>();
-    for (int index = 0; index < sortedWeightKeys.length; index++) {
-      final int weight = sortedWeightKeys[index];
-      sortedWeights[weight] = weightIndex[weight];
-    }
-
-    // 3. Reassign the task index map based on the order from (2)
-    final Map<int, String> reversedColumnKeyIndex = columnKeyIndex
-        .map((String key, int value) => MapEntry<int, String>(value, key));
-    int newIndex = 0;
-    sortedWeights.forEach((int key, int value) {
-      final String taskKey = reversedColumnKeyIndex[key];
-      sortedColumnKeyIndex[taskKey] = newIndex++;
-    });
-
-    return sortedColumnKeyIndex;
-  }
-
   /// Create a matrix of [Task] for easier sorting of [List<CommitStatus>].
   ///
   /// If no [Task] can be placed in a cell of the matrix, it will be left null.
-  List<Column> _createTaskMatrix(List<CommitStatus> statuses,
+  @visibleForTesting
+  List<Column> createTaskMatrix(List<CommitStatus> statuses,
       {Map<String, int> columnKeyIndex}) {
     columnKeyIndex ??= _columnKeyIndex;
 
     /// Rows are commits, columns are [Task] with same [taskColumnKey].
-    final List<Column> matrix = List<Column>.filled(
-        columnKeyIndex.keys.length, Column(statuses.length));
+    final List<Column> matrix = List<Column>.generate(
+        columnKeyIndex.keys.length, (int i) => Column(statuses.length, columnKeyIndex.keys.elementAt(i)));
 
     for (int row = 0; row < statuses.length; row++) {
       final CommitStatus status = statuses[row];
@@ -139,9 +102,12 @@ class TaskMatrix {
       /// Organize [Task] in [CommitStatus] to the column they map to.
       for (Stage stage in status.stages) {
         for (Task task in stage.tasks) {
-          final int columnIndex = columnKeyIndex[_taskColumnKey(task)];
+          final String columnKey = taskColumnKey(task);
+          final int columnIndex = columnKeyIndex[columnKey];
           final Column column = matrix[columnIndex];
-          column.tasks[row] = task;
+          final List<Task> tasks = column.tasks;
+
+          tasks[row] = task;
           column.sampleTask = task;
         }
       }
@@ -152,7 +118,9 @@ class TaskMatrix {
 }
 
 class Column {
-  Column(int size) : tasks = List<Task>.filled(size, null);
+  Column(int size, this.key) : tasks = List<Task>(size);
+
+  final String key;
 
   List<Task> tasks;
 
--- app_flutter/test/task_matrix_test.dart
+++ app_flutter/test/task_matrix_test.dart
@@ -10,10 +10,15 @@ import 'package:cocoon_service/protos.dart' show CommitStatus, Stage, Task;
 
 void main() {
   group('TaskMatrix', () {
-    test('create task matrix', () {});
+    CommitStatus statusA;
+    CommitStatus statusB;
 
-    test('create task matrix that has skips', () {
-      final CommitStatus statusA = CommitStatus()
+    List<CommitStatus> statuses;
+
+    TaskMatrix matrix;
+
+    setUp(() {
+      statusA = CommitStatus()
         ..stages.insert(
             0,
             Stage()
@@ -22,9 +27,56 @@ void main() {
                   Task()
                     ..status = TaskBox.statusSucceeded
                     ..stageName = 'A'
-                    ..name = 'special task'));
+                    ..name = '1')
+              ..tasks.insert(
+                  1,
+                  Task()
+                    ..status = TaskBox.statusSucceeded
+                    ..stageName = 'A'
+                    ..name = '2'));
 
-      final CommitStatus statusB = CommitStatus()
+      statusB = CommitStatus()
+        ..stages.insert(
+            0,
+            Stage()
+              ..tasks.insert(
+                  0,
+                  Task()
+                    ..status = TaskBox.statusFailed
+                    ..stageName = 'A'
+                    ..name = '1')
+              ..tasks.insert(
+                  1,
+                  Task()
+                    ..status = TaskBox.statusFailed
+                    ..stageName = 'A'
+                    ..name = '2'));
+
+      statuses = <CommitStatus>[statusA, statusB];
+
+      matrix = TaskMatrix(statuses: statuses);
+    });
+
+    test('task column key', () {
+      final String key = matrix.taskColumnKey(Task()
+        ..stageName = 'A'
+        ..name = '1');
+
+      expect(key, 'A:1');
+    });
+
+    test('create task matrix', () {
+      final List<Column> matrix =
+          TaskMatrix(statuses: statuses).createTaskMatrix(statuses);
+
+      expect(matrix[0].tasks[0], statusA.stages[0].tasks[0]);
+      expect(matrix[0].tasks[1], statusB.stages[0].tasks[0]);
+      expect(matrix[1].tasks[0], statusA.stages[0].tasks[1]);
+      expect(matrix[1].tasks[1], statusB.stages[0].tasks[1]);
+    });
+
+    test('create task matrix that has skips', () {
+      final CommitStatus statusC = CommitStatus()
         ..stages.insert(
             0,
             Stage()
@@ -32,24 +84,27 @@ void main() {
                   0,
                   Task()
                     ..status = TaskBox.statusSucceeded
-                    ..stageName = 'B'
+                    ..stageName = 'C'
                     ..name = 'special task'));
 
-      final List<CommitStatus> statusesAB = <CommitStatus>[statusA, statusB];
-      final TaskMatrix matrix = TaskMatrix(statuses: statusesAB);
-
-      print('${matrix.task(0,0)?.stageName ?? 'null'}, ${matrix.task(0,1)?.stageName ?? 'null'}');
-      print('${matrix.task(1,0)?.stageName ?? 'null'}, ${matrix.task(1,1)?.stageName ?? 'null'}');
-
-      expect(matrix.task(0, 0), statusA.stages[0].tasks[0]);
+      final List<CommitStatus> statusesABC = <CommitStatus>[
+        statusA,
+        statusB,
+        statusC
+      ];
+      final TaskMatrix matrix = TaskMatrix(statuses: statusesABC);
+      
+      expect(matrix.task(0, 0), statusC.stages[0].tasks[0]);
       expect(matrix.task(0, 1), isNull);
       expect(matrix.task(1, 0), isNull);
       expect(matrix.task(1, 1), statusB.stages[0].tasks[0]);
 
-      expect(matrix.sampleTask(0), statusA.stages[0].tasks[0]);
+      expect(matrix.sampleTask(0), statusC.stages[0].tasks[0]);
       expect(matrix.sampleTask(1), statusB.stages[0].tasks[0]);
     });
 
     test('sorting by recently failed', () {});
+
+    test('create column key index works', () {});
   });
 }
