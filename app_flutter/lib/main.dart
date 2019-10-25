e3812d92913bc9650855f42053140596e73b7280
Casey Hillers <chillers@google.com>
2019-10-25 09:40:07 -0700

    tv refreshing

--- app_flutter/lib/main.dart
+++ app_flutter/lib/main.dart
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
+import 'dart:html' as html;
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'build_dashboard.dart';
@@ -11,6 +15,13 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    if (kIsWeb) {
+      /// This application is displayed on a TV that is hard to reach.
+      /// Refreshing this page ensures the TV stays relatively up to date without
+      /// anyone having to get on a ladder to use the TV as a computer.
+      Timer.periodic(const Duration(days: 1), (_) => html.window.location.reload());
+    }
+
     return MaterialApp(
       title: 'Flutter Build Dashboard',
       theme: ThemeData(
