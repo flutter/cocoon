6e701a5304503529de1d9e45e0dfd756ff7a1755
Casey Hillers <caseyhillers@gmail.com>
2019-11-19 06:42:54 -0800

    green appbar

--- app_flutter/test/build_dashboard_test.dart
+++ app_flutter/test/build_dashboard_test.dart
@@ -2,11 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 
+import 'package:cocoon_service/protos.dart' show CommitStatus, Task;
+
+import 'package:app_flutter/service/google_authentication.dart';
 import 'package:app_flutter/build_dashboard.dart';
+import 'package:app_flutter/main.dart' as app show theme;
+import 'package:app_flutter/service/cocoon.dart';
 import 'package:app_flutter/sign_in_button.dart';
 import 'package:app_flutter/state/flutter_build.dart';
 
@@ -22,4 +29,73 @@ void main() {
 
     expect(find.byType(SignInButton), findsOneWidget);
   });
+
+  testWidgets('shows tree closed when fetch tree status is false',
+      (WidgetTester tester) async {
+    final FlutterBuildState fakeBuildState = FakeFlutterBuildState();
+
+    await tester.pumpWidget(MaterialApp(
+        theme: app.theme,
+        home: ChangeNotifierProvider<FlutterBuildState>(
+          builder: (_) => fakeBuildState,
+          child: BuildDashboard(),
+        )));
+
+    expect(find.text('Tree is Closed'), findsOneWidget);
+
+    final AppBar appbarWidget = find.byType(AppBar).evaluate().first.widget;
+    expect(appbarWidget.backgroundColor, app.theme.errorColor);
+  });
+
+  testWidgets('shows tree open when fetch tree status is true',
+      (WidgetTester tester) async {
+    final FlutterBuildState fakeBuildState = FakeFlutterBuildState()
+      ..isTreeBuilding = (CocoonResponse<bool>()..data = true);
+
+    await tester.pumpWidget(MaterialApp(
+        theme: app.theme,
+        home: ChangeNotifierProvider<FlutterBuildState>(
+          builder: (_) => fakeBuildState,
+          child: BuildDashboard(),
+        )));
+
+    expect(find.text('Tree is Open'), findsOneWidget);
+
+    final AppBar appbarWidget = find.byType(AppBar).evaluate().first.widget;
+    expect(appbarWidget.backgroundColor, app.theme.appBarTheme.color);
+  });
+}
+
+class FakeFlutterBuildState extends ChangeNotifier
+    implements FlutterBuildState {
+  @override
+  GoogleSignInService authService = GoogleSignInService();
+
+  @override
+  Timer refreshTimer;
+
+  @override
+  bool hasError = false;
+
+  @override
+  CocoonResponse<bool> isTreeBuilding = CocoonResponse<bool>()..data = false;
+
+  @override
+  Duration get refreshRate => null;
+
+  @override
+  Future<bool> rerunTask(Task task) => null;
+
+  @override
+  Future<void> signIn() => null;
+
+  @override
+  Future<void> signOut() => null;
+
+  @override
+  Future<void> startFetchingBuildStateUpdates() => null;
+
+  @override
+  CocoonResponse<List<CommitStatus>> statuses =
+      CocoonResponse<List<CommitStatus>>()..data = <CommitStatus>[];
 }
