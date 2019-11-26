7ca1e000b267075e7edb84e3f21faf4272758825
Godofredo Contreras <godofredoc@google.com>
2019-11-26 08:57:49 -0800

    Runner scripts to run cocoon tests on luci.
    We need new scripts that encapsulates the commands cirrus was running
    for tests in this repo.

---
+++ test_utilities/bin/dart_test_runner.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Runner for dart tests. It expects a single parameter with the full
+# path to the start folder where tests will be run.
+
+set -e
+
+# Build and analize
+echo "Running tests from $1"
+pushd $1 > /dev/null
+pub get
+
+# agent doesn't use build_runner as of this writing.
+if grep -lq "build_runner" pubspec.yaml; then
+  pub run build_runner build --delete-conflicting-outputs
+fi
+
+# See https://github.com/dart-lang/sdk/issues/25551 for why this is necessary.
+pub global run tuneup check
+pub run test
+
+popd > /dev/null
