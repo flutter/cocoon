6617e07f4f01310787d0bc2058a631cce1c3a534
Keyong Han <keyonghan@google.com>
2019-12-04 13:58:02 -0800

    changed to check API

--- app_dart/lib/src/service/github_service.dart
+++ app_dart/lib/src/service/github_service.dart
@@ -8,31 +8,31 @@ import 'package:http/http.dart';
 import 'package:meta/meta.dart';
 
 @immutable
-class GithubService{
-
+class GithubService {
   const GithubService(this.github, this.slug);
 
   final GitHub github;
   final RepositorySlug slug;
 
-  String helper(String sha){
+  String helper(String sha) {
     return sha;
   }
 
-  Future<List<dynamic>> checkRuns(
-    RepositorySlug slug, String sha
-  ) async {
+  Future<List<dynamic>> checkRuns(RepositorySlug slug, String sha) async {
     final String path = '/repos/${slug.fullName}/commits/$sha/check-runs';
-    final Response response = await github.request('GET', path,
-        headers: <String, String>{'Accept': 'application/vnd.github.antiope-preview+json'});
-
-    if (response.statusCode == 202) {
-      throw NotReady(github, path);
-    } else if (response.statusCode != 200) {
-      github.handleStatusCode(response);
-    } else {
+    //final Response response = await github.request('GET', path,
+    //    headers: <String, String>{
+    //      'Accept': 'application/vnd.github.antiope-preview+json'
+    //    });
+    final PaginationHelper paginationHelper = PaginationHelper(github);
+    final List<dynamic> checkRuns = <dynamic>[];
+    await for (Response response in paginationHelper.fetchStreamed('GET', path,
+        headers: <String, String>{
+          'Accept': 'application/vnd.github.antiope-preview+json'
+        })) {
       final Map<String, dynamic> jsonStatus = json.decode(response.body);
-      return jsonStatus['check_runs'];
+      checkRuns.addAll(jsonStatus['check_runs']);
     }
+    return checkRuns;
   }
 }
