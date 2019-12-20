332a1e90e5a2dfd64cb927350d4d810a1199a4f3
Casey Hillers <chillers@google.com>
2019-12-20 10:05:02 -0800

    export agent, dartfmt

--- app_dart/lib/src/model/proto/internal/agent.pb.dart
+++ app_dart/lib/src/model/proto/internal/agent.pb.dart
@@ -13,37 +13,46 @@ import 'package:protobuf/protobuf.dart' as $pb;
 import 'key.pb.dart' as $0;
 
 class Agent extends $pb.GeneratedMessage {
-  static final $pb.BuilderInfo _i = $pb.BuilderInfo('Agent', createEmptyInstance: create)
-    ..aOM<$0.RootKey>(1, 'key', subBuilder: $0.RootKey.create)
-    ..aOS(2, 'agentId')
-    ..aInt64(3, 'healthCheckTimestamp')
-    ..aOB(4, 'isHealthy')
-    ..aOB(5, 'isHidden')
-    ..pPS(6, 'capabilities')
-    ..aOS(7, 'healthDetails')
-    ..aOS(8, 'authToken')
-    ..hasRequiredFields = false
-  ;
+  static final $pb.BuilderInfo _i =
+      $pb.BuilderInfo('Agent', createEmptyInstance: create)
+        ..aOM<$0.RootKey>(1, 'key', subBuilder: $0.RootKey.create)
+        ..aOS(2, 'agentId')
+        ..aInt64(3, 'healthCheckTimestamp')
+        ..aOB(4, 'isHealthy')
+        ..aOB(5, 'isHidden')
+        ..pPS(6, 'capabilities')
+        ..aOS(7, 'healthDetails')
+        ..aOS(8, 'authToken')
+        ..hasRequiredFields = false;
 
   Agent._() : super();
   factory Agent() => create();
-  factory Agent.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
-  factory Agent.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  factory Agent.fromBuffer($core.List<$core.int> i,
+          [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromBuffer(i, r);
+  factory Agent.fromJson($core.String i,
+          [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromJson(i, r);
   Agent clone() => Agent()..mergeFromMessage(this);
-  Agent copyWith(void Function(Agent) updates) => super.copyWith((message) => updates(message as Agent));
+  Agent copyWith(void Function(Agent) updates) =>
+      super.copyWith((message) => updates(message as Agent));
   $pb.BuilderInfo get info_ => _i;
   @$core.pragma('dart2js:noInline')
   static Agent create() => Agent._();
   Agent createEmptyInstance() => create();
   static $pb.PbList<Agent> createRepeated() => $pb.PbList<Agent>();
   @$core.pragma('dart2js:noInline')
-  static Agent getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Agent>(create);
+  static Agent getDefault() =>
+      _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Agent>(create);
   static Agent _defaultInstance;
 
   @$pb.TagNumber(1)
   $0.RootKey get key => $_getN(0);
   @$pb.TagNumber(1)
-  set key($0.RootKey v) { setField(1, v); }
+  set key($0.RootKey v) {
+    setField(1, v);
+  }
+
   @$pb.TagNumber(1)
   $core.bool hasKey() => $_has(0);
   @$pb.TagNumber(1)
@@ -54,7 +63,10 @@ class Agent extends $pb.GeneratedMessage {
   @$pb.TagNumber(2)
   $core.String get agentId => $_getSZ(1);
   @$pb.TagNumber(2)
-  set agentId($core.String v) { $_setString(1, v); }
+  set agentId($core.String v) {
+    $_setString(1, v);
+  }
+
   @$pb.TagNumber(2)
   $core.bool hasAgentId() => $_has(1);
   @$pb.TagNumber(2)
@@ -63,7 +75,10 @@ class Agent extends $pb.GeneratedMessage {
   @$pb.TagNumber(3)
   $fixnum.Int64 get healthCheckTimestamp => $_getI64(2);
   @$pb.TagNumber(3)
-  set healthCheckTimestamp($fixnum.Int64 v) { $_setInt64(2, v); }
+  set healthCheckTimestamp($fixnum.Int64 v) {
+    $_setInt64(2, v);
+  }
+
   @$pb.TagNumber(3)
   $core.bool hasHealthCheckTimestamp() => $_has(2);
   @$pb.TagNumber(3)
@@ -72,7 +87,10 @@ class Agent extends $pb.GeneratedMessage {
   @$pb.TagNumber(4)
   $core.bool get isHealthy => $_getBF(3);
   @$pb.TagNumber(4)
-  set isHealthy($core.bool v) { $_setBool(3, v); }
+  set isHealthy($core.bool v) {
+    $_setBool(3, v);
+  }
+
   @$pb.TagNumber(4)
   $core.bool hasIsHealthy() => $_has(3);
   @$pb.TagNumber(4)
@@ -81,7 +99,10 @@ class Agent extends $pb.GeneratedMessage {
   @$pb.TagNumber(5)
   $core.bool get isHidden => $_getBF(4);
   @$pb.TagNumber(5)
-  set isHidden($core.bool v) { $_setBool(4, v); }
+  set isHidden($core.bool v) {
+    $_setBool(4, v);
+  }
+
   @$pb.TagNumber(5)
   $core.bool hasIsHidden() => $_has(4);
   @$pb.TagNumber(5)
@@ -93,7 +114,10 @@ class Agent extends $pb.GeneratedMessage {
   @$pb.TagNumber(7)
   $core.String get healthDetails => $_getSZ(6);
   @$pb.TagNumber(7)
-  set healthDetails($core.String v) { $_setString(6, v); }
+  set healthDetails($core.String v) {
+    $_setString(6, v);
+  }
+
   @$pb.TagNumber(7)
   $core.bool hasHealthDetails() => $_has(6);
   @$pb.TagNumber(7)
@@ -102,10 +126,12 @@ class Agent extends $pb.GeneratedMessage {
   @$pb.TagNumber(8)
   $core.String get authToken => $_getSZ(7);
   @$pb.TagNumber(8)
-  set authToken($core.String v) { $_setString(7, v); }
+  set authToken($core.String v) {
+    $_setString(7, v);
+  }
+
   @$pb.TagNumber(8)
   $core.bool hasAuthToken() => $_has(7);
   @$pb.TagNumber(8)
   void clearAuthToken() => clearField(8);
 }
-
--- app_dart/lib/src/model/proto/protos.dart
+++ app_dart/lib/src/model/proto/protos.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export 'internal/agent.pb.dart';
 export 'internal/commit.pb.dart';
 export 'internal/commit_status.pb.dart';
 export 'internal/key.pb.dart';
