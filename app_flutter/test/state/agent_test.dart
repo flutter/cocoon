0547d543dd1cd5e9ea9d42b6aac04c72f2d6a02d
Casey Hillers <caseyhillers@gmail.com>
2019-12-31 17:18:48 -0800

    agent state tests

---
+++ app_flutter/test/state/agent_test.dart
@@ -0,0 +1,91 @@
+// Copyright (c) 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:cocoon_service/protos.dart' show Agent;
+
+import 'package:app_flutter/service/cocoon.dart';
+import 'package:app_flutter/service/fake_cocoon.dart';
+import 'package:app_flutter/state/agent.dart';
+
+void main() {
+  group('AgentState', () {
+    AgentState agentState;
+    MockCocoonService mockService;
+
+    setUp(() {
+      mockService = MockCocoonService();
+      agentState = AgentState(cocoonServiceValue: mockService);
+
+      when(mockService.fetchAgentStatuses()).thenAnswer((_) =>
+          Future<CocoonResponse<List<Agent>>>.value(
+              CocoonResponse<List<Agent>>()..data = <Agent>[Agent()]));
+    });
+
+    testWidgets('timer should periodically fetch updates',
+        (WidgetTester tester) async {
+      agentState.startFetchingStateUpdates();
+
+      // startFetching immediately starts fetching results
+      verify(mockService.fetchAgentStatuses()).called(1);
+
+      // Periodic timers don't necessarily run at the same time in each interval.
+      // We double the refreshRate to gurantee a call would have been made.
+      await tester.pump(agentState.refreshRate * 2);
+      verify(mockService.fetchAgentStatuses()).called(greaterThan(1));
+
+      // Tear down fails to cancel the timer before the test is over
+      agentState.dispose();
+    });
+
+    testWidgets('multiple start updates should not change the timer',
+        (WidgetTester tester) async {
+      agentState.startFetchingStateUpdates();
+      final Timer refreshTimer = agentState.refreshTimer;
+
+      // This second run should not change the refresh timer
+      agentState.startFetchingStateUpdates();
+
+      expect(refreshTimer, equals(agentState.refreshTimer));
+
+      // Since startFetching sends out requests on start, we need to wait
+      // for them to finish before disposing of the state.
+      await tester.pumpAndSettle();
+
+      // Tear down fails to cancel the timer before the test is over
+      agentState.dispose();
+    });
+
+    testWidgets('fetching agents error should not delete previous data',
+        (WidgetTester tester) async {
+      agentState.startFetchingStateUpdates();
+
+      // Periodic timers don't necessarily run at the same time in each interval.
+      // We double the refreshRate to gurantee a call would have been made.
+      await tester.pump(agentState.refreshRate * 2);
+      final List<Agent> originalData = agentState.agents;
+
+      when(mockService.fetchAgentStatuses()).thenAnswer((_) =>
+          Future<CocoonResponse<List<Agent>>>.value(
+              CocoonResponse<List<Agent>>()..error = 'error'));
+
+      await tester.pump(agentState.refreshRate * 2);
+      verify(mockService.fetchAgentStatuses()).called(greaterThan(1));
+
+      expect(agentState.agents, originalData);
+      expect(
+          agentState.errors.message, AgentState.errorMessageFetchingStatuses);
+
+      // Tear down fails to cancel the timer before the test is over
+      agentState.dispose();
+    });
+  });
+}
+
+/// CocoonService for checking interactions.
+class MockCocoonService extends Mock implements FakeCocoonService {}
