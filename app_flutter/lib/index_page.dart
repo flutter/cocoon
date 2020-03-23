9bdbc5f25132ca796cb18fbdd9a4a5546411ba1a
Ian Hickson <ian@hixie.ch>
2020-03-23 15:51:21 -0700

    HeaderText widget
    As requested in previous PR review.

--- app_flutter/lib/index_page.dart
+++ app_flutter/lib/index_page.dart
@@ -7,11 +7,12 @@ import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'error_brook_watcher.dart';
+import 'header_text.dart';
 import 'navigation_drawer.dart';
 import 'sign_in_button.dart';
 import 'state/index.dart';
 
-/// Index page
+/// Index page.
 ///
 /// Expects an [IndexState] to be available via [Provider].
 class IndexPage extends StatelessWidget {
@@ -48,8 +49,7 @@ class IndexPage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
                   separator,
-                  // TODO(ianh): Factor out into a HeaderText widget.
-                  Text('Select a dashboard', style: Theme.of(context).textTheme.headline4, textAlign: TextAlign.center),
+                  const HeaderText('Select a dashboard'),
                   separator,
                   RaisedButton.icon(
                     icon: Icon(Icons.build),
