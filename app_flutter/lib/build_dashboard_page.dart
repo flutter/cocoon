flutter/cocoon

commit cda7afea8f59241e97bbfa2160f5d9f2f8393430

Move state creation to the top of the widget tree (#707)

app_flutter/lib/build_dashboard_page.dart
@@ -5,98 +5,35 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'error_brook_watcher.dart';
 import 'navigation_drawer.dart';
-import 'service/google_authentication.dart';
 import 'sign_in_button.dart';
 import 'state/flutter_build.dart';
 import 'status_grid.dart';
-
-/// [BuildDashboard] parent widget that manages the state of the dashboard.
-class BuildDashboardPage extends StatefulWidget {
-  // TODO(ianh): there's a number of problems with the design here
-  // - the widget itself (as opposed to its State) has state (it creates an FlutterBuildState)
-  // - the State doesn't handle the widget's buildState property changing dynamically
-  // - the State doesn't handle the case of the signInService changing dynamically
-  // - the State caches the buildState from the widget, leading to a two-sources-of-truth situation
-  // - the State causes the FlutterBuildState to start donig its updates, rather than just subscribing
-  //   and letting the FlutterBuildState logic determine whether it has clients and should be live
-  // We could probably solve most of these problems by moving all the app state out of the widget
-  // tree and using inherited widgets to get at it.
-
-  BuildDashboardPage({
-    Key key,
-    FlutterBuildState buildState,
-    GoogleSignInService signInService,
-  })  : buildState = buildState ?? FlutterBuildState(authServiceValue: signInService),
-        super(key: key);
-
-  static const String routeName = '/build';
-
-  final FlutterBuildState buildState;
-
-  @visibleForTesting
-  static const Duration errorSnackbarDuration = Duration(seconds: 8);
-
-  @override
-  _BuildDashboardPageState createState() => _BuildDashboardPageState();
-}
-
-class _BuildDashboardPageState extends State<BuildDashboardPage> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
-  FlutterBuildState buildState;
-
-  @override
-  void initState() {
-    super.initState();
-    widget.buildState.startFetchingUpdates();
-    widget.buildState.errors.addListener(_showErrorSnackbar);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    buildState = widget.buildState;
-    return ChangeNotifierProvider<FlutterBuildState>(
-      create: (_) => buildState,
-      child: BuildDashboard(scaffoldKey: _scaffoldKey),
-    );
-  }
-
-  void _showErrorSnackbar(String error) {
-    final Row snackbarContent = Row(
-      children: <Widget>[
-        const Icon(Icons.error),
-        const SizedBox(width: 10),
-        Text(error),
-      ],
-    );
-    _scaffoldKey.currentState.showSnackBar(
-      SnackBar(
-        content: snackbarContent,
-        backgroundColor: Theme.of(context).errorColor,
-        duration: BuildDashboardPage.errorSnackbarDuration,
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    buildState.errors.removeListener(_showErrorSnackbar);
-    super.dispose();
-  }
-}
 
 /// Shows information about the current build status of flutter/flutter.
 ///
 /// The tree's current build status is reflected in [AppBar].
 /// The results from tasks run on individual commits is shown in [StatusGrid].
-class BuildDashboard extends StatelessWidget {
-  const BuildDashboard({
+class BuildDashboardPage extends StatefulWidget {
+  const BuildDashboardPage({
     Key key,
-    this.scaffoldKey,
   }) : super(key: key);
 
-  final GlobalKey<ScaffoldState> scaffoldKey;
+  static const String routeName = '/build';
+
+  @override
+  State<BuildDashboardPage> createState() => _BuildDashboardPageState();
+}
+
+class _BuildDashboardPageState extends State<BuildDashboardPage> {
+  FlutterBuildState _buildState;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _buildState = Provider.of<FlutterBuildState>(context)..startFetchingUpdates();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -116,15 +53,15 @@ class BuildDashboard extends StatelessWidget {
       true: Text('Tree is Open'),
     };
 
-    return Consumer<FlutterBuildState>(
-      builder: (_, FlutterBuildState buildState, Widget child) => Scaffold(
-        key: scaffoldKey,
+    return AnimatedBuilder(
+      animation: _buildState,
+      builder: (BuildContext context, Widget child) => Scaffold(
         appBar: AppBar(
-          title: statusTable[buildState.isTreeBuilding],
-          backgroundColor: colorTable[buildState.isTreeBuilding],
+          title: statusTable[_buildState.isTreeBuilding],
+          backgroundColor: colorTable[_buildState.isTreeBuilding],
           actions: <Widget>[
             DropdownButton<String>(
-                value: buildState.currentBranch,
+                value: _buildState.currentBranch,
                 icon: Icon(
                   Icons.arrow_downward,
                   color: Colors.white,
@@ -137,22 +74,26 @@ class BuildDashboard extends StatelessWidget {
                   color: Colors.white,
                 ),
                 onChanged: (String branch) {
-                  buildState.updateCurrentBranch(branch);
+                  _buildState.updateCurrentBranch(branch);
                 },
-                items: buildState.branches.map<DropdownMenuItem<String>>((String value) {
+                items: _buildState.branches.map<DropdownMenuItem<String>>((String value) {
                   return DropdownMenuItem<String>(
                     value: value,
                     child: Text(value, style: const TextStyle(color: Colors.black)),
                   );
                 }).toList()),
             const SizedBox(width: 10), // Padding between branches and sign in
-            SignInButton(authService: buildState.authService),
+            const SignInButton(),
           ],
         ),
-        body: Column(
-          children: const <Widget>[
-            StatusGridContainer(),
-          ],
+        body: ErrorBrookWatcher(
+          errors: _buildState.errors,
+          child: Column(
+            // TODO(ianh): Replace with a more idiomatic solution.
+            children: const <Widget>[
+              StatusGridContainer(),
+            ],
+          ),
         ),
         drawer: const NavigationDrawer(),
       ),

app_flutter/test/status_grid_test.dart
@@ -10,7 +10,7 @@ import 'package:provider/provider.dart';
 import 'package:cocoon_service/protos.dart' show Commit, CommitStatus, Stage, Task;
 
 import 'package:app_flutter/service/cocoon.dart';
-import 'package:app_flutter/service/fake_cocoon.dart';
+import 'package:app_flutter/service/dev_cocoon.dart';
 import 'package:app_flutter/state/flutter_build.dart';
 import 'package:app_flutter/commit_box.dart';
 import 'package:app_flutter/status_grid.dart';
@@ -22,14 +22,16 @@ import 'utils/fake_flutter_build.dart';
 
 void main() {
   group('StatusGrid', () {
-    FakeCocoonService service;
+    DevelopmentCocoonService service;
 
     List<CommitStatus> statuses;
 
     TaskMatrix taskMatrix;
 
     setUpAll(() async {
-      service = FakeCocoonService();
+      // TODO(ianh): DevelopmentCocoonService has randomness, which introduces the possibility of flakes.
+      // We should either pin the seed or use a more direct fake that isn't random.
+      service = DevelopmentCocoonService();
       final CocoonResponse<List<CommitStatus>> response = await service.fetchCommitStatuses();
       statuses = response.data;
       taskMatrix = TaskMatrix(statuses: statuses);

commit 53b73ef7487e8247932fbfea78415737cc02e3cb

Fix a regression introduced by https://github.com/flutter/cocoon/pull/707

app_flutter/lib/build_dashboard_page.dart
@@ -88,11 +88,8 @@ class _BuildDashboardPageState extends State<BuildDashboardPage> {
         ),
         body: ErrorBrookWatcher(
           errors: _buildState.errors,
-          child: Column(
-            // TODO(ianh): Replace with a more idiomatic solution.
-            children: const <Widget>[
-              StatusGridContainer(),
-            ],
+          child: const SizedBox.expand(
+            child: StatusGridContainer(),
           ),
         ),
         drawer: const NavigationDrawer(),

app_flutter/lib/status_grid.dart
@@ -28,16 +28,16 @@ class StatusGridContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<FlutterBuildState>(
-      builder: (_, FlutterBuildState buildState, Widget child) {
+    final FlutterBuildState buildState = Provider.of<FlutterBuildState>(context);
+    return AnimatedBuilder(
+      animation: buildState,
+      builder: (BuildContext context, Widget child) {
         final List<CommitStatus> statuses = buildState.statuses;
 
         // Assume if there is no data that it is loading.
         if (statuses.isEmpty) {
-          return const Expanded(
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
+          return const Center(
+            child: CircularProgressIndicator(),
           );
         }
 
@@ -106,104 +106,102 @@ class StatusGrid extends StatelessWidget {
     /// The grid needs to know its dimensions. Column is based off how many tasks are
     /// in a row (+ 1 to account for [CommitBox]).
     final int columnCount = taskMatrix.columns + 1;
-    return Expanded(
-      // The grid is wrapped with SingleChildScrollView to enable scrolling both
-      // horizontally and vertically
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Container(
-          width: columnCount * cellSize,
-          // TODO(chillers): Refactor this to a separate TaskView widget. https://github.com/flutter/flutter/issues/43376
-          child: GridView.builder(
-            addRepaintBoundaries: false,
+    // The grid is wrapped with SingleChildScrollView to enable scrolling both
+    // horizontally and vertically
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Container(
+        width: columnCount * cellSize,
+        // TODO(chillers): Refactor this to a separate TaskView widget. https://github.com/flutter/flutter/issues/43376
+        child: GridView.builder(
+          addRepaintBoundaries: false,
 
-            /// The grid has as many rows as there are statuses. Additionally,
-            /// one row for task descriptions, and one at the bottom to show
-            /// a loader for more data.
-            itemCount: columnCount * (statuses.length + 2),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: columnCount,
-            ),
-            itemBuilder: (BuildContext context, int gridIndex) {
-              if (gridIndex == 0) {
-                /// The top left corner of the grid is nothing since
-                /// the left column is for [CommitBox] and the top
-                /// row is for [TaskIcon].
-                return const SizedBox();
-              }
-
-              /// Loader row at the bottom of the grid.
-              if (_isLastRow(
-                gridIndex: gridIndex,
-                columnCount: columnCount,
-              )) {
-                final int loaderIndex = gridIndex % columnCount;
-
-                /// This loader row will spell out [loadingText].
-                const String loaderRowText = 'LOADING ';
-                final Color lastRowColor = buildState.moreStatusesExist ? Colors.blueGrey : Colors.transparent;
-                final String keyValue =
-                    buildState.moreStatusesExist ? 'loader-$loaderIndex' : 'hidden-loader-$loaderIndex';
-
-                /// Only trigger [fetchMoreCommitStatuses] API call once for
-                /// this loading row.
-                if (loaderIndex == 0 && buildState.moreStatusesExist) {
-                  buildState.fetchMoreCommitStatuses();
-                }
-
-                return Container(
-                  key: insertCellKeys ? Key(keyValue) : null,
-                  color: lastRowColor,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(10.0, 14.0, 10.0, 14.0),
-                    child: buildState.moreStatusesExist
-                        ? Text(loaderRowText[loaderIndex % loaderRowText.length],
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                            ))
-                        : null,
-                  ),
-                );
-              }
-
-              /// This [GridView] is composed of a row of [TaskIcon] and a subgrid
-              /// of [List<List<Task>>]. This allows the row of [TaskIcon] to align
-              /// with the column of [Task] that it maps to.
-              ///
-              /// Mapping [gridIndex] to [index] allows us to ignore the overhead the
-              /// row of [TaskIcon] introduces.
-              final int index = gridIndex - columnCount;
-              if (index < 0) {
-                return TaskIcon(
-                  key: insertCellKeys ? Key('taskicon-${index % columnCount}') : null,
-                  task: taskMatrix.sampleTask(gridIndex - 1),
-                );
-              }
-
-              final int row = index ~/ columnCount;
-              if (index % columnCount == 0) {
-                return CommitBox(commit: statuses[row].commit);
-              }
-
-              final int column = (index % columnCount) - 1;
-              final Task task = taskMatrix.task(row, column);
-              if (task == null) {
-                /// [Task] was skipped so don't show anything.
-                return SizedBox(
-                  key: insertCellKeys ? Key('cell-$row-$column') : null,
-                  width: StatusGrid.cellSize,
-                );
-              }
-
-              return TaskBox(
-                key: insertCellKeys ? Key('cell-$row-$column') : null,
-                task: task,
-                buildState: buildState,
-                commit: statuses[row].commit,
-              );
-            },
+          /// The grid has as many rows as there are statuses. Additionally,
+          /// one row for task descriptions, and one at the bottom to show
+          /// a loader for more data.
+          itemCount: columnCount * (statuses.length + 2),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: columnCount,
           ),
+          itemBuilder: (BuildContext context, int gridIndex) {
+            if (gridIndex == 0) {
+              /// The top left corner of the grid is nothing since
+              /// the left column is for [CommitBox] and the top
+              /// row is for [TaskIcon].
+              return const SizedBox();
+            }
+
+            /// Loader row at the bottom of the grid.
+            if (_isLastRow(
+              gridIndex: gridIndex,
+              columnCount: columnCount,
+            )) {
+              final int loaderIndex = gridIndex % columnCount;
+
+              /// This loader row will spell out [loadingText].
+              const String loaderRowText = 'LOADING ';
+              final Color lastRowColor = buildState.moreStatusesExist ? Colors.blueGrey : Colors.transparent;
+              final String keyValue =
+                  buildState.moreStatusesExist ? 'loader-$loaderIndex' : 'hidden-loader-$loaderIndex';
+
+              /// Only trigger [fetchMoreCommitStatuses] API call once for
+              /// this loading row.
+              if (loaderIndex == 0 && buildState.moreStatusesExist) {
+                buildState.fetchMoreCommitStatuses();
+              }
+
+              return Container(
+                key: insertCellKeys ? Key(keyValue) : null,
+                color: lastRowColor,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 14.0, 10.0, 14.0),
+                  child: buildState.moreStatusesExist
+                      ? Text(loaderRowText[loaderIndex % loaderRowText.length],
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ))
+                      : null,
+                ),
+              );
+            }
+
+            /// This [GridView] is composed of a row of [TaskIcon] and a subgrid
+            /// of [List<List<Task>>]. This allows the row of [TaskIcon] to align
+            /// with the column of [Task] that it maps to.
+            ///
+            /// Mapping [gridIndex] to [index] allows us to ignore the overhead the
+            /// row of [TaskIcon] introduces.
+            final int index = gridIndex - columnCount;
+            if (index < 0) {
+              return TaskIcon(
+                key: insertCellKeys ? Key('taskicon-${index % columnCount}') : null,
+                task: taskMatrix.sampleTask(gridIndex - 1),
+              );
+            }
+
+            final int row = index ~/ columnCount;
+            if (index % columnCount == 0) {
+              return CommitBox(commit: statuses[row].commit);
+            }
+
+            final int column = (index % columnCount) - 1;
+            final Task task = taskMatrix.task(row, column);
+            if (task == null) {
+              /// [Task] was skipped so don't show anything.
+              return SizedBox(
+                key: insertCellKeys ? Key('cell-$row-$column') : null,
+                width: StatusGrid.cellSize,
+              );
+            }
+
+            return TaskBox(
+              key: insertCellKeys ? Key('cell-$row-$column') : null,
+              task: task,
+              buildState: buildState,
+              commit: statuses[row].commit,
+            );
+          },
         ),
       ),
     );

app_flutter/test/status_grid_test.dart
@@ -5,7 +5,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
 
 import 'package:cocoon_service/protos.dart' show Commit, CommitStatus, Stage, Task;
 
@@ -13,12 +12,14 @@ import 'package:app_flutter/service/cocoon.dart';
 import 'package:app_flutter/service/dev_cocoon.dart';
 import 'package:app_flutter/state/flutter_build.dart';
 import 'package:app_flutter/commit_box.dart';
+import 'package:app_flutter/state_provider.dart';
 import 'package:app_flutter/status_grid.dart';
 import 'package:app_flutter/task_box.dart';
 import 'package:app_flutter/task_icon.dart';
 import 'package:app_flutter/task_matrix.dart' show TaskMatrix;
 
 import 'utils/fake_flutter_build.dart';
+import 'utils/mocks.dart';
 
 void main() {
   group('StatusGrid', () {
@@ -45,13 +46,9 @@ void main() {
     testWidgets('shows loading indicator when statuses is empty', (WidgetTester tester) async {
       await tester.pumpWidget(
         MaterialApp(
-          home: Column(
-            children: <Widget>[
-              ChangeNotifierProvider<FlutterBuildState>(
-                create: (_) => FakeFlutterBuildState(),
-                child: const StatusGridContainer(),
-              ),
-            ],
+          home: ValueProvider<FlutterBuildState>(
+            value: FakeFlutterBuildState(),
+            child: const StatusGridContainer(),
           ),
         ),
       );
@@ -60,17 +57,41 @@ void main() {
       expect(find.byType(GridView), findsNothing);
     });
 
+    testWidgets('commits show in the same column (indirectly via StatusGridContainer)', (WidgetTester tester) async {
+      final FlutterBuildState buildState = FlutterBuildState(
+        cocoonService: service,
+        authService: MockGoogleSignInService(),
+      );
+      buildState.startFetchingUpdates();
+      await tester.pumpWidget(
+        MaterialApp(
+          home: ValueProvider<FlutterBuildState>(
+            value: buildState,
+            child: const StatusGridContainer(),
+          ),
+        ),
+      );
+
+      final List<Element> commits = find.byType(CommitBox).evaluate().toList();
+
+      final double xPosition = commits.first.size.topLeft(Offset.zero).dx;
+
+      for (final Element commit in commits) {
+        // All the x positions should match the first instance if they're all in the same column
+        expect(commit.size.topLeft(Offset.zero).dx, xPosition);
+      }
+
+      await tester.pumpWidget(Container());
+      buildState.dispose();
+    });
+
     testWidgets('commits show in the same column', (WidgetTester tester) async {
       await tester.pumpWidget(
         MaterialApp(
-          home: Column(
-            children: <Widget>[
-              StatusGrid(
-                buildState: FakeFlutterBuildState(),
-                statuses: statuses,
-                taskMatrix: taskMatrix,
-              ),
-            ],
+          home: StatusGrid(
+            buildState: FakeFlutterBuildState(),
+            statuses: statuses,
+            taskMatrix: taskMatrix,
           ),
         ),
       );
@@ -88,14 +109,10 @@ void main() {
     testWidgets('first task in grid is the first task given', (WidgetTester tester) async {
       await tester.pumpWidget(
         MaterialApp(
-          home: Column(
-            children: <Widget>[
-              StatusGrid(
-                buildState: FakeFlutterBuildState(),
-                statuses: statuses,
-                taskMatrix: taskMatrix,
-              ),
-            ],
+          home: StatusGrid(
+            buildState: FakeFlutterBuildState(),
+            statuses: statuses,
+            taskMatrix: taskMatrix,
           ),
         ),
       );
@@ -146,15 +163,11 @@ void main() {
 
       await tester.pumpWidget(
         MaterialApp(
-          home: Column(
-            children: <Widget>[
-              StatusGrid(
-                buildState: FakeFlutterBuildState(),
-                statuses: statusesWithSkips,
-                taskMatrix: taskMatrix,
-                insertCellKeys: true,
-              ),
-            ],
+          home: StatusGrid(
+            buildState: FakeFlutterBuildState(),
+            statuses: statusesWithSkips,
+            taskMatrix: taskMatrix,
+            insertCellKeys: true,
           ),
         ),
       );
@@ -198,15 +211,11 @@ void main() {
 
       await tester.pumpWidget(
         MaterialApp(
-          home: Column(
-            children: <Widget>[
-              StatusGrid(
-                buildState: FakeFlutterBuildState(),
-                statuses: statusesWithSkips,
-                taskMatrix: taskMatrix,
-                insertCellKeys: true,
-              ),
-            ],
+          home: StatusGrid(
+            buildState: FakeFlutterBuildState(),
+            statuses: statusesWithSkips,
+            taskMatrix: taskMatrix,
+            insertCellKeys: true,
           ),
         ),
       );
@@ -226,15 +235,11 @@ void main() {
     testWidgets('task icon row is created', (WidgetTester tester) async {
       await tester.pumpWidget(
         MaterialApp(
-          home: Column(
-            children: <Widget>[
-              StatusGrid(
-                buildState: FakeFlutterBuildState(),
-                statuses: statuses,
-                taskMatrix: taskMatrix,
-                insertCellKeys: true,
-              ),
-            ],
+          home: StatusGrid(
+            buildState: FakeFlutterBuildState(),
+            statuses: statuses,
+            taskMatrix: taskMatrix,
+            insertCellKeys: true,
           ),
         ),
       );
@@ -261,15 +266,11 @@ void main() {
       final TaskMatrix smallTaskMatrix = TaskMatrix(statuses: smallRangeOfStatusesToShowLoader);
       await tester.pumpWidget(
         MaterialApp(
-          home: Column(
-            children: <Widget>[
-              StatusGrid(
-                buildState: FakeFlutterBuildState(),
-                statuses: smallRangeOfStatusesToShowLoader,
-                taskMatrix: smallTaskMatrix,
-                insertCellKeys: true,
-              ),
-            ],
+          home: StatusGrid(
+            buildState: FakeFlutterBuildState(),
+            statuses: smallRangeOfStatusesToShowLoader,
+            taskMatrix: smallTaskMatrix,
+            insertCellKeys: true,
           ),
         ),
       );
@@ -287,15 +288,11 @@ void main() {
       final TaskMatrix smallTaskMatrix = TaskMatrix(statuses: smallRangeOfStatusesToShowLoader);
       await tester.pumpWidget(
         MaterialApp(
-          home: Column(
-            children: <Widget>[
-              StatusGrid(
-                buildState: FakeFlutterBuildState()..moreStatusesExist = false,
-                statuses: smallRangeOfStatusesToShowLoader,
-                taskMatrix: smallTaskMatrix,
-                insertCellKeys: true,
-              ),
-            ],
+          home: StatusGrid(
+            buildState: FakeFlutterBuildState()..moreStatusesExist = false,
+            statuses: smallRangeOfStatusesToShowLoader,
+            taskMatrix: smallTaskMatrix,
+            insertCellKeys: true,
           ),
         ),
       );