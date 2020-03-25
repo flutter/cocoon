93709775f76ac97bda9b4f2b23d7808006058b33
Ian Hickson <ian@hixie.ch>
2020-03-25 19:42:34 -0400

    Make AgentState automatically start fetching data when listened. (#722)
    This means we don't have to explicitly tell it to start fetching.

--- app_flutter/test/state/agent_test.dart
+++ app_flutter/test/state/agent_test.dart
@@ -17,16 +17,12 @@ import '../utils/output.dart';
 
 void main() {
   group('AgentState', () {
-    AgentState agentState;
     MockCocoonService mockCocoonService;
     MockGoogleSignInService mockSignInService;
-    String lastError;
 
     setUp(() {
       mockSignInService = MockGoogleSignInService();
       mockCocoonService = MockCocoonService();
-      agentState = AgentState(cocoonService: mockCocoonService, authService: mockSignInService)
-        ..errors.addListener((String message) => lastError = message);
 
       when(mockCocoonService.fetchAgentStatuses()).thenAnswer(
           (_) => Future<CocoonResponse<List<Agent>>>.value(CocoonResponse<List<Agent>>.data(<Agent>[Agent()])));
@@ -38,52 +34,61 @@ void main() {
     });
 
     testWidgets('timer should periodically fetch updates', (WidgetTester tester) async {
-      agentState.startFetchingStateUpdates();
+      final AgentState agentState = AgentState(cocoonService: mockCocoonService, authService: mockSignInService);
+      verifyNever(mockCocoonService.fetchAgentStatuses());
+
+      void listener() {}
+      agentState.addListener(listener);
 
       // startFetching immediately starts fetching results
       verify(mockCocoonService.fetchAgentStatuses()).called(1);
+      verifyNever(mockCocoonService.fetchAgentStatuses());
 
-      // Periodic timers don't necessarily run at the same time in each interval.
-      // We double the refreshRate to gurantee a call would have been made.
-      await tester.pump(agentState.refreshRate * 2);
-      verify(mockCocoonService.fetchAgentStatuses()).called(greaterThan(1));
+      await tester.pump(agentState.refreshRate);
+      verify(mockCocoonService.fetchAgentStatuses()).called(1);
 
-      // Tear down fails to cancel the timer before the test is over
+      await tester.pump(agentState.refreshRate);
+      verify(mockCocoonService.fetchAgentStatuses()).called(1);
+
       agentState.dispose();
     });
 
     testWidgets('multiple start updates should not change the timer', (WidgetTester tester) async {
-      agentState.startFetchingStateUpdates();
+      final AgentState agentState = AgentState(cocoonService: mockCocoonService, authService: mockSignInService);
+      void listener1() {}
+      agentState.addListener(listener1);
       final Timer refreshTimer = agentState.refreshTimer;
 
-      // This second run should not change the refresh timer
-      agentState.startFetchingStateUpdates();
+      // This second listener should not change the refresh timer.
+      void listener2() {}
+      agentState.addListener(listener2);
 
       expect(refreshTimer, equals(agentState.refreshTimer));
 
-      // Since startFetching sends out requests on start, we need to wait
-      // for them to finish before disposing of the state.
-      await tester.pumpAndSettle();
-
-      // Tear down fails to cancel the timer before the test is over
       agentState.dispose();
     });
 
     testWidgets('fetching agents error should not delete previous data', (WidgetTester tester) async {
-      agentState.startFetchingStateUpdates();
+      String lastError;
+      final AgentState agentState = AgentState(cocoonService: mockCocoonService, authService: mockSignInService)
+        ..errors.addListener((String message) => lastError = message);
+      verifyNever(mockCocoonService.fetchAgentStatuses());
 
-      // Periodic timers don't necessarily run at the same time in each interval.
-      // We double the refreshRate to gurantee a call would have been made.
-      await tester.pump(agentState.refreshRate * 2);
+      void listener1() {}
+      agentState.addListener(listener1);
+      verify(mockCocoonService.fetchAgentStatuses()).called(1);
+
+      await tester.pump(agentState.refreshRate);
       final List<Agent> originalData = agentState.agents;
 
       when(mockCocoonService.fetchAgentStatuses()).thenAnswer(
-          (_) => Future<CocoonResponse<List<Agent>>>.value(const CocoonResponse<List<Agent>>.error('error')));
+        (_) => Future<CocoonResponse<List<Agent>>>.value(const CocoonResponse<List<Agent>>.error('error')),
+      );
 
       await checkOutput(
         block: () async {
           await tester.pump(agentState.refreshRate);
-          verify(mockCocoonService.fetchAgentStatuses()).called(greaterThan(1));
+          verify(mockCocoonService.fetchAgentStatuses()).called(2);
         },
         output: <String>[
           'An error occured fetching agent statuses from Cocoon: error',
@@ -93,26 +98,31 @@ void main() {
       expect(agentState.agents, originalData);
       expect(lastError, startsWith(AgentState.errorMessageFetchingStatuses));
 
-      // Tear down fails to cancel the timer before the test is over
       agentState.dispose();
     });
 
     test('authorize agent calls cocoon service', () async {
-      when(mockCocoonService.authorizeAgent(any, any))
-          .thenAnswer((_) async => const CocoonResponse<String>.data('abc123'));
+      final AgentState agentState = AgentState(cocoonService: mockCocoonService, authService: mockSignInService);
+      when(mockCocoonService.authorizeAgent(any, any)).thenAnswer(
+        (_) async => const CocoonResponse<String>.data('abc123'),
+      );
       verifyNever(mockCocoonService.authorizeAgent(any, any));
 
       await agentState.authorizeAgent(Agent());
 
       verify(mockCocoonService.authorizeAgent(any, any)).called(1);
+      agentState.dispose();
     });
 
     test('reserve task calls cocoon service', () async {
+      final AgentState agentState = AgentState(cocoonService: mockCocoonService, authService: mockSignInService);
       verifyNever(mockCocoonService.reserveTask(any, any));
 
       await agentState.reserveTask(Agent());
 
       verify(mockCocoonService.reserveTask(any, any)).called(1);
+
+      agentState.dispose();
     });
   });
 }
