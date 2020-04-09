56c80aeede87afe7b0edc90d6e0723d5edebbcae
Keyong Han <keyonghan@google.com>
2020-04-09 11:56:47 -0700

    remove unsed import

--- app_dart/lib/src/request_handlers/get_branches.dart
+++ app_dart/lib/src/request_handlers/get_branches.dart
@@ -7,15 +7,9 @@ import 'dart:async';
 import 'package:meta/meta.dart';
 
 import '../datastore/cocoon_config.dart';
-import '../foundation/providers.dart';
-import '../foundation/typedefs.dart';
-import '../foundation/utils.dart';
 import '../request_handling/body.dart';
 import '../request_handling/request_handler.dart';
 
-/// Queries GitHub for the list of all available branches on
-/// [config.flutterSlug] repo, and returns list of branches
-/// that match pre-defined branch regular expressions.
 @immutable
 class GetBranches extends RequestHandler<Body> {
   const GetBranches(
