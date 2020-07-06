ce959eda072250eeedff2e8481e028976d9bf80b
Keyong Han <keyonghan@google.com>
2020-07-06 13:08:31 -0700

    test

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ rm -rf build
 flutter pub get
 flutter config --enable-web
 flutter build web --dart-define FLUTTER_WEB_USE_SKIA=true
-
+cd ../app_dart
+rm -rf build
+cp -r ../app_flutter/build build
---
+++ build2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ../app_dart
+gcloud app deploy --project tvolkert-test --version testkeyonghan --no-promote --no-stop-previous-version
