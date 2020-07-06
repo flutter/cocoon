87f5ccd146f46232fe31f6f48fe9ac666878824c
Keyong Han <keyonghan@google.com>
2020-07-06 11:58:54 -0700

    test

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cd app && flutter pub run build_runner build --release --output build --delete-conflicting-outputs
+cd app 
+flutter pub get
+flutter pub run build_runner build --release --output build --delete-conflicting-outputs
