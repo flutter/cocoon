5f1dc0f0a57cb62b339d086356263f4659fb3de8
Keyong Han <keyonghan@google.com>
2020-07-06 16:23:44 -0700

    test

--- build0.sh
+++ build0.sh
@@ -4,3 +4,5 @@ cd app
 rm -rf build
 flutter pub get
 flutter pub run build_runner build --release --output build --delete-conflicting-outputs
+mv build/web/build.html build/web/old_build.html
+cp -rn build/web ../app_dart/build/
