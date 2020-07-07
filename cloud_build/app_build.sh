5899f4155bbd8397f298989d67347ad66aa2f890
Keyong Han <keyonghan@google.com>
2020-07-07 11:55:56 -0700

    add license

--- cloud_build/app_build.sh
+++ cloud_build/app_build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright 2020 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 #
 # Build old angular dart app to generate flutter benchmark dashboard.
 
