913f12f164bd3cc4a30e11e7a843c13663f27bc4
Keyong Han <keyonghan@google.com>
2020-07-07 12:06:11 -0700

    license fix

--- cloud_build/app_build.sh
+++ cloud_build/app_build.sh
@@ -2,7 +2,7 @@
 # Copyright 2020 The Flutter Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-#
+
 # Build old angular dart app to generate flutter benchmark dashboard.
 
 cd app
--- cloud_build/deploy.sh
+++ cloud_build/deploy.sh
@@ -2,7 +2,7 @@
 # Copyright 2020 The Flutter Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-#
+
 # Deploy a new flutter dashboard version to google cloud.
 
 cd app_dart
