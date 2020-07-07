a877bf8ee65fc9845a530a72eb8efbae9cd19ba4
Keyong Han <keyonghan@google.com>
2020-07-07 10:38:17 -0700

    test

--- cloud_build/deploy.sh
+++ cloud_build/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd app_dart
-gcloud app deploy --project tvolkert-test --version version-$SHORT_SHA --no-promote --no-stop-previous-version
+version="version-$1"
+gcloud app deploy --project tvolkert-test --version "$version" --no-promote --no-stop-previous-version
