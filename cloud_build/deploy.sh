71978e7ed98c32336ac874b91b3445fba653e30a
Keyong Han <keyonghan@google.com>
2020-07-07 10:25:24 -0700

    test

--- cloud_build/deploy.sh
+++ cloud_build/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd app_dart
-gcloud app deploy --project tvolkert-test --version testkeyonghan --no-promote --no-stop-previous-version
+gcloud app deploy --project tvolkert-test --version version-$SHORT_SHA --no-promote --no-stop-previous-version
