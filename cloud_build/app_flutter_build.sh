00b65f5a5e2fae15318c8629bd02bb0a48047cce
Keyong Han <keyonghan@google.com>
2020-07-07 15:19:57 -0700

    test

--- cloud_build/app_flutter_build.sh
+++ cloud_build/app_flutter_build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+# Copyright 2020 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
-cd app_flutter
+# Build flutter app to generate flutter build dashboard.
+
+pushd app_flutter > /dev/null
 rm -rf build
 flutter pub get
 flutter config --enable-web
 flutter build web --dart-define FLUTTER_WEB_USE_SKIA=true
-cd ../app_dart
-rm -rf build
-cp -r ../app_flutter/build build
+rm -rf ../app_dart/build
+cp -r build ../app_dart/build
+flutter clean
+popd > /dev/null
--- cloud_build/deploy.sh
+++ cloud_build/deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Copyright 2020 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
-cd app_dart
-gcloud app deploy --project tvolkert-test --version "version-$1" --no-promote --no-stop-previous-version
+# Deploy a new flutter dashboard version to google cloud.
+
+pushd app_dart > /dev/null
+gcloud app deploy --project "$1" --version "version-$2" --no-promote --no-stop-previous-version
+popd > /dev/null
