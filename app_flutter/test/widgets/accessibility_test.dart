74be70096164e2fbf68653c8456e62a08becd41d
Kate Lovett <katelovett@google.com>
2020-07-17 09:49:10 -0700

    Review feedback

--- app_flutter/test/widgets/accessibility_test.dart
+++ app_flutter/test/widgets/accessibility_test.dart
@@ -11,45 +11,15 @@ import 'package:cocoon_service/protos.dart' show Commit;
 void main() {
   group('Author avatars meet guidelines for theme brightness', () {
     Widget buildAuthors() {
-      final List<String> names = <String>[
-        'a',
-        'b',
-        'c',
-        'd',
-        'e',
-        'f',
-        'g',
-        'h',
-        'i',
-        'j',
-        'k',
-        'l',
-        'm',
-        'n',
-        'o',
-        'p',
-        'q',
-        'r',
-        's',
-        't',
-        'u',
-        'v',
-        'w',
-        'x',
-        'y',
-        'z',
-        '1',
-        '2',
-        '3',
-        '4',
-        '5',
-        '6',
-        '7',
-        '8',
-        '9',
-        '0',
-      ];
+      final List<String> names = <String>[];
       final List<CommitAuthorAvatar> avatars = <CommitAuthorAvatar>[];
+
+      for (int i = 65; i <= 90; i++) {
+        names.add(String.fromCharCode(i));
+      }
+      for (int i = 0; i <= 9; i++) {
+        names.add(i.toString());
+      }
 
       for (final String name in names) {
         avatars.add(CommitAuthorAvatar(commit: Commit()..author = name));
