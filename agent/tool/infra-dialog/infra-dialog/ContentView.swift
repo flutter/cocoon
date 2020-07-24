62dc010b90e0acee109c45339d045b8736826459
Tong Wu <6259181+digiter@users.noreply.github.com>
2020-07-24 13:31:53 -0700

    [agent] Check in the infra-dialog Xcode project.
    Bug: https://github.com/flutter/flutter/issues/54792

---
+++ agent/tool/infra-dialog/infra-dialog/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  infra-dialog
+//
+//  Created by Tong Wu on 4/14/20.
+//  Copyright © 2020 Tong Wu. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+@available(iOS 13.0, *)
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
