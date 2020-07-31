50888ec2188b3323ad49df6b375b2780d445cb14
Keyong Han <keyonghan@google.com>
2020-07-31 13:19:40 -0700

    add json check to presubmit

--- dev/validate_json.dart
+++ dev/validate_json.dart
@@ -3,22 +3,30 @@
 // found in the LICENSE file.
 
 import 'dart:convert';
+import 'dart:io';
 import 'dart:io' show File;
+import 'dart:io' as io_internals show exit;
+
+final bool hasColor = stdout.supportsAnsiEscapes;
+final String bold = hasColor ? '\x1B[1m' : ''; // used for shard titles
+final String red = hasColor ? '\x1B[31m' : ''; // used for errors
+final String reset = hasColor ? '\x1B[0m' : '';
+final String reverse = hasColor ? '\x1B[7m' : ''; // used for clocks
 
 /// Validates if the input builders JSON file has valid contents.
 ///
 /// Examples:
 /// dart validate_json.dart /path/to/json/file
-Future<bool> main(List<String> args) async {
+Future<void> main(List<String> args) async {
   final String jsonString = await File(args[0]).readAsString();
   Map<String, dynamic> decodedJson;
-  bool decodedStatus = true;
+  final List<String> messages = <String>[];
   try {
     decodedJson = json.decode(jsonString) as Map<String, dynamic>;
     final List<dynamic> builders = decodedJson['builders'] as List<dynamic>;
     if (builders == null) {
-      print('Json format is violated: no "builders" exists. Please follow');
-      print('''
+      messages.add('${bold}Json format is violated: no "builders" exists. Please follow: $reset');
+      messages.add('''
       {
         "builders":[
           {
@@ -27,12 +35,37 @@ Future<bool> main(List<String> args) async {
           }
         ]
       }''');
-      return decodedStatus = false;
+      exitWithError(messages);
     }
-  } on FormatException catch (e) {
-    print('error: $e');
-    return decodedStatus = false;
+  } on ExitException catch (error) {
+    error.apply();
   }
-  print('Success.');
-  return decodedStatus;
+  print('$clock ${bold}Analysis successful.$reset');
+}
+
+class ExitException implements Exception {
+  ExitException(this.exitCode);
+
+  final int exitCode;
+
+  void apply() {
+    io_internals.exit(exitCode);
+  }
+}
+
+void exitWithError(List<String> messages) {
+  final String redLine = '$red━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$reset';
+  print(redLine);
+  messages.forEach(print);
+  print(redLine);
+  exit(1);
+}
+
+String get clock {
+  final DateTime now = DateTime.now();
+  return '$reverse▌'
+      '${now.hour.toString().padLeft(2, "0")}:'
+      '${now.minute.toString().padLeft(2, "0")}:'
+      '${now.second.toString().padLeft(2, "0")}'
+      '▐$reset';
 }
---
+++ test_utilities/bin/dev_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright 2020 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Runner for dart tests. It expects a single parameter with the full
+# path to the start folder where tests will be run.
+
+set -e
+
+dir=$(dirname $0)
+
+pushd $dir/../../dev > /dev/null
+dart validate_json.dart cocoon_try_builders.json
+
+popd > /dev/null
+
