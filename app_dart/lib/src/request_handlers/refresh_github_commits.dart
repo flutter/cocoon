edaa98a245cbd10e7780f16550c0c45933ebe094
Keyong Han <keyonghan@google.com>
2020-08-11 18:57:59 -0700

    use taskName

--- app_dart/lib/src/request_handlers/refresh_github_commits.dart
+++ app_dart/lib/src/request_handlers/refresh_github_commits.dart
@@ -203,7 +203,7 @@ class RefreshGithubCommits extends ApiRequestHandler<Body> {
 
     final List<LuciBuilder> prodBuilders = await LuciBuilder.getProdBuilders('flutter', config);
     for (LuciBuilder builder in prodBuilders) {
-      tasks.add(newTask(builder.name, 'chromebot', <String>['can-update-github'], builder.flaky, 0));
+      tasks.add(newTask(builder.taskName, 'chromebot', <String>['can-update-github'], builder.flaky, 0));
     }
 
     final YamlMap yaml = await _loadDevicelabManifest(sha);
