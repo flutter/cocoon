d2e641d15ecdbc8e8abb2bed8b476e1f1dfb8500
Tong Wu <6259181+digiter@users.noreply.github.com>
2020-08-20 12:37:30 -0700

    [app_dart] Fix encoding task entity error.
    Bug: https://github.com/flutter/flutter/issues/64265

--- app_dart/test/model/task_test.dart
+++ app_dart/test/model/task_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:cocoon_service/src/model/appengine/task.dart';
+import 'package:gcloud/db.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -18,8 +19,20 @@ void main() {
       expect(() => Task()..status = 'unknown', throwsArgumentError);
     });
 
+    test('creates a valid chromebot task', () {
+      final DatastoreDB db = DatastoreDB(null);
+      final Key key = db.emptyKey.append(Task, id: 42);
+      final Task t = Task.chromebot(key, 123, 'taskName', false);
+      validateModel(t);
+    });
+
     test('disallows flaky be null', () {
       expect(() => Task.chromebot(null, 123, 'taskName', null), throwsA(isA<AssertionError>()));
     });
   });
+}
+
+void validateModel(Task task) {
+  // Throws an exception when property validation fails.
+  ModelDBImpl().toDatastoreEntity(task);
 }
