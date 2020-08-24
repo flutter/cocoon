409951e6592841b3e496aa9427be09e578e2766f
Keyong Han <keyonghan@google.com>
2020-08-24 15:25:37 -0700

    update attemps for LUCI tasks

--- app_dart/lib/src/request_handlers/refresh_chromebot_status.dart
+++ app_dart/lib/src/request_handlers/refresh_chromebot_status.dart
@@ -89,6 +89,9 @@ class RefreshChromebotStatus extends ApiRequestHandler<Body> {
           update.buildNumberList = buildNumberList;
           update.builderName = builder.name;
           update.luciBucket = 'luci.flutter.prod';
+          if (luciTasks.last.status == Task.statusFailed || luciTasks.last.status == Task.statusSucceeded) {
+            update.attempts += 1;
+          }
           await datastore.insert(<Task>[update]);
         }
       }
