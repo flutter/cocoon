476e5d4e8fa1374389b9989a82acc777658382af
Casey Hillers <caseyhillers@gmail.com>
2020-09-17 18:34:21 -0700

    [app_flutter] Catch google sign in platform exceptions (#944)

--- app_flutter/lib/service/google_authentication.dart
+++ app_flutter/lib/service/google_authentication.dart
@@ -3,9 +3,13 @@
 // found in the LICENSE file.
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 /// Service class for interacting with Google Sign In authentication for Cocoon backend.
+///
+/// Almost all operations with the plugin can throw an exception and should be caught
+/// to prevent this service from crashing.
 class GoogleSignInService extends ChangeNotifier {
   /// Creates a new [GoogleSignIn].
   GoogleSignInService({GoogleSignIn googleSignIn})
@@ -18,8 +22,12 @@ class GoogleSignInService extends ChangeNotifier {
       notifyListeners();
     });
 
-    // TODO(chillers): Re-enable when flakiness is fixed. https://github.com/flutter/flutter/issues/52338
-    // _googleSignIn.signInSilently();
+    try {
+      _googleSignIn.signInSilently();
+    } on PlatformException catch (error) {
+      debugPrint('GoogleSignIn error code: ' + error.code);
+      debugPrint(error.message);
+    }
   }
 
   /// A list of Google API OAuth Scopes this project needs access to.
@@ -38,7 +46,17 @@ class GoogleSignInService extends ChangeNotifier {
   final GoogleSignIn _googleSignIn;
 
   /// Whether or not the application has been signed in to.
-  Future<bool> get isAuthenticated => _googleSignIn.isSignedIn();
+  ///
+  /// If the plugin fails, default to unauthenticated.
+  Future<bool> get isAuthenticated {
+    try {
+      return _googleSignIn.isSignedIn();
+    } on PlatformException catch (error) {
+      debugPrint('GoogleSignIn error code: ' + error.code);
+      debugPrint(error.message);
+    }
+    return Future<bool>.value(false);
+  }
 
   /// The Google Account for the signed in user, null if no user is signed in.
   ///
@@ -62,12 +80,22 @@ class GoogleSignInService extends ChangeNotifier {
 
   /// Initiate the Google Sign In process.
   Future<void> signIn() async {
-    user = await _googleSignIn.signIn();
-    notifyListeners();
+    try {
+      user = await _googleSignIn.signIn();
+      notifyListeners();
+    } on PlatformException catch (error) {
+      debugPrint('GoogleSignIn error code: ' + error.code);
+      debugPrint(error.message);
+    }
   }
 
   Future<void> signOut() async {
-    user = await _googleSignIn.signOut();
-    notifyListeners();
+    try {
+      user = await _googleSignIn.signOut();
+      notifyListeners();
+    } on PlatformException catch (error) {
+      debugPrint('GoogleSignIn error code: ' + error.code);
+      debugPrint(error.message);
+    }
   }
 }
