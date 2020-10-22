cb6d1e5a5cce72a4de454ff7a52bd1dd06711a39
Casey Hillers <chillers@google.com>
2020-10-21 18:50:31 -0700

    Add desktop support to Flutter Dashboard (#976)

---
+++ app_flutter/linux/flutter/generated_plugin_registrant.h
@@ -0,0 +1,17 @@
+// Copyright 2020 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//
+//  Generated file. Do not edit.
+//
+
+#ifndef GENERATED_PLUGIN_REGISTRANT_
+#define GENERATED_PLUGIN_REGISTRANT_
+
+#include <flutter_linux/flutter_linux.h>
+
+// Registers Flutter plugins.
+void fl_register_plugins(FlPluginRegistry* registry);
+
+#endif  // GENERATED_PLUGIN_REGISTRANT_
