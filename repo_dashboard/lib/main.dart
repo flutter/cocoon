eb704adb6853b922bb28b3723a0cc51fe02f8687
Casey Hillers <chillers@google.com>
2020-11-17 13:26:26 -0800

    flutter demo -> flutter dashboard

--- repo_dashboard/lib/main.dart
+++ repo_dashboard/lib/main.dart
@@ -14,7 +14,7 @@ class RepositoryDashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Dashboard',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
