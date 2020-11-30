95a494a1398bf2fbb4df4e0b5b8fc725fa2a16b8
Casey Hillers <chillers@google.com>
2020-11-30 13:51:26 -0800

    fix

--- app/web/benchmarks.dart
+++ app/web/benchmarks.dart
@@ -11,9 +11,9 @@ import 'package:cocoon/benchmark/benchmark_grid.template.dart' as ng;
 
 Future<Null> main() async {
   final http.Client httpClient = BrowserClient();
-  runApp(ng.BenchmarkGridNgFactory, createInjector: ([Injector injector = const Injector.empty()]) {
+  runApp(ng.BenchmarkGridNgFactory, createInjector: ([Injector injector]) {
     return Injector.map({
       http.Client: httpClient,
-    });
+    }, injector);
   });
 }
