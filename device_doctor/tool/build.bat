a6d244103dc42cc3c02fcc92f60b028e80691d21
Keyong Han <keyonghan@google.com>
2020-12-11 10:43:27 -0800

    echo on & update build.sh

--- device_doctor/tool/build.bat
+++ device_doctor/tool/build.bat
@@ -2,7 +2,6 @@
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
-@ECHO off
 REM Checks if cipd command is available.
 FOR /F "tokens=*" %%g IN ('where cipd') do (SET CIPD=%%g)
 IF %ERRORLEVEL% NEQ 0 (
