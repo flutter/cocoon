91f727a0d9a0506e20bd74403dfe2c8c89d264b7
Casey Hillers <chillers@google.com>
2021-02-12 20:06:25 -0800

    documentation

--- app_dart/lib/src/request_handlers/github_rate_limit_status.dart
+++ app_dart/lib/src/request_handlers/github_rate_limit_status.dart
@@ -14,12 +14,19 @@ import '../service/github_service.dart';
 
 /// Endpoint to collect the current GitHub API quota usage of Cocoon on all supported Flutter repos.
 ///
-/// This endpoint pushes data to BigQuery for metric collection to analyze usage over time.
+/// This endpoint pushes data to BigQuery for metric collection to analyze usage over time. There
+/// is a cron job set to run every minute, behind a [CacheRequestHandler] to ensure there exists
+/// at most one entry per repo per minute.
+///
+/// BigQuery entries contain the following fields:
+///   `repo`: Repository corresponding to this entry.
+///   `timestamp`: Timestamp of this entry.
+///   `limit`: Total API calls allowed against `repo`.
+///   `remaining`: Total number of API calls remaining before `repo` is blocked from sending further requests.
+///   `reset`: Timestamp when the API rate limiting will next reset `remaining` back to `limit`.
 @immutable
 class GithubRateLimitStatus extends RequestHandler<Body> {
-  const GithubRateLimitStatus(
-    Config config,
-  ) : super(config: config);
+  const GithubRateLimitStatus(Config config) : super(config: config);
 
   @override
   Future<Body> get() async {
@@ -29,6 +36,7 @@ class GithubRateLimitStatus extends RequestHandler<Body> {
     for (String repository in Config.supportedRepos) {
       final GithubService githubService = await config.createGithubService('flutter', repository);
       final Map<String, dynamic> quotaUsage = (await githubService.getRateLimit()).toJson();
+      quotaUsage['timestamp'] = DateTime.now().millisecondsSinceEpoch;
       quotaUsage['repo'] = repository;
       totalQuotaUsage.add(quotaUsage);
 
