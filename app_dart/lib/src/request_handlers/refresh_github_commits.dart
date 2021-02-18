5f3a8a515e065276407fb71ada6c3146c7a34f6b
Casey Hillers <chillers@google.com>
2021-02-17 17:15:17 -0800

    [app_dart] Create scheduler service (#1087)

--- app_dart/lib/src/request_handlers/refresh_github_commits.dart
+++ app_dart/lib/src/request_handlers/refresh_github_commits.dart
@@ -3,30 +3,24 @@
 // found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
 
-import 'package:cocoon_service/src/service/luci.dart';
 import 'package:gcloud/db.dart';
 import 'package:github/github.dart';
-import 'package:googleapis/bigquery/v2.dart';
 import 'package:meta/meta.dart';
 import 'package:truncate/truncate.dart';
-import 'package:yaml/yaml.dart';
 
 import '../datastore/cocoon_config.dart';
 import '../foundation/providers.dart';
 import '../foundation/typedefs.dart';
 import '../foundation/utils.dart';
 import '../model/appengine/commit.dart';
-import '../model/appengine/task.dart';
 import '../model/devicelab/manifest.dart';
 import '../request_handling/api_request_handler.dart';
 import '../request_handling/authentication.dart';
 import '../request_handling/body.dart';
-import '../request_handling/exceptions.dart';
 import '../service/datastore.dart';
 import '../service/github_service.dart';
+import '../service/scheduler.dart';
 
 /// Queries GitHub for the list of recent commits according to different branches,
 /// and creates corresponding rows in the cloud datastore and the BigQuery for any commits
@@ -39,24 +33,27 @@ class RefreshGithubCommits extends ApiRequestHandler<Body> {
     AuthenticationProvider authenticationProvider, {
     @visibleForTesting this.datastoreProvider = DatastoreService.defaultProvider,
     @visibleForTesting this.httpClientProvider = Providers.freshHttpClient,
-    @visibleForTesting this.branchHttpClientProvider = Providers.freshHttpClient,
     @visibleForTesting this.gitHubBackoffCalculator = twoSecondLinearBackoff,
   })  : assert(datastoreProvider != null),
         assert(httpClientProvider != null),
-        assert(branchHttpClientProvider != null),
-        assert(gitHubBackoffCalculator != null),
         super(config: config, authenticationProvider: authenticationProvider);
 
   final DatastoreServiceProvider datastoreProvider;
-  final HttpClientProvider httpClientProvider;
-  final HttpClientProvider branchHttpClientProvider;
   final GitHubBackoffCalculator gitHubBackoffCalculator;
+  final HttpClientProvider httpClientProvider;
 
   @override
   Future<Body> get() async {
     final RepositorySlug slug = RepositorySlug('flutter', 'flutter');
     final GithubService githubService = await config.createGithubService(slug.owner, slug.name);
     final DatastoreService datastore = datastoreProvider(config.db);
+
+    final Scheduler scheduler = Scheduler(
+        config: config,
+        datastore: datastore,
+        httpClient: httpClientProvider(),
+        gitHubBackoffCalculator: gitHubBackoffCalculator,
+        log: log);
 
     for (String branch in await config.flutterBranches) {
       final List<Commit> lastProcessedCommit = await datastore.queryRecentCommits(limit: 1, branch: branch).toList();
@@ -75,186 +72,31 @@ class RefreshGithubCommits extends ApiRequestHandler<Body> {
         continue;
       }
 
-      final List<Commit> newCommits = await _getNewCommits(commits, datastore, branch);
-
-      if (newCommits.isEmpty) {
-        // Nothing to do.
-        continue;
-      }
-      log.debug('Found ${newCommits.length} new commits for branch $branch on GitHub');
-
-      // Save [Commit] to BigQuery and create [Task] in Datastore.
-      await _saveData(newCommits, datastore);
+      final List<Commit> recentCommits = await _getRecentCommits(commits, datastore, branch);
+      await scheduler.addCommits(recentCommits);
     }
     return Body.empty;
   }
 
-  Future<void> _saveData(
-    List<Commit> newCommits,
-    DatastoreService datastore,
-  ) async {
-    const String projectId = 'flutter-dashboard';
-    const String dataset = 'cocoon';
-    const String table = 'Checklist';
-
-    final TabledataResourceApi tabledataResourceApi = await config.createTabledataResourceApi();
-    final List<Map<String, Object>> tableDataInsertAllRequestRows = <Map<String, Object>>[];
-
-    for (Commit commit in newCommits) {
-      /// Consolidate [commits] together
-      ///
-      /// Prepare for bigquery [insertAll]
-      tableDataInsertAllRequestRows.add(<String, Object>{
-        'json': <String, Object>{
-          'ID': commit.id,
-          'CreateTimestamp': commit.timestamp,
-          'FlutterRepositoryPath': commit.repository,
-          'CommitSha': commit.sha,
-          'CommitAuthorLogin': commit.author,
-          'CommitAuthorAvatarURL': commit.authorAvatarUrl,
-          'CommitMessage': commit.message,
-          'Branch': commit.branch,
-        },
-      });
-
-      final List<Task> tasks = await _createTasks(
-        commitKey: commit.key,
-        sha: commit.sha,
-        createTimestamp: DateTime.now().millisecondsSinceEpoch,
-      );
-
-      try {
-        await datastore.withTransaction<void>((Transaction transaction) async {
-          transaction.queueMutations(inserts: <Commit>[commit]);
-          transaction.queueMutations(inserts: tasks);
-          await transaction.commit();
-          log.debug('Committed ${tasks.length} new tasks for commit ${commit.sha}');
-        });
-      } catch (error) {
-        log.error('Failed to add commit ${commit.sha}: $error');
-      }
-    }
-
-    /// Final [rows] to be inserted to [BigQuery]
-    final TableDataInsertAllRequest rows =
-        TableDataInsertAllRequest.fromJson(<String, Object>{'rows': tableDataInsertAllRequestRows});
-
-    /// Insert [commits] to [BigQuery]
-    try {
-      await tabledataResourceApi.insertAll(rows, projectId, dataset, table);
-    } on ApiRequestError {
-      log.warning('Failed to add commits to BigQuery: $ApiRequestError');
-    }
-  }
-
-  Future<List<Commit>> _getNewCommits(List<RepositoryCommit> commits, DatastoreService datastore, String branch) async {
-    final List<Commit> newCommits = <Commit>[];
+  Future<List<Commit>> _getRecentCommits(
+      List<RepositoryCommit> commits, DatastoreService datastore, String branch) async {
+    final List<Commit> recentCommits = <Commit>[];
     for (RepositoryCommit commit in commits) {
       final String id = 'flutter/flutter/$branch/${commit.sha}';
       final Key<String> key = datastore.db.emptyKey.append<String>(Commit, id: id);
-
-      if (await datastore.db.lookupValue<Commit>(key, orElse: () => null) == null) {
-        newCommits.add(Commit(
-          key: key,
-          timestamp: commit.commit.committer.date.millisecondsSinceEpoch,
-          repository: 'flutter/flutter',
-          sha: commit.sha,
-          author: commit.author.login,
-          authorAvatarUrl: commit.author.avatarUrl,
-          // The field has a size of 1500 we need to ensure the commit message
-          // is at most 1500 chars long.
-          message: truncate(commit.commit.message, 1490, omission: '...'),
-          branch: branch,
-        ));
-      } else {
-        // Once we've found a commit that's already been recorded, we stop looking.
-        break;
-      }
-    }
-    return newCommits;
-  }
-
-  Future<List<Task>> _createTasks({
-    @required Key<String> commitKey,
-    @required String sha,
-    @required int createTimestamp,
-  }) async {
-    Task newTask(
-      String name,
-      String stageName,
-      List<String> requiredCapabilities,
-      bool isFlaky,
-      int timeoutInMinutes,
-    ) {
-      return Task(
-        key: commitKey.append(Task),
-        commitKey: commitKey,
-        createTimestamp: createTimestamp,
-        startTimestamp: 0,
-        endTimestamp: 0,
-        name: name,
-        attempts: 0,
-        isFlaky: isFlaky,
-        timeoutInMinutes: timeoutInMinutes,
-        requiredCapabilities: requiredCapabilities,
-        stageName: stageName,
-        status: Task.statusNew,
-      );
-    }
-
-    final List<Task> tasks = <Task>[];
-    final List<LuciBuilder> prodBuilders = await LuciBuilder.getProdBuilders('flutter', config);
-    for (LuciBuilder builder in prodBuilders) {
-      // These built-in tasks are not listed in the manifest.
-      tasks.add(Task.chromebot(commitKey: commitKey, createTimestamp: createTimestamp, builder: builder));
-    }
-
-    final YamlMap yaml = await _loadDevicelabManifest(sha);
-    final Manifest manifest = Manifest.fromJson(yaml);
-    manifest.tasks.forEach((String taskName, ManifestTask info) {
-      tasks.add(newTask(
-        taskName,
-        info.stage,
-        info.requiredAgentCapabilities,
-        info.isFlaky,
-        info.timeoutInMinutes,
+      recentCommits.add(Commit(
+        key: key,
+        timestamp: commit.commit.committer.date.millisecondsSinceEpoch,
+        repository: 'flutter/flutter',
+        sha: commit.sha,
+        author: commit.author.login,
+        authorAvatarUrl: commit.author.avatarUrl,
+        // The field has a size of 1500 we need to ensure the commit message
+        // is at most 1500 chars long.
+        message: truncate(commit.commit.message, 1490, omission: '...'),
+        branch: branch,
       ));
-    });
-
-    return tasks;
-  }
-
-  Future<YamlMap> _loadDevicelabManifest(String sha) async {
-    final String path = '/flutter/flutter/$sha/dev/devicelab/manifest.yaml';
-    final Uri url = Uri.https('raw.githubusercontent.com', path);
-
-    final HttpClient client = httpClientProvider();
-    try {
-      for (int attempt = 0; attempt < 3; attempt++) {
-        final HttpClientRequest clientRequest = await client.getUrl(url);
-
-        try {
-          final HttpClientResponse clientResponse = await clientRequest.close();
-          final int status = clientResponse.statusCode;
-
-          if (status == HttpStatus.ok) {
-            final String content = await utf8.decoder.bind(clientResponse).join();
-            return loadYaml(content) as YamlMap;
-          } else {
-            log.warning('Attempt to download manifest.yaml failed (HTTP $status)');
-          }
-        } catch (error, stackTrace) {
-          log.error('Attempt to download manifest.yaml failed:\n$error\n$stackTrace');
-        }
-
-        await Future<void>.delayed(gitHubBackoffCalculator(attempt));
-      }
-    } finally {
-      client.close(force: true);
     }
-
-    log.error('GitHub not responding; giving up');
-    response.headers.set(HttpHeaders.retryAfterHeader, '120');
-    throw const HttpStatusException(HttpStatus.serviceUnavailable, 'GitHub not responding');
+    return recentCommits;
   }
 }
---
+++ app_dart/lib/src/service/scheduler.dart
@@ -0,0 +1,221 @@
+// Copyright 2021 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:appengine/appengine.dart';
+import 'package:gcloud/db.dart';
+import 'package:googleapis/bigquery/v2.dart';
+import 'package:meta/meta.dart';
+import 'package:yaml/yaml.dart';
+
+import '../datastore/cocoon_config.dart';
+import '../foundation/utils.dart';
+import '../model/appengine/commit.dart';
+import '../model/appengine/task.dart';
+import '../model/devicelab/manifest.dart';
+import '../request_handling/exceptions.dart';
+import '../service/luci.dart';
+import 'datastore.dart';
+
+/// Scheduler service to validate all commits to supported Flutter repositories.
+///
+/// Scheduler responsibilties include:
+///   1. Tracking commits in Cocoon
+///   2. Ensuring commits are validated (via scheduling tasks against commits)
+///   3. Retry mechanisms for tasks
+class Scheduler {
+  Scheduler({
+    @required this.config,
+    @required this.datastore,
+    this.gitHubBackoffCalculator = twoSecondLinearBackoff,
+    this.httpClient,
+    this.log,
+  })  : assert(datastore != null),
+        assert(gitHubBackoffCalculator != null);
+
+  final Config config;
+  final DatastoreService datastore;
+  final HttpClient httpClient;
+  final GitHubBackoffCalculator gitHubBackoffCalculator;
+  final Logging log;
+
+  /// Ensure [commits] exist in Cocoon.
+  ///
+  /// If [Commit] does not exist in Datastore:
+  ///   * Write it to datastore
+  ///   * Schedule tasks listed in its scheduler config
+  /// Otherwise, ignore it.
+  Future<void> addCommits(List<Commit> commits) async {
+    final List<Commit> newCommits = await _getNewCommits(commits);
+    log.debug('Found ${newCommits.length} new commits on GitHub');
+    for (Commit commit in newCommits) {
+      await _addCommit(commit);
+    }
+  }
+
+  Future<void> _addCommit(Commit commit) async {
+    try {
+      await datastore.withTransaction<void>((Transaction transaction) async {
+        transaction.queueMutations(inserts: <Commit>[commit]);
+        await transaction.commit();
+        log.debug('Committed commit ${commit.sha}');
+      });
+      await _scheduleTasks(commit);
+    } catch (error) {
+      log.error('Failed to add commit ${commit.sha}: $error');
+    }
+
+    await _uploadToBigQuery(commit);
+  }
+
+  /// Return subset of [commits] not stored in Datastore.
+  Future<List<Commit>> _getNewCommits(List<Commit> commits) async {
+    final List<Commit> newCommits = <Commit>[];
+    // Ensure commits are sorted from newest to oldest
+    commits.sort((Commit a, Commit b) => a.timestamp.compareTo(b.timestamp));
+    for (Commit commit in commits) {
+      if (await datastore.db.lookupValue<Commit>(commit.key, orElse: () => null) == null) {
+        newCommits.add(commit);
+      } else {
+        // Once we've found a commit that's already been recorded, we stop looking.
+        break;
+      }
+    }
+
+    // Reverses commits to be in order of oldest to newest.
+    return newCommits;
+  }
+
+  /// Create [Tasks] specified in [commit] scheduler config.
+  Future<List<Task>> _scheduleTasks(Commit commit) async {
+    Task newTask(
+      String name,
+      String stageName,
+      List<String> requiredCapabilities,
+      bool isFlaky,
+      int timeoutInMinutes,
+    ) {
+      return Task(
+        key: commit.key.append(Task),
+        commitKey: commit.key,
+        createTimestamp: commit.timestamp,
+        startTimestamp: 0,
+        endTimestamp: 0,
+        name: name,
+        attempts: 0,
+        isFlaky: isFlaky,
+        timeoutInMinutes: timeoutInMinutes,
+        requiredCapabilities: requiredCapabilities,
+        stageName: stageName,
+        status: Task.statusNew,
+      );
+    }
+
+    final List<Task> tasks = <Task>[];
+    final List<LuciBuilder> prodBuilders = await LuciBuilder.getProdBuilders('flutter', config);
+    for (LuciBuilder builder in prodBuilders) {
+      // These built-in tasks are not listed in the manifest.
+      tasks.add(Task.chromebot(commitKey: commit.key, createTimestamp: commit.timestamp, builder: builder));
+    }
+
+    final YamlMap yaml = await loadDevicelabManifest(commit);
+    final Manifest manifest = Manifest.fromJson(yaml);
+    manifest.tasks.forEach((String taskName, ManifestTask info) {
+      tasks.add(newTask(
+        taskName,
+        info.stage,
+        info.requiredAgentCapabilities,
+        info.isFlaky,
+        info.timeoutInMinutes,
+      ));
+    });
+
+    try {
+      await datastore.withTransaction<void>((Transaction transaction) async {
+        transaction.queueMutations(inserts: tasks);
+        await transaction.commit();
+        log.debug('Committed ${tasks.length} new tasks for commit ${commit.sha}');
+      });
+    } catch (error) {
+      log.error('Failed to commit tasks for ${commit.sha}: $error');
+    }
+
+    return tasks;
+  }
+
+  /// Load in memory the Cocoon Agent DeviceLab scheduler config.
+  ///
+  // TODO(chillers): Remove when DeviceLab has migrated to LUCI. https://github.com/flutter/flutter/projects/151
+  @visibleForTesting
+  Future<YamlMap> loadDevicelabManifest(Commit commit) async {
+    final String path = '/flutter/flutter/${commit.sha}/dev/devicelab/manifest.yaml';
+    final Uri url = Uri.https('raw.githubusercontent.com', path);
+
+    try {
+      for (int attempt = 0; attempt < 3; attempt++) {
+        final HttpClientRequest clientRequest = await httpClient.getUrl(url);
+
+        try {
+          final HttpClientResponse clientResponse = await clientRequest.close();
+          final int status = clientResponse.statusCode;
+
+          if (status == HttpStatus.ok) {
+            final String content = await utf8.decoder.bind(clientResponse).join();
+            return loadYaml(content) as YamlMap;
+          } else {
+            log.warning('Attempt to download manifest.yaml failed (HTTP $status)');
+          }
+        } catch (error, stackTrace) {
+          log.error('Attempt to download manifest.yaml failed:\n$error\n$stackTrace');
+        }
+
+        await Future<void>.delayed(gitHubBackoffCalculator(attempt));
+      }
+    } finally {
+      httpClient.close(force: true);
+    }
+
+    log.error('GitHub not responding; giving up');
+    throw HttpStatusException(HttpStatus.serviceUnavailable, 'Failed to load $path from GitHub');
+  }
+
+  /// Push [Commit] to BigQuery as part of the infra metrics dashboards.
+  Future<void> _uploadToBigQuery(Commit commit) async {
+    const String projectId = 'flutter-dashboard';
+    const String dataset = 'cocoon';
+    const String table = 'Checklist';
+
+    final TabledataResourceApi tabledataResourceApi = await config.createTabledataResourceApi();
+    final List<Map<String, Object>> tableDataInsertAllRequestRows = <Map<String, Object>>[];
+
+    /// Consolidate [commits] together
+    ///
+    /// Prepare for bigquery [insertAll]
+    tableDataInsertAllRequestRows.add(<String, Object>{
+      'json': <String, Object>{
+        'ID': commit.id,
+        'CreateTimestamp': commit.timestamp,
+        'FlutterRepositoryPath': commit.repository,
+        'CommitSha': commit.sha,
+        'CommitAuthorLogin': commit.author,
+        'CommitAuthorAvatarURL': commit.authorAvatarUrl,
+        'CommitMessage': commit.message,
+        'Branch': commit.branch,
+      },
+    });
+
+    /// Final [rows] to be inserted to [BigQuery]
+    final TableDataInsertAllRequest rows =
+        TableDataInsertAllRequest.fromJson(<String, Object>{'rows': tableDataInsertAllRequestRows});
+
+    /// Insert [commits] to [BigQuery]
+    try {
+      await tabledataResourceApi.insertAll(rows, projectId, dataset, table);
+    } on ApiRequestError {
+      log.warning('Failed to add commits to BigQuery: $ApiRequestError');
+    }
+  }
+}
