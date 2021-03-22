0d6a3ff191f33cdbdab2ab0bdce6cbd999e31b0a
Casey Hillers <chillers@google.com>
2021-03-22 11:43:15 -0700

    scheduler proto

--- app_dart/lib/src/model/proto/protos.dart
+++ app_dart/lib/src/model/proto/protos.dart
@@ -7,5 +7,6 @@ export 'internal/build_status_response.pb.dart';
 export 'internal/commit.pb.dart';
 export 'internal/commit_status.pb.dart';
 export 'internal/key.pb.dart';
+export 'internal/scheduler.pb.dart';
 export 'internal/stage.pb.dart';
 export 'internal/task.pb.dart';
