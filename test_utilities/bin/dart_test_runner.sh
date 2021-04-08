fd27e81231d65431fa49684f0960bc72ff88b9fe
Christopher Fujino <christopherfujino@gmail.com>
2021-04-08 11:31:12 -0700

    use dart_test_runner.sh

--- test_utilities/bin/dart_test_runner.sh
+++ test_utilities/bin/dart_test_runner.sh
@@ -8,29 +8,37 @@
 
 set -ex
 
-# Build and analize
+# Build and analyze
 echo "Running tests from $1"
 pushd "$1" > /dev/null
 flutter clean
-pub get
+dart pub get
 
 echo "############# files that require formatting ###########"
-dartfmt -n --line-length=120 --set-exit-if-changed .
+dartfmt --dry-run --line-length=120 --set-exit-if-changed .
 echo "#######################################################"
 
 # agent doesn't use build_runner as of this writing.
 if grep -lq "build_runner" pubspec.yaml; then
   echo "############# build ###########"
-  pub run build_runner build --delete-conflicting-outputs
+  dart run build_runner build --delete-conflicting-outputs
   echo "###############################"
 fi
 
 # See https://github.com/dart-lang/sdk/issues/25551 for why this is necessary.
-pub global run tuneup check
+dart pub global run tuneup check
 # Only try tests if test folder exist.
 if [ -d 'test' ]; then
   echo "############# tests ###########"
-  pub run test --test-randomize-ordering-seed=random --reporter expanded
+  dart test --test-randomize-ordering-seed=random --reporter expanded
+  echo "###############################"
+fi
+
+INTEGRATION_TEST_DIR="$PWD/integration_test/"
+# Only try tests if integration test folder exist.
+if [ -d "$INTEGRATION_TEST_DIR" ]; then
+  echo "###### integration tests ######"
+  dart test --test-randomize-ordering-seed=random --reporter expanded "$INTEGRATION_TEST_DIR"
   echo "###############################"
 fi
 
