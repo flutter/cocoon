5cd0c1705644d0a462ea58ef9527a35c46851e35
Casey Hillers <chillers@google.com>
2021-05-18 11:08:53 -0700

    move dart projects to stable

--- test_utilities/bin/dart_test_runner.sh
+++ test_utilities/bin/dart_test_runner.sh
@@ -12,6 +12,8 @@ set -ex
 echo "Running tests from $1"
 pushd "$1" > /dev/null
 flutter clean
+flutter channel stable
+flutter upgrade
 dart pub get
 
 echo "############# files that require formatting ###########"
