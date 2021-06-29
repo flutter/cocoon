d1c6b687e2e826a44a1ce067f11a9a32cfb93701
Casey Hillers <chillers@google.com>
2021-06-29 11:53:27 -0700

    Add platform properties to .ci.yaml

--- app_dart/lib/src/model/proto/internal/scheduler.pb.dart
+++ app_dart/lib/src/model/proto/internal/scheduler.pb.dart
@@ -13,21 +13,60 @@ import 'scheduler.pbenum.dart';
 
 export 'scheduler.pbenum.dart';
 
+class SchedulerConfig_Properties extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'SchedulerConfig.Properties', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'scheduler'), createEmptyInstance: create)
+    ..m<$core.String, $core.String>(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'properties', entryClassName: 'SchedulerConfig.Properties.PropertiesEntry', keyFieldType: $pb.PbFieldType.OS, valueFieldType: $pb.PbFieldType.OS, packageName: const $pb.PackageName('scheduler'))
+    ..hasRequiredFields = false
+  ;
+
+  SchedulerConfig_Properties._() : super();
+  factory SchedulerConfig_Properties({
+    $core.Map<$core.String, $core.String>? properties,
+  }) {
+    final _result = create();
+    if (properties != null) {
+      _result.properties.addAll(properties);
+    }
+    return _result;
+  }
+  factory SchedulerConfig_Properties.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory SchedulerConfig_Properties.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  SchedulerConfig_Properties clone() => SchedulerConfig_Properties()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  SchedulerConfig_Properties copyWith(void Function(SchedulerConfig_Properties) updates) => super.copyWith((message) => updates(message as SchedulerConfig_Properties)) as SchedulerConfig_Properties; // ignore: deprecated_member_use
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static SchedulerConfig_Properties create() => SchedulerConfig_Properties._();
+  SchedulerConfig_Properties createEmptyInstance() => create();
+  static $pb.PbList<SchedulerConfig_Properties> createRepeated() => $pb.PbList<SchedulerConfig_Properties>();
+  @$core.pragma('dart2js:noInline')
+  static SchedulerConfig_Properties getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<SchedulerConfig_Properties>(create);
+  static SchedulerConfig_Properties? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.Map<$core.String, $core.String> get properties => $_getMap(0);
+}
+
 class SchedulerConfig extends $pb.GeneratedMessage {
-  static final $pb.BuilderInfo _i = $pb.BuilderInfo(
-      const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'SchedulerConfig',
-      package:
-          const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'scheduler'),
-      createEmptyInstance: create)
-    ..pc<Target>(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'targets', $pb.PbFieldType.PM,
-        subBuilder: Target.create)
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'SchedulerConfig', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'scheduler'), createEmptyInstance: create)
+    ..pc<Target>(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'targets', $pb.PbFieldType.PM, subBuilder: Target.create)
     ..pPS(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'enabledBranches')
-    ..hasRequiredFields = false;
+    ..m<$core.String, SchedulerConfig_Properties>(3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'platformProperties', entryClassName: 'SchedulerConfig.PlatformPropertiesEntry', keyFieldType: $pb.PbFieldType.OS, valueFieldType: $pb.PbFieldType.OM, valueCreator: SchedulerConfig_Properties.create, packageName: const $pb.PackageName('scheduler'))
+    ..hasRequiredFields = false
+  ;
 
   SchedulerConfig._() : super();
   factory SchedulerConfig({
     $core.Iterable<Target>? targets,
     $core.Iterable<$core.String>? enabledBranches,
+    $core.Map<$core.String, SchedulerConfig_Properties>? platformProperties,
   }) {
     final _result = create();
     if (targets != null) {
@@ -36,31 +75,30 @@ class SchedulerConfig extends $pb.GeneratedMessage {
     if (enabledBranches != null) {
       _result.enabledBranches.addAll(enabledBranches);
     }
+    if (platformProperties != null) {
+      _result.platformProperties.addAll(platformProperties);
+    }
     return _result;
   }
-  factory SchedulerConfig.fromBuffer($core.List<$core.int> i,
-          [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
-      create()..mergeFromBuffer(i, r);
-  factory SchedulerConfig.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
-      create()..mergeFromJson(i, r);
-  @$core.Deprecated('Using this can add significant overhead to your binary. '
-      'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
-      'Will be removed in next major version')
+  factory SchedulerConfig.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory SchedulerConfig.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
   SchedulerConfig clone() => SchedulerConfig()..mergeFromMessage(this);
-  @$core.Deprecated('Using this can add significant overhead to your binary. '
-      'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
-      'Will be removed in next major version')
-  SchedulerConfig copyWith(void Function(SchedulerConfig) updates) =>
-      super.copyWith((message) => updates(message as SchedulerConfig))
-          as SchedulerConfig; // ignore: deprecated_member_use
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  SchedulerConfig copyWith(void Function(SchedulerConfig) updates) => super.copyWith((message) => updates(message as SchedulerConfig)) as SchedulerConfig; // ignore: deprecated_member_use
   $pb.BuilderInfo get info_ => _i;
   @$core.pragma('dart2js:noInline')
   static SchedulerConfig create() => SchedulerConfig._();
   SchedulerConfig createEmptyInstance() => create();
   static $pb.PbList<SchedulerConfig> createRepeated() => $pb.PbList<SchedulerConfig>();
   @$core.pragma('dart2js:noInline')
-  static SchedulerConfig getDefault() =>
-      _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<SchedulerConfig>(create);
+  static SchedulerConfig getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<SchedulerConfig>(create);
   static SchedulerConfig? _defaultInstance;
 
   @$pb.TagNumber(1)
@@ -68,40 +106,29 @@ class SchedulerConfig extends $pb.GeneratedMessage {
 
   @$pb.TagNumber(2)
   $core.List<$core.String> get enabledBranches => $_getList(1);
+
+  @$pb.TagNumber(3)
+  $core.Map<$core.String, SchedulerConfig_Properties> get platformProperties => $_getMap(2);
 }
 
 class Target extends $pb.GeneratedMessage {
-  static final $pb.BuilderInfo _i = $pb.BuilderInfo(
-      const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'Target',
-      package:
-          const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'scheduler'),
-      createEmptyInstance: create)
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'Target', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'scheduler'), createEmptyInstance: create)
     ..aOS(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'name')
     ..pPS(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'dependencies')
     ..aOB(3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'bringup')
-    ..a<$core.int>(4, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'timeout', $pb.PbFieldType.O3,
-        defaultOrMaker: 30)
-    ..a<$core.String>(5, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'testbed', $pb.PbFieldType.OS,
-        defaultOrMaker: 'linux-vm')
-    ..m<$core.String, $core.String>(6, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'properties',
-        entryClassName: 'Target.PropertiesEntry',
-        keyFieldType: $pb.PbFieldType.OS,
-        valueFieldType: $pb.PbFieldType.OS,
-        packageName: const $pb.PackageName('scheduler'))
+    ..a<$core.int>(4, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'timeout', $pb.PbFieldType.O3, defaultOrMaker: 30)
+    ..a<$core.String>(5, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'testbed', $pb.PbFieldType.OS, defaultOrMaker: 'linux-vm')
+    ..m<$core.String, $core.String>(6, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'properties', entryClassName: 'Target.PropertiesEntry', keyFieldType: $pb.PbFieldType.OS, valueFieldType: $pb.PbFieldType.OS, packageName: const $pb.PackageName('scheduler'))
     ..aOS(7, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'builder')
-    ..e<SchedulerSystem>(
-        8, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'scheduler', $pb.PbFieldType.OE,
-        defaultOrMaker: SchedulerSystem.cocoon, valueOf: SchedulerSystem.valueOf, enumValues: SchedulerSystem.values)
-    ..a<$core.bool>(9, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'presubmit', $pb.PbFieldType.OB,
-        defaultOrMaker: true)
-    ..a<$core.bool>(
-        10, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'postsubmit', $pb.PbFieldType.OB,
-        defaultOrMaker: true)
+    ..e<SchedulerSystem>(8, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'scheduler', $pb.PbFieldType.OE, defaultOrMaker: SchedulerSystem.cocoon, valueOf: SchedulerSystem.valueOf, enumValues: SchedulerSystem.values)
+    ..a<$core.bool>(9, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'presubmit', $pb.PbFieldType.OB, defaultOrMaker: true)
+    ..a<$core.bool>(10, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'postsubmit', $pb.PbFieldType.OB, defaultOrMaker: true)
     ..pPS(11, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'runIf')
     ..pPS(12, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'enabledBranches')
     ..aOS(13, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'recipe')
     ..pPS(14, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'tags')
-    ..hasRequiredFields = false;
+    ..hasRequiredFields = false
+  ;
 
   Target._() : super();
   factory Target({
@@ -165,19 +192,18 @@ class Target extends $pb.GeneratedMessage {
     }
     return _result;
   }
-  factory Target.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
-      create()..mergeFromBuffer(i, r);
-  factory Target.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
-      create()..mergeFromJson(i, r);
-  @$core.Deprecated('Using this can add significant overhead to your binary. '
-      'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
-      'Will be removed in next major version')
+  factory Target.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory Target.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
   Target clone() => Target()..mergeFromMessage(this);
-  @$core.Deprecated('Using this can add significant overhead to your binary. '
-      'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
-      'Will be removed in next major version')
-  Target copyWith(void Function(Target) updates) =>
-      super.copyWith((message) => updates(message as Target)) as Target; // ignore: deprecated_member_use
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  Target copyWith(void Function(Target) updates) => super.copyWith((message) => updates(message as Target)) as Target; // ignore: deprecated_member_use
   $pb.BuilderInfo get info_ => _i;
   @$core.pragma('dart2js:noInline')
   static Target create() => Target._();
@@ -190,10 +216,7 @@ class Target extends $pb.GeneratedMessage {
   @$pb.TagNumber(1)
   $core.String get name => $_getSZ(0);
   @$pb.TagNumber(1)
-  set name($core.String v) {
-    $_setString(0, v);
-  }
-
+  set name($core.String v) { $_setString(0, v); }
   @$pb.TagNumber(1)
   $core.bool hasName() => $_has(0);
   @$pb.TagNumber(1)
@@ -205,10 +228,7 @@ class Target extends $pb.GeneratedMessage {
   @$pb.TagNumber(3)
   $core.bool get bringup => $_getBF(2);
   @$pb.TagNumber(3)
-  set bringup($core.bool v) {
-    $_setBool(2, v);
-  }
-
+  set bringup($core.bool v) { $_setBool(2, v); }
   @$pb.TagNumber(3)
   $core.bool hasBringup() => $_has(2);
   @$pb.TagNumber(3)
@@ -217,10 +237,7 @@ class Target extends $pb.GeneratedMessage {
   @$pb.TagNumber(4)
   $core.int get timeout => $_getI(3, 30);
   @$pb.TagNumber(4)
-  set timeout($core.int v) {
-    $_setSignedInt32(3, v);
-  }
-
+  set timeout($core.int v) { $_setSignedInt32(3, v); }
   @$pb.TagNumber(4)
   $core.bool hasTimeout() => $_has(3);
   @$pb.TagNumber(4)
@@ -229,10 +246,7 @@ class Target extends $pb.GeneratedMessage {
   @$pb.TagNumber(5)
   $core.String get testbed => $_getS(4, 'linux-vm');
   @$pb.TagNumber(5)
-  set testbed($core.String v) {
-    $_setString(4, v);
-  }
-
+  set testbed($core.String v) { $_setString(4, v); }
   @$pb.TagNumber(5)
   $core.bool hasTestbed() => $_has(4);
   @$pb.TagNumber(5)
@@ -244,10 +258,7 @@ class Target extends $pb.GeneratedMessage {
   @$pb.TagNumber(7)
   $core.String get builder => $_getSZ(6);
   @$pb.TagNumber(7)
-  set builder($core.String v) {
-    $_setString(6, v);
-  }
-
+  set builder($core.String v) { $_setString(6, v); }
   @$pb.TagNumber(7)
   $core.bool hasBuilder() => $_has(6);
   @$pb.TagNumber(7)
@@ -256,10 +267,7 @@ class Target extends $pb.GeneratedMessage {
   @$pb.TagNumber(8)
   SchedulerSystem get scheduler => $_getN(7);
   @$pb.TagNumber(8)
-  set scheduler(SchedulerSystem v) {
-    setField(8, v);
-  }
-
+  set scheduler(SchedulerSystem v) { setField(8, v); }
   @$pb.TagNumber(8)
   $core.bool hasScheduler() => $_has(7);
   @$pb.TagNumber(8)
@@ -268,10 +276,7 @@ class Target extends $pb.GeneratedMessage {
   @$pb.TagNumber(9)
   $core.bool get presubmit => $_getB(8, true);
   @$pb.TagNumber(9)
-  set presubmit($core.bool v) {
-    $_setBool(8, v);
-  }
-
+  set presubmit($core.bool v) { $_setBool(8, v); }
   @$pb.TagNumber(9)
   $core.bool hasPresubmit() => $_has(8);
   @$pb.TagNumber(9)
@@ -280,10 +285,7 @@ class Target extends $pb.GeneratedMessage {
   @$pb.TagNumber(10)
   $core.bool get postsubmit => $_getB(9, true);
   @$pb.TagNumber(10)
-  set postsubmit($core.bool v) {
-    $_setBool(9, v);
-  }
-
+  set postsubmit($core.bool v) { $_setBool(9, v); }
   @$pb.TagNumber(10)
   $core.bool hasPostsubmit() => $_has(9);
   @$pb.TagNumber(10)
@@ -298,10 +300,7 @@ class Target extends $pb.GeneratedMessage {
   @$pb.TagNumber(13)
   $core.String get recipe => $_getSZ(12);
   @$pb.TagNumber(13)
-  set recipe($core.String v) {
-    $_setString(12, v);
-  }
-
+  set recipe($core.String v) { $_setString(12, v); }
   @$pb.TagNumber(13)
   $core.bool hasRecipe() => $_has(12);
   @$pb.TagNumber(13)
@@ -310,3 +309,4 @@ class Target extends $pb.GeneratedMessage {
   @$pb.TagNumber(14)
   $core.List<$core.String> get tags => $_getList(13);
 }
+
--- app_dart/lib/src/model/proto/internal/scheduler.pbjson.dart
+++ app_dart/lib/src/model/proto/internal/scheduler.pbjson.dart
@@ -8,7 +8,6 @@
 import 'dart:core' as $core;
 import 'dart:convert' as $convert;
 import 'dart:typed_data' as $typed_data;
-
 @$core.Deprecated('Use schedulerSystemDescriptor instead')
 const SchedulerSystem$json = const {
   '1': 'SchedulerSystem',
@@ -20,20 +19,49 @@ const SchedulerSystem$json = const {
 };
 
 /// Descriptor for `SchedulerSystem`. Decode as a `google.protobuf.EnumDescriptorProto`.
-final $typed_data.Uint8List schedulerSystemDescriptor =
-    $convert.base64Decode('Cg9TY2hlZHVsZXJTeXN0ZW0SCgoGY29jb29uEAESCAoEbHVjaRACEhMKD2dvb2dsZV9pbnRlcm5hbBAD');
+final $typed_data.Uint8List schedulerSystemDescriptor = $convert.base64Decode('Cg9TY2hlZHVsZXJTeXN0ZW0SCgoGY29jb29uEAESCAoEbHVjaRACEhMKD2dvb2dsZV9pbnRlcm5hbBAD');
 @$core.Deprecated('Use schedulerConfigDescriptor instead')
 const SchedulerConfig$json = const {
   '1': 'SchedulerConfig',
   '2': const [
     const {'1': 'targets', '3': 1, '4': 3, '5': 11, '6': '.scheduler.Target', '10': 'targets'},
     const {'1': 'enabled_branches', '3': 2, '4': 3, '5': 9, '10': 'enabledBranches'},
+    const {'1': 'platform_properties', '3': 3, '4': 3, '5': 11, '6': '.scheduler.SchedulerConfig.PlatformPropertiesEntry', '10': 'platformProperties'},
   ],
+  '3': const [SchedulerConfig_PlatformPropertiesEntry$json, SchedulerConfig_Properties$json],
+};
+
+@$core.Deprecated('Use schedulerConfigDescriptor instead')
+const SchedulerConfig_PlatformPropertiesEntry$json = const {
+  '1': 'PlatformPropertiesEntry',
+  '2': const [
+    const {'1': 'key', '3': 1, '4': 1, '5': 9, '10': 'key'},
+    const {'1': 'value', '3': 2, '4': 1, '5': 11, '6': '.scheduler.SchedulerConfig.Properties', '10': 'value'},
+  ],
+  '7': const {'7': true},
+};
+
+@$core.Deprecated('Use schedulerConfigDescriptor instead')
+const SchedulerConfig_Properties$json = const {
+  '1': 'Properties',
+  '2': const [
+    const {'1': 'properties', '3': 1, '4': 3, '5': 11, '6': '.scheduler.SchedulerConfig.Properties.PropertiesEntry', '10': 'properties'},
+  ],
+  '3': const [SchedulerConfig_Properties_PropertiesEntry$json],
+};
+
+@$core.Deprecated('Use schedulerConfigDescriptor instead')
+const SchedulerConfig_Properties_PropertiesEntry$json = const {
+  '1': 'PropertiesEntry',
+  '2': const [
+    const {'1': 'key', '3': 1, '4': 1, '5': 9, '10': 'key'},
+    const {'1': 'value', '3': 2, '4': 1, '5': 9, '10': 'value'},
+  ],
+  '7': const {'7': true},
 };
 
 /// Descriptor for `SchedulerConfig`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List schedulerConfigDescriptor = $convert.base64Decode(
-    'Cg9TY2hlZHVsZXJDb25maWcSKwoHdGFyZ2V0cxgBIAMoCzIRLnNjaGVkdWxlci5UYXJnZXRSB3RhcmdldHMSKQoQZW5hYmxlZF9icmFuY2hlcxgCIAMoCVIPZW5hYmxlZEJyYW5jaGVz');
+final $typed_data.Uint8List schedulerConfigDescriptor = $convert.base64Decode('Cg9TY2hlZHVsZXJDb25maWcSKwoHdGFyZ2V0cxgBIAMoCzIRLnNjaGVkdWxlci5UYXJnZXRSB3RhcmdldHMSKQoQZW5hYmxlZF9icmFuY2hlcxgCIAMoCVIPZW5hYmxlZEJyYW5jaGVzEmMKE3BsYXRmb3JtX3Byb3BlcnRpZXMYAyADKAsyMi5zY2hlZHVsZXIuU2NoZWR1bGVyQ29uZmlnLlBsYXRmb3JtUHJvcGVydGllc0VudHJ5UhJwbGF0Zm9ybVByb3BlcnRpZXMabAoXUGxhdGZvcm1Qcm9wZXJ0aWVzRW50cnkSEAoDa2V5GAEgASgJUgNrZXkSOwoFdmFsdWUYAiABKAsyJS5zY2hlZHVsZXIuU2NoZWR1bGVyQ29uZmlnLlByb3BlcnRpZXNSBXZhbHVlOgI4ARqiAQoKUHJvcGVydGllcxJVCgpwcm9wZXJ0aWVzGAEgAygLMjUuc2NoZWR1bGVyLlNjaGVkdWxlckNvbmZpZy5Qcm9wZXJ0aWVzLlByb3BlcnRpZXNFbnRyeVIKcHJvcGVydGllcxo9Cg9Qcm9wZXJ0aWVzRW50cnkSEAoDa2V5GAEgASgJUgNrZXkSFAoFdmFsdWUYAiABKAlSBXZhbHVlOgI4AQ==');
 @$core.Deprecated('Use targetDescriptor instead')
 const Target$json = const {
   '1': 'Target',
@@ -45,15 +73,7 @@ const Target$json = const {
     const {'1': 'testbed', '3': 5, '4': 1, '5': 9, '7': 'linux-vm', '10': 'testbed'},
     const {'1': 'properties', '3': 6, '4': 3, '5': 11, '6': '.scheduler.Target.PropertiesEntry', '10': 'properties'},
     const {'1': 'builder', '3': 7, '4': 1, '5': 9, '10': 'builder'},
-    const {
-      '1': 'scheduler',
-      '3': 8,
-      '4': 1,
-      '5': 14,
-      '6': '.scheduler.SchedulerSystem',
-      '7': 'cocoon',
-      '10': 'scheduler'
-    },
+    const {'1': 'scheduler', '3': 8, '4': 1, '5': 14, '6': '.scheduler.SchedulerSystem', '7': 'cocoon', '10': 'scheduler'},
     const {'1': 'presubmit', '3': 9, '4': 1, '5': 8, '7': 'true', '10': 'presubmit'},
     const {'1': 'postsubmit', '3': 10, '4': 1, '5': 8, '7': 'true', '10': 'postsubmit'},
     const {'1': 'run_if', '3': 11, '4': 3, '5': 9, '10': 'runIf'},
@@ -75,5 +95,4 @@ const Target_PropertiesEntry$json = const {
 };
 
 /// Descriptor for `Target`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List targetDescriptor = $convert.base64Decode(
-    'CgZUYXJnZXQSEgoEbmFtZRgBIAEoCVIEbmFtZRIiCgxkZXBlbmRlbmNpZXMYAiADKAlSDGRlcGVuZGVuY2llcxIfCgdicmluZ3VwGAMgASgIOgVmYWxzZVIHYnJpbmd1cBIcCgd0aW1lb3V0GAQgASgFOgIzMFIHdGltZW91dBIiCgd0ZXN0YmVkGAUgASgJOghsaW51eC12bVIHdGVzdGJlZBJBCgpwcm9wZXJ0aWVzGAYgAygLMiEuc2NoZWR1bGVyLlRhcmdldC5Qcm9wZXJ0aWVzRW50cnlSCnByb3BlcnRpZXMSGAoHYnVpbGRlchgHIAEoCVIHYnVpbGRlchJACglzY2hlZHVsZXIYCCABKA4yGi5zY2hlZHVsZXIuU2NoZWR1bGVyU3lzdGVtOgZjb2Nvb25SCXNjaGVkdWxlchIiCglwcmVzdWJtaXQYCSABKAg6BHRydWVSCXByZXN1Ym1pdBIkCgpwb3N0c3VibWl0GAogASgIOgR0cnVlUgpwb3N0c3VibWl0EhUKBnJ1bl9pZhgLIAMoCVIFcnVuSWYSKQoQZW5hYmxlZF9icmFuY2hlcxgMIAMoCVIPZW5hYmxlZEJyYW5jaGVzEhYKBnJlY2lwZRgNIAEoCVIGcmVjaXBlEhIKBHRhZ3MYDiADKAlSBHRhZ3MaPQoPUHJvcGVydGllc0VudHJ5EhAKA2tleRgBIAEoCVIDa2V5EhQKBXZhbHVlGAIgASgJUgV2YWx1ZToCOAE=');
+final $typed_data.Uint8List targetDescriptor = $convert.base64Decode('CgZUYXJnZXQSEgoEbmFtZRgBIAEoCVIEbmFtZRIiCgxkZXBlbmRlbmNpZXMYAiADKAlSDGRlcGVuZGVuY2llcxIfCgdicmluZ3VwGAMgASgIOgVmYWxzZVIHYnJpbmd1cBIcCgd0aW1lb3V0GAQgASgFOgIzMFIHdGltZW91dBIiCgd0ZXN0YmVkGAUgASgJOghsaW51eC12bVIHdGVzdGJlZBJBCgpwcm9wZXJ0aWVzGAYgAygLMiEuc2NoZWR1bGVyLlRhcmdldC5Qcm9wZXJ0aWVzRW50cnlSCnByb3BlcnRpZXMSGAoHYnVpbGRlchgHIAEoCVIHYnVpbGRlchJACglzY2hlZHVsZXIYCCABKA4yGi5zY2hlZHVsZXIuU2NoZWR1bGVyU3lzdGVtOgZjb2Nvb25SCXNjaGVkdWxlchIiCglwcmVzdWJtaXQYCSABKAg6BHRydWVSCXByZXN1Ym1pdBIkCgpwb3N0c3VibWl0GAogASgIOgR0cnVlUgpwb3N0c3VibWl0EhUKBnJ1bl9pZhgLIAMoCVIFcnVuSWYSKQoQZW5hYmxlZF9icmFuY2hlcxgMIAMoCVIPZW5hYmxlZEJyYW5jaGVzEhYKBnJlY2lwZRgNIAEoCVIGcmVjaXBlEhIKBHRhZ3MYDiADKAlSBHRhZ3MaPQoPUHJvcGVydGllc0VudHJ5EhAKA2tleRgBIAEoCVIDa2V5EhQKBXZhbHVlGAIgASgJUgV2YWx1ZToCOAE=');
