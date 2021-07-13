0d71951b49ba0ce61e63743eb8f2b6c682a0a21c
Casey Hillers <chillers@google.com>
2021-07-13 11:22:56 -0700

    [ci_yaml] Local jspb generator (#1275)

--- app_dart/bin/generate_jspb.dart
+++ app_dart/bin/generate_jspb.dart
@@ -9,14 +9,8 @@ import 'package:cocoon_service/cocoon_service.dart';
 import 'package:cocoon_service/protos.dart';
 import 'package:yaml/yaml.dart';
 
-Future<void> main(List<String> args) async {
-  if (args.length != 2) {
-    print('generate_jspb.dart \$repo \$sha');
-    exit(1);
-  }
-  final String repo = args.first;
-  final String sha = args[1];
-  final Uri ciYamlUrl = Uri.https('raw.githubusercontent.com', 'flutter/$repo/$sha/.ci.yaml');
+Future<String> getRemoteConfigContent(String repo, String ref) async {
+  final Uri ciYamlUrl = Uri.https('raw.githubusercontent.com', 'flutter/$repo/$ref/.ci.yaml');
   final HttpClient client = HttpClient();
   final HttpClientRequest clientRequest = await client.getUrl(ciYamlUrl);
   final HttpClientResponse clientResponse = await clientRequest.close();
@@ -25,6 +19,26 @@ Future<void> main(List<String> args) async {
   }
   final String configContent = await utf8.decoder.bind(clientResponse).join();
   client.close(force: true);
+  return configContent;
+}
+
+String getLocalConfigContent(String path) {
+  final File configFile = File(path);
+  return configFile.readAsStringSync();
+}
+
+Future<void> main(List<String> args) async {
+  if (args.length != 1 || args.length != 2) {
+    print('generate_jspb.dart \$local_ci_yaml');
+    print('generate_jspb.dart \$repo \$sha');
+    exit(1);
+  }
+  String configContent;
+  if (args.length == 2) {
+    configContent = await getRemoteConfigContent(args[0], args[1]);
+  } else {
+    configContent = getLocalConfigContent(args[0]);
+  }
   final YamlMap configYaml = loadYaml(configContent) as YamlMap;
   // There's an assumption that we're only generating builder configs from commits that
   // have already landed with validation. Otherwise, this will fail.
