c2bf859103293155e14f3e1bd80830ff850d73b2
Keyong Han <keyonghan@google.com>
2021-07-28 11:49:07 -0700

    support test validation

---
+++ app_dart/test/request_handlers/flaky_handler_utiles_test.dart
@@ -0,0 +1,52 @@
+// Copyright 2020 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:cocoon_service/src/request_handlers/flaky_handler_utils.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Gets test ownership', () {
+    String testOwnersContent;
+
+    group('framework host only', () {
+      test('returns correct owner when no mulitple tests share the same file', () async {
+        testOwnersContent = '''
+## Host only framework tests
+# Linux abc
+abc_test.sh @ghi @flutter/jkl
+## Firebase tests
+''';
+        final String owner = getTestOwner('Linux abc', BuilderType.frameworkHostOnly, testOwnersContent);
+        expect(owner, 'ghi');
+      });
+
+      test('returns correct owner when mulitple tests share the same file', () async {
+        testOwnersContent = '''
+## Host only framework tests
+# Linux abc
+# Linu def
+abc_test.sh @ghi @flutter/jkl
+## Firebase tests
+''';
+        final String owner1 = getTestOwner('Linux abc', BuilderType.frameworkHostOnly, testOwnersContent);
+        expect(owner1, 'ghi');
+        // final String owner2 = getTestOwner('Linux def', BuilderType.frameworkHostOnly, testOwnersContent);
+        // expect(owner2, 'ghi');
+      });
+    });
+
+    group('firebaselab only', () {
+      test('returns correct owner', () async {
+        testOwnersContent = '''
+## Firebase tests
+# Linux abc
+/test/abc @def @flutter/ghi
+## Shards tests
+''';
+        final String owner = getTestOwner('Linux firebase_abc', BuilderType.firebaselab, testOwnersContent);
+        expect(owner, 'def');
+      });
+    });
+  });
+}
