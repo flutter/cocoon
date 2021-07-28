7953643fa9c69a40c627bdba136dab472d3dcb2d
Keyong Han <keyonghan@google.com>
2021-07-28 14:59:22 -0700

    update main

--- app_dart/bin/validate_task_ownership.dart
+++ app_dart/bin/validate_task_ownership.dart
@@ -2,39 +2,85 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:cocoon_service/src/request_handlers/flaky_handler_utils.dart';
 import 'package:yaml/yaml.dart';
 
-/// Validates task ownership.
-///
-/// It expects two parameters: the full path to the config file (`.ci.yaml`), and the full path
-/// to the `TESTOWNERS` file.
-///
-/// This supports only framework repository now.
-void main(List<String> args) {
-  final String ciYamlPath = args[0];
-  final String testOwnersPath = args[1];
+/// Remote check based on flutter `repo` and the commit `ref`.
+/// 
+/// This currently support `flutter/flutter` only.
+Future<List<String>> remoteCheck(String repo, String ref) async {
+  final HttpClient client = HttpClient();
+  final Uri ciYamlUrl = Uri.https('raw.githubusercontent.com', 'flutter/$repo/$ref/.ci.yaml');
+  final HttpClientRequest ciYamlRequest = await client.getUrl(ciYamlUrl);
+  final HttpClientResponse ciYamlResponse = await ciYamlRequest.close();
+  final Uri testOwnersUrl = Uri.https('raw.githubusercontent.com', 'flutter/$repo/$ref/TESTOWNERS');
+  final HttpClientRequest testOwnersRequest = await client.getUrl(testOwnersUrl);
+  final HttpClientResponse testOwnersResponse = await testOwnersRequest.close();
+  if (ciYamlResponse.statusCode != HttpStatus.ok) {
+    throw HttpException('HTTP ${ciYamlResponse.statusCode}: $ciYamlUrl');
+  }
+  if (testOwnersResponse.statusCode != HttpStatus.ok) {
+    throw HttpException('HTTP ${testOwnersResponse.statusCode}: $testOwnersUrl');
+  }
+  final String ciYamlContent = await utf8.decoder.bind(ciYamlResponse).join();
+  final String testOwnersContent = await utf8.decoder.bind(testOwnersResponse).join();
+  client.close(force: true);
+
+  final List<String> noOwnerBuilders = validateOwnership(ciYamlContent, testOwnersContent);
+  return noOwnerBuilders;
+}
+
+/// Local check is based on paths to the local `.ci.yaml` and `TESTOWNERS` files.
+List<String> localCheck(String ciYamlPath, String testOwnersPath) {
   final File ciYamlFile = File(ciYamlPath);
   final File testOwnersFile = File(testOwnersPath);
   if (!ciYamlFile.existsSync() || !testOwnersFile.existsSync()) {
     print('validate_task_ownership.dart ciYamlPath testOwnersPath repo');
     exit(1);
   }
+  final List<String> noOwnerBuilders =
+      validateOwnership(ciYamlFile.readAsStringSync(), testOwnersFile.readAsStringSync());
+  return noOwnerBuilders;
+}
+
+/// Validate test ownership defined in `testOwnersContenct` for tests configured in `ciYamlContent`.
+List<String> validateOwnership(String ciYamlContent, String testOwnersContenct) {
   final List<String> noOwnerBuilders = <String>[];
-  final YamlMap ciYaml = loadYaml(ciYamlFile.readAsStringSync()) as YamlMap;
+  final YamlMap ciYaml = loadYaml(ciYamlContent) as YamlMap;
   final YamlList yamlList = ciYaml['targets'] as YamlList;
   final List<YamlMap> targets = yamlList.map<YamlMap>((dynamic target) => target as YamlMap).toList();
   for (final YamlMap target in targets) {
     final String builder = target['builder'] as String;
-    final String owner = getTestOwner(builder, getTypeForBuilder(builder, ciYaml), testOwnersFile.readAsStringSync());
+    final String owner = getTestOwner(builder, getTypeForBuilder(builder, ciYaml), testOwnersContenct);
     print('$builder: $owner');
     if (owner == null) {
       noOwnerBuilders.add(builder);
     }
   }
+  return noOwnerBuilders;
+}
 
+/// Validates task ownership.
+///
+/// It expects two parameters for remote validation: the flutter `repo` and the `commit`.
+///
+/// It expects three parameters for remote validation: `local` arg, the full path to the config
+/// file (`.ci.yaml`), and the full pathto the `TESTOWNERS` file.
+Future<void> main(List<String> args) async {
+  if (args.length != 2 && args.length != 3) {
+    print('validate_task_ownership.dart \$repo \$sha');
+    print('generate_jspb.dart local \$local_ci_yaml \$lcoal_TESTOWNERS');
+    exit(1);
+  }
+  List<String> noOwnerBuilders;
+  if (args.length == 2) {
+    noOwnerBuilders = await remoteCheck(args[0], args[1]);
+  } else {
+    noOwnerBuilders = localCheck(args[1], args[2]);
+  }
   if (noOwnerBuilders.isNotEmpty) {
     print('# Test ownership check failed.');
     print('Builders missing owner: $noOwnerBuilders');
