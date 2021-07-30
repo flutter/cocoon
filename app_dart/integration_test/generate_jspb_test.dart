0860179a803c456214acef5139444113cdebcd6c
Casey Hillers <chillers@google.com>
2021-07-30 11:12:31 -0700

    flutterfmt

--- app_dart/integration_test/generate_jspb_test.dart
+++ app_dart/integration_test/generate_jspb_test.dart
@@ -10,19 +10,19 @@ import 'common.dart';
 
 /// Validates `bin/generate_jspb.dart` which is used in the ci.yaml roller script.
 Future<void> main() async {
-    test('validate cocoon ci.yaml generates jspb', () async {
-      final ProcessResult generateResult = Process.runSync('dart', <String>['run', 'bin/generate_jspb.dart', '../.ci.yaml']);
-      if (generateResult.exitCode != 0) {
-        fail('generate_jspb.dart failed with exit code ${generateResult.exitCode}\n'
-        'stderr: ${generateResult.stderr}\n'
-        'stdout: ${generateResult.stdout}'
-        );
-      }
+  test('validate cocoon ci.yaml generates jspb', () async {
+    final ProcessResult generateResult =
+        Process.runSync('dart', <String>['run', 'bin/generate_jspb.dart', '../.ci.yaml']);
+    if (generateResult.exitCode != 0) {
+      fail('generate_jspb.dart failed with exit code ${generateResult.exitCode}\n'
+          'stderr: ${generateResult.stderr}\n'
+          'stdout: ${generateResult.stdout}');
+    }
 
-      // Update expectations file
-      final File jspbExpectationsFile = File('integration_test/data/cocoon_config.json');
-      jspbExpectationsFile.writeAsStringSync(generateResult.stdout as String);
+    // Update expectations file
+    final File jspbExpectationsFile = File('integration_test/data/cocoon_config.json');
+    jspbExpectationsFile.writeAsStringSync(generateResult.stdout as String);
 
-      expectNoDiff();
-    });
+    expectNoDiff();
+  });
 }
