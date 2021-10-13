3c63116a95f10ae51a858c79ff3516a4f1be88bd
Casey Hillers <chillers@google.com>
2021-10-13 13:35:02 -0700

    Fix cron.yaml deployment path issue

--- cloud_build/deploy.sh
+++ cloud_build/deploy.sh
@@ -9,7 +9,7 @@ cp -r scheduler app_dart/build/
 pushd app_dart > /dev/null
 set -e
 gcloud app deploy --project "$1" --version "version-$2" -q "$3" --no-stop-previous-version
-gcloud app deploy --project "$1" app_dart/cron.yaml
+gcloud app deploy --project "$1" cron.yaml
 gcloud app services set-traffic default --splits version-$2=1 --quiet
 # Google Cloud quota only allows 30 versions.
 # For daily builds, this will keep the 10 most recent versions, but wipe the rest.
