8acf9432bae8135ec91a901bd88233d970f8bf79
Casey Hillers <chillers@google.com>
2021-10-13 14:23:01 -0700

    [cloud_build] Only keep 10 most recent versions (#1394)

--- cloud_build/deploy.sh
+++ cloud_build/deploy.sh
@@ -9,6 +9,9 @@ cp -r scheduler app_dart/build/
 pushd app_dart > /dev/null
 set -e
 gcloud app deploy --project "$1" --version "version-$2" -q "$3" --no-stop-previous-version
-gcloud app deploy --project "$1" app_dart/cron.yaml
+gcloud app deploy --project "$1" cron.yaml
 gcloud app services set-traffic default --splits version-$2=1 --quiet
+# Google Cloud quota only allows 30 versions.
+# For daily builds, this will keep the 10 most recent versions, but wipe the rest.
+gcloud app versions list --format="value(version.id)" --sort-by="~version.createTime" | tail -n +11 | xargs -r gcloud app versions delete --quiet
 popd > /dev/null
