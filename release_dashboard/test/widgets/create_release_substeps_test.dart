b848800b136c069d860f9f4a0a9c5fb3fbd0c08c
Casey Hillers <chillers@google.com>
2021-10-26 10:42:33 -0700

    fix analysis issue

--- release_dashboard/test/widgets/create_release_substeps_test.dart
+++ release_dashboard/test/widgets/create_release_substeps_test.dart
@@ -4,7 +4,6 @@
 
 import 'package:conductor_ui/widgets/create_release_substeps.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
