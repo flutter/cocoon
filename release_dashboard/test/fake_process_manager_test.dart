937f9846bbd3b5d89fdcccc4dfa2d85ae20d0e03
Christopher Fujino <christopherfujino@gmail.com>
2021-10-29 14:10:50 -0700

    add fake_process_manager

---
+++ release_dashboard/test/fake_process_manager_test.dart
@@ -0,0 +1,33 @@
+import 'fake_process_manager.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('FakeProcessManager', () {
+    test('does not throw when expected FakeCommand runs', () {
+      final FakeProcessManager processManager = FakeProcessManager.list(
+        <FakeCommand>[
+          const FakeCommand(command: <String>['rm', '-rf', '/']),
+        ],
+      );
+
+      processManager.runSync(
+        <String>['rm', '-rf', '/'],
+      );
+
+      expect(processManager.hasRemainingExpectations, false);
+    });
+
+    test('throws when unexpected command is run', () {
+      final FakeProcessManager processManager = FakeProcessManager.empty();
+
+      TestFailure? failure;
+      // Note expect(() => cb(), throwsA(TestFailure)) does not work
+      try {
+        processManager.runSync(<String>['rm', '-rf', '/']);
+      } on TestFailure catch (e) {
+        failure = e;
+      }
+      expect(failure != null, true);
+    });
+  });
+}
