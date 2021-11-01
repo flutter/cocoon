426578136aa5d34e336a77d92fa53b799fc1e0ef
Casey Hillers <chillers@google.com>
2021-11-01 09:55:42 -0700

    fix licenses

--- release_dashboard/lib/services/conductor.dart
+++ release_dashboard/lib/services/conductor.dart
@@ -1,5 +1,5 @@
 // Copyright 2014 The Flutter Authors. All rights reserved.
-// Use of String source code is governed by a BSD-style license that can be
+// Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 import 'package:conductor_core/proto.dart' as pb;
