e762f0844c3e767f66254ac609d9046a31a48b45
yusuf-goog <91688203+yusuf-goog@users.noreply.github.com>
2021-11-01 15:28:01 -0700

    Update provision_salt.sh (#1433)

--- dev/provision_salt.sh
+++ dev/provision_salt.sh
@@ -12,14 +12,14 @@ MINION_PLIST_PATH=/Library/LaunchDaemons/com.saltstack.salt.minion.plist
 # Pins the version to 2019.2.0 and Python 2 to be compatible with Fuchsia salt master.
 function install_salt() {
   if [[ "$(uname)" == 'Linux' ]]; then
-    wget -O - https://repo.saltstack.com/py3/debian/10/amd64/3002/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-    echo 'deb http://repo.saltstack.com/py3/debian/10/amd64/3002 buster main' | sudo tee /etc/apt/sources.list.d/saltstack.list
+    wget -O - https://repo.saltproject.io/py3/debian/10/amd64/3002/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+    echo 'deb http://repo.saltproject.io/py3/debian/10/amd64/3002 buster main' | sudo tee /etc/apt/sources.list.d/saltstack.list
     # Also provision debian backports for m2crypto
     echo 'deb http://deb.debian.org/debian buster-backports main' | sudo tee /etc/apt/sources.list.d/backports.list
     sudo apt update
     sudo apt install salt-minion
   elif [[ "$(uname)" == 'Darwin' ]]; then
-    curl https://repo.saltstack.com/osx/salt-3002.1-py3-x86_64.pkg -o /tmp/salt.pkg
+    curl https://repo.saltproject.io/osx/salt-3002.1-py3-x86_64.pkg -o /tmp/salt.pkg
     sudo installer -pkg /tmp/salt.pkg -target /
   fi
 }
