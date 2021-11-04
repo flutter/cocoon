3a1c5fb0f91338b433e3ed58622dd93c4d6934ff
Alex Chen <alexcn@google.com>
2021-11-04 11:55:13 -0400

    made classes

--- release_dashboard/lib/logic/git.dart
+++ release_dashboard/lib/logic/git.dart
@@ -47,3 +47,63 @@ class git {
     }
   }
 }
+
+abstract class gitValidation {
+  /// Returns the regex needed to validate this type of input.
+  RegExp get regex;
+
+  /// Returns the error message if the regex validation fails.
+  String get errorMsg;
+
+  /// Uses [regex] to validate the input provided. Returns true if it is valid, false otherwise.
+  bool validate(String? input);
+}
+
+/// Provides all the tools and methods to validate a single Git hash such as the dart revision.
+class gitHash extends gitValidation {
+  // Allows empty string because the dart revision which is the only input using this regex could be empty
+  final RegExp gitHashRegex = RegExp(r'^$|^[0-9a-f]{40}$');
+  final String gitHashErrorMsg = 'Must be 40 alphanumeric characters or an empty string.';
+
+  RegExp get regex => gitHashRegex;
+
+  String get errorMsg => gitHashErrorMsg;
+
+  bool validate(String? input) {
+    return this.regex.hasMatch(input == null ? '' : input.trim()); // removes leading or trailing whitespaces
+  }
+}
+
+/// Provides all the tools and methods to validate a multiple Git hash entry such as cherrypicks.
+class multiGitHash extends gitValidation {
+  /// Supports multiple git hashes delimited by a comma or an empty string.
+  ///
+  /// valid: c7714158950347,c7714158950347,c7714158950347
+  /// valid: c7714158950347
+  /// invalid:   c7714158950347,@@cccccc
+  /// invalid (cannot end with a comma):   c7714158950347,c7714158950347,
+  final RegExp multiGitHashRegex = RegExp(r'^$|^[0-9a-z]{40}(?:,[0-9a-z]{40})*$');
+  final String multiGitHashErrorMsg =
+      'Must be one or more groups of 40 alphanumeric characters delimited by a comma or an empty string.';
+
+  RegExp get regex => multiGitHashRegex;
+
+  String get errorMsg => multiGitHashErrorMsg;
+
+  bool validate(String? input) {
+    return this.regex.hasMatch(input == null ? '' : input.replaceAll(' ', '')); // removes any whitespaces
+  }
+}
+
+/// Provides all the tools and methods to validate a single Git branch such as the candidate branch.
+class gitBranch extends gitValidation {
+  final String githubRemoteErrorMsg = "Must be a valid Github remote string, e.g. 'git@github.com:user/flutter.git'";
+
+  RegExp get regex => githubRemotePattern;
+
+  String get errorMsg => githubRemoteErrorMsg;
+
+  bool validate(String? input) {
+    return this.regex.hasMatch(input == null ? '' : input.trim()); // removes leading or trailing whitespaces
+  }
+}
