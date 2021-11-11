87c1638124f5bdfaf2ddea0a0036352f363f78a3
Alex Chen <alexcn@google.com>
2021-11-11 15:43:13 -0500

    small import bug

--- release_dashboard/lib/widgets/progression.dart
+++ release_dashboard/lib/widgets/progression.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:conductor_core/proto.dart' as pb;
 import 'package:flutter/material.dart';
 
 import '../services/conductor.dart';
