90fda5148f3b5636577a9558ccb64d05634743c8
Alex Chen <alexcn@google.com>
2021-11-11 20:28:02 -0500

    fixed stepper test

--- release_dashboard/test/widgets/stepper_test.dart
+++ release_dashboard/test/widgets/stepper_test.dart
@@ -2,29 +2,36 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:conductor_ui/state/status_state.dart';
 import 'package:conductor_ui/widgets/progression.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 
 import '../fakes/services/fake_conductor.dart';
 
 void main() {
   testWidgets('When user clicks on a previously completed step, Stepper does not navigate back.',
       (WidgetTester tester) async {
-    await tester.pumpWidget(
-      MaterialApp(
-        home: Builder(builder: (context) {
-          return Column(
-            children: <Widget>[
-              MainProgression(
-                previousCompletedStep: 1,
-                conductor: FakeConductor(),
-              ),
-            ],
-          );
-        }),
-      ),
-    );
+    await tester.pumpWidget(Builder(builder: (context) {
+      return ChangeNotifierProvider(
+        create: (context) => StatusState(conductor: FakeConductor()),
+        child: MaterialApp(
+          home: Material(
+            child: Column(
+              children: <Widget>[
+                Builder(builder: (context) {
+                  return MainProgression(
+                    previousCompletedStep: 1,
+                    conductor: FakeConductor(),
+                  );
+                }),
+              ],
+            ),
+          ),
+        ),
+      );
+    }));
 
     expect(tester.widget<Stepper>(find.byType(Stepper)).currentStep, equals(1));
 
