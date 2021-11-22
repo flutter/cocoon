13ccc544554ef45dbabb0e22330246441ff0e273
Alex Chen <alexcn@google.com>
2021-11-22 12:17:58 -0500

    added dart docs to enums

--- release_dashboard/lib/enums/cherrypick.dart
+++ release_dashboard/lib/enums/cherrypick.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/// Keys for engine/framework cherrypicks which are in [Map<K, V>] format.
 enum cherrypick {
   trunkRevision,
   state,
--- release_dashboard/lib/enums/conductor_status.dart
+++ release_dashboard/lib/enums/conductor_status.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/// Keys for the release status which is in [Map<K, V>] format.
 enum conductorStatus {
   conductorVersion,
   releaseChannel,
