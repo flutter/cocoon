944170377fa2417f1b7b7a216fdd522a3b14dd78
Alex Chen <y862chen@uwaterloo.ca>
2021-11-29 14:00:17 -0500

    [release_dashboard] Releae Complete Step UI

---
+++ release_dashboard/test/widgets/release_completed_test.dart
@@ -0,0 +1,42 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:conductor_core/proto.dart' as pb;
+import 'package:conductor_ui/state/status_state.dart';
+import 'package:conductor_ui/widgets/release_completed.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+import '../src/services/fake_conductor.dart';
+import '../src/test_state_generator.dart';
+
+void main() {
+  late pb.ConductorState stateWithoutConflicts;
+
+  setUp(() {
+    stateWithoutConflicts = generateConductorState();
+  });
+  testWidgets('Render all elements correctly', (WidgetTester tester) async {
+    await tester.pumpWidget(ChangeNotifierProvider(
+      create: (context) => StatusState(conductor: FakeConductor(testState: stateWithoutConflicts)),
+      child: MaterialApp(
+        home: Material(
+          child: Column(
+            children: <Widget>[
+              Builder(builder: (context) {
+                return const ReleaseCompleted();
+              }),
+            ],
+          ),
+        ),
+      ),
+    ));
+
+    expect(find.textContaining('Congratulations!'), findsOneWidget);
+    expect(find.textContaining('is successfully released'), findsOneWidget);
+    expect(find.textContaining(kReleaseChannel), findsOneWidget);
+    expect(find.textContaining(kReleaseVersion), findsOneWidget);
+  });
+}
