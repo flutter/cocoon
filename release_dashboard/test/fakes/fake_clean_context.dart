dd87c3df284c95823205cc9db0a497a89d25b47e
Alex Chen <y862chen@uwaterloo.ca>
2021-12-01 15:37:28 -0500

    removed memory from FakeCleanContext

--- release_dashboard/test/fakes/fake_clean_context.dart
+++ release_dashboard/test/fakes/fake_clean_context.dart
@@ -4,39 +4,28 @@
 
 import 'package:conductor_core/conductor_core.dart';
 import 'package:file/file.dart';
-import 'package:file/memory.dart';
 
 /// Initializes a fake [CleanContext] in a fake local environment for testing.
 ///
 /// [runOverride] parameter overrides the parent's [run] method if it is not null. Else the parent's
 /// [run] method is used.
-class FakeCleanContext extends CleanContext {
-  factory FakeCleanContext({
-    File? stateFile,
-    Future<void> Function()? runOverride,
-  }) {
-    final FileSystem fileSystem = MemoryFileSystem.test();
-    stateFile ??= fileSystem.file(kStateFileName);
-    return FakeCleanContext._(
-      stateFile: stateFile,
-      runOverride: runOverride,
-    );
-  }
-
-  FakeCleanContext._({
-    required File stateFile,
+class FakeCleanContext implements CleanContext {
+  FakeCleanContext({
     this.runOverride,
-  }) : super(stateFile: stateFile);
+  });
 
   /// An optional override async callback for the real [run] method.
   Future<void> Function()? runOverride;
 
-  /// Call the [runOverride] parameter if it is not null, else call the parent's [run] method.
+  /// Call the [runOverride] parameter if it is not null, else return a future that doesn't throw any error.
   @override
   Future<void> run() {
     if (runOverride != null) {
       return runOverride!();
     }
-    return super.run();
+    return Future.value();
   }
+
+  @override
+  File get stateFile => throw UnimplementedError();
 }
