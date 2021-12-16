13ffa08affe5a45f20bdf3b56bb695ffcb78a8ba
Casey Hillers <chillers@google.com>
2021-12-16 09:04:01 -0800

    [ci.yaml] Make generate_jspb compilable (#1528)

---
+++ app_dart/lib/ci_yaml.dart
@@ -0,0 +1,7 @@
+// Copyright 2019 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+export 'src/model/ci_yaml/ci_yaml.dart';
+export 'src/model/ci_yaml/target.dart';
+export 'src/service/scheduler/graph.dart';
