aa1eb26d950803eb1fb3dbbde947075b124cb109
Casey Hillers <chillers@google.com>
2022-02-07 08:14:45 -0800

    Initial project creation

---
+++ auto_submit/README.md
@@ -0,0 +1,3 @@
+# Autosubmit
+
+RFC is available in https://flutter.dev/go/autosubmit
---
+++ auto_submit/bin/server.dart
@@ -0,0 +1,61 @@
+// Copyright 2021 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:convert';
+
+import 'package:auto_submit/helpers.dart';
+import 'package:googleapis/firestore/v1.dart';
+import 'package:googleapis_auth/auth_io.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+Future main() async {
+  final projectId = await currentProjectId();
+  print('Current GCP project id: $projectId');
+
+  final authClient = await clientViaApplicationDefaultCredentials(
+    scopes: [FirestoreApi.datastoreScope],
+  );
+
+  try {
+    final api = FirestoreApi(authClient);
+
+    Future<Response> incrementHandler(Request request) async {
+      final result = await api.projects.databases.documents.commit(
+        _incrementRequest(projectId),
+        'projects/$projectId/databases/(default)',
+      );
+
+      return Response.ok(
+        JsonUtf8Encoder(' ').convert(result),
+        headers: {
+          'content-type': 'application/json',
+        },
+      );
+    }
+
+    final router = Router()..get('/', incrementHandler);
+
+    await serveHandler(router);
+  } finally {
+    authClient.close();
+  }
+}
+
+CommitRequest _incrementRequest(String projectId) => CommitRequest(
+      writes: [
+        Write(
+          transform: DocumentTransform(
+            document:
+                'projects/$projectId/databases/(default)/documents/settings/count',
+            fieldTransforms: [
+              FieldTransform(
+                fieldPath: 'count',
+                increment: Value(integerValue: '1'),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
