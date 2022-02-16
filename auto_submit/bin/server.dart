8346b56a223f653bd25b077d96442db39a0cd5bf
Casey Hillers <chillers@google.com>
2022-02-16 15:05:31 -0800

    Remove firestore logic

--- auto_submit/bin/server.dart
+++ auto_submit/bin/server.dart
@@ -5,56 +5,20 @@
 import 'dart:convert';
 
 import 'package:auto_submit/helpers.dart';
-import 'package:googleapis/firestore/v1.dart';
-import 'package:googleapis_auth/auth_io.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 Future main() async {
-  final projectId = await currentProjectId();
-  print('Current GCP project id: $projectId');
-
-  final authClient = await clientViaApplicationDefaultCredentials(
-    scopes: [FirestoreApi.datastoreScope],
-  );
-
-  try {
-    final api = FirestoreApi(authClient);
-
-    Future<Response> incrementHandler(Request request) async {
-      final result = await api.projects.databases.documents.commit(
-        _incrementRequest(projectId),
-        'projects/$projectId/databases/(default)',
-      );
-
-      return Response.ok(
-        JsonUtf8Encoder(' ').convert(result),
-        headers: {
-          'content-type': 'application/json',
-        },
-      );
-    }
-
-    final router = Router()..get('/', incrementHandler);
-
-    await serveHandler(router);
-  } finally {
-    authClient.close();
-  }
-}
-
-CommitRequest _incrementRequest(String projectId) => CommitRequest(
-      writes: [
-        Write(
-          transform: DocumentTransform(
-            document: 'projects/$projectId/databases/(default)/documents/settings/count',
-            fieldTransforms: [
-              FieldTransform(
-                fieldPath: 'count',
-                increment: Value(integerValue: '1'),
-              )
-            ],
-          ),
-        ),
-      ],
+  Future<Response> emptyHandler(Request request) async {
+    return Response.ok(
+      jsonEncode(<String, String>{}),
+      headers: {
+        'content-type': 'application/json',
+      },
     );
+  }
+
+  final router = Router()..get('/', emptyHandler);
+
+  await serveHandler(router);
+}
