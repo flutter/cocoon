dd28a626a5c1df517c8a1edbcbbaaf9aea479fd2
Casey Hillers <chillers@google.com>
2022-02-16 15:35:02 -0800

    [scheduler] Update task from push message (#1605)

--- app_dart/test/model/task_test.dart
+++ app_dart/test/model/task_test.dart
@@ -4,6 +4,7 @@
 
 import 'package:cocoon_service/src/model/appengine/commit.dart';
 import 'package:cocoon_service/src/model/appengine/task.dart';
+import 'package:cocoon_service/src/model/luci/push_message.dart';
 import 'package:cocoon_service/src/service/luci.dart';
 import 'package:gcloud/db.dart';
 import 'package:test/test.dart';
@@ -54,6 +55,100 @@ void main() {
         flaky: null,
       );
       expect(Task.chromebot(commitKey: commitKey, createTimestamp: 123, builder: builder).isFlaky, isFalse);
+    });
+
+    group('updateFromBuild', () {
+      test('updates if buildNumber is null', () {
+        final DateTime created = DateTime.utc(2022, 1, 11, 1, 1);
+        final DateTime started = DateTime.utc(2022, 1, 11, 1, 2);
+        final DateTime completed = DateTime.utc(2022, 1, 11, 1, 3);
+        final Build build = generatePushMessageBuild(
+          1,
+          createdTimestamp: created,
+          startedTimestamp: started,
+          completedTimestamp: completed,
+        );
+        final Task task = generateTask(1);
+
+        expect(task.status, Task.statusNew);
+        expect(task.buildNumberList, isNull);
+        expect(task.buildNumber, isNull);
+        expect(task.endTimestamp, 0);
+        expect(task.createTimestamp, 0);
+        expect(task.startTimestamp, 0);
+
+        task.updateFromBuild(build);
+
+        expect(task.status, Task.statusSucceeded);
+        expect(task.buildNumber, 1);
+        expect(task.buildNumberList, '1');
+        expect(task.createTimestamp, created.millisecondsSinceEpoch);
+        expect(task.startTimestamp, started.millisecondsSinceEpoch);
+        expect(task.endTimestamp, completed.millisecondsSinceEpoch);
+      });
+
+      test('updates if buildNumber is prior to pushMessage', () {
+        final Build build = generatePushMessageBuild(
+          1,
+          buildNumber: 2,
+          status: Status.started,
+        );
+        final Task task = generateTask(
+          1,
+          buildNumber: 1,
+          status: Task.statusSucceeded,
+        );
+
+        expect(task.buildNumberList, '1');
+        expect(task.status, Task.statusSucceeded);
+
+        task.updateFromBuild(build);
+
+        expect(task.buildNumber, 2);
+        expect(task.buildNumberList, '1,2');
+        expect(task.status, Task.statusInProgress);
+      });
+
+      test('does not update status if older status', () {
+        final Build build = generatePushMessageBuild(
+          1,
+          status: Status.started,
+        );
+        final Task task = generateTask(
+          1,
+          buildNumber: 1,
+          status: Task.statusSucceeded,
+        );
+
+        expect(task.buildNumber, 1);
+        expect(task.status, Task.statusSucceeded);
+
+        task.updateFromBuild(build);
+
+        expect(task.buildNumber, 1);
+        expect(task.status, Task.statusSucceeded);
+      });
+
+      test('does not update if build is older than task', () {
+        final Build build = generatePushMessageBuild(
+          1,
+          status: Status.completed,
+          result: Result.success,
+        );
+        final Task task = generateTask(
+          1,
+          buildNumber: 2,
+          status: Task.statusNew,
+        );
+
+        expect(task.buildNumber, 2);
+        expect(task.status, Task.statusNew);
+
+        task.updateFromBuild(build);
+
+        expect(task.buildNumber, 2);
+        expect(task.status, Task.statusNew);
+      });
     });
   });
 }
