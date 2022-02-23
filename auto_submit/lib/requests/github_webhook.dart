0b33655f19a91d113784f4ff03edc0d4680c6dec
Kristin Bi <ruolinbi@google.com>
2022-02-23 15:56:40 -0800

    check autsubmit label

--- auto_submit/lib/requests/github_webhook.dart
+++ auto_submit/lib/requests/github_webhook.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:shelf/shelf.dart';
 import 'package:auto_submit/service/log.dart';
@@ -17,7 +18,26 @@ class GithubWebhook {
   Future<Response> post(Request request) async {
     final Map<String, String> reqHeader = request.headers;
     logger.info('Header: $reqHeader');
+
+    //Listen to the pull request with 'autosubmit' label.
+    bool hasAutosubmit = false;
     final String rawBody = await request.readAsString();
+    final body = json.decode(rawBody) as Map<String, dynamic>;
+    List<Map<String, dynamic>> labels = <Map<String, dynamic>>[];
+    if (body.containsKey('pull_request') && body['pull_request'].containsKey('labels')) {
+      labels = List<Map<String, dynamic>>.from(body['pull_request']['labels']);
+    }
+
+    for (int i = 0; i < labels.length; i++) {
+      if (labels[i]['name'] == 'autosubmit') {
+        hasAutosubmit = true;
+      }
+    }
+
+    if (hasAutosubmit) {
+      //Creat a github client and use rest API to get this single pr
+      //to check shouldMerge later.
+    }
 
     return Response.ok(
       rawBody,
--- auto_submit/test/requests/github_webhook_test.dart
+++ auto_submit/test/requests/github_webhook_test.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:test/test.dart';
 import 'package:auto_submit/requests/github_webhook.dart';
@@ -12,18 +14,77 @@ void main() {
     late GithubWebhook githubWebhook;
 
     setUp(() {
-      req = Request('POST', Uri.parse('http://localhost/'),
-          headers: {
-            'header1': 'header value1',
+      req = Request('POST', Uri.parse('http://localhost/'), headers: {
+        'header1': 'header value1',
+      }, body: '''{
+      "action": "open",
+      "number": 1598,
+      "pull_request": {
+        "url": "https://api.github.com/repos/flutter/pulls/123",
+        "id": 294034,
+        "title": "Defer reassemble until reload is finished",
+        "user": {
+          "login": "octocat",
+          "id": 862741
+        },
+        "labels": [
+          {
+            "id": 487496476,
+            "node_id": "MDU6TGFiZWw0ODc0OTY0NzY=",
+            "url": "https://api.github.com/repos/flutter/labels/cla:%20yes",
+            "name": "cla: yes",
+            "color": "ffffff",
+            "default": false
           },
-          body: "{\"label1\": \"label1cotent\"}");
+          {
+            "id": 284437560,
+            "node_id": "MDU6TGFiZWwyODQ0Mzc1NjA=",
+            "url": "https://api.github.com/repos/flutter/labels/autosubmit",
+            "name": "autosubmit",
+            "color": "207de5",
+            "default": false
+          }
+        ]
+      },
+      "repository": {
+        "id": 1868532,
+        "name": "flutter",
+        "full_name": "flutter/cocoon",
+        "private": false,
+        "owner": {
+          "login": "flutter",
+          "id": 21031067
+        },
+        "html_url": "https://github.com/cocooon/octocat",
+        "watchers": 0
+      }
+    }''');
       githubWebhook = GithubWebhook();
     });
 
     test('call handler to handle the post request', () async {
       Response response = await githubWebhook.post(req);
       final String resBody = await response.readAsString();
-      expect(resBody, "{\"label1\": \"label1cotent\"}");
+      final body = json.decode(resBody) as Map<String, dynamic>;
+      List<Map<String, dynamic>> labels = List<Map<String, dynamic>>.from(body['pull_request']['labels']);
+      expect(labels, [
+        {
+          'id': 487496476,
+          'node_id': 'MDU6TGFiZWw0ODc0OTY0NzY=',
+          'url': 'https://api.github.com/repos/flutter/labels/cla:%20yes',
+          'name': 'cla: yes',
+          'color': 'ffffff',
+          'default': false
+        },
+        {
+          'id': 284437560,
+          'node_id': 'MDU6TGFiZWwyODQ0Mzc1NjA=',
+          'url': 'https://api.github.com/repos/flutter/labels/autosubmit',
+          'name': 'autosubmit',
+          'color': '207de5',
+          'default': false
+        }
+      ]);
     });
   });
 }
