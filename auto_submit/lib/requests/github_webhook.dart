7958004c948625f99d12057b0549bac6ca65640b
KristinBi <ruolinbi@google.com>
2022-02-25 16:21:01 -0800

    Listen to 'autosubmit' label (#1619)

--- auto_submit/lib/requests/github_webhook.dart
+++ auto_submit/lib/requests/github_webhook.dart
@@ -3,9 +3,11 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:convert';
 
-import 'package:shelf/shelf.dart';
 import 'package:auto_submit/service/log.dart';
+import 'package:github/github.dart';
+import 'package:shelf/shelf.dart';
 
 /// Handler for processing GitHub webhooks.
 ///
@@ -17,7 +19,22 @@ class GithubWebhook {
   Future<Response> post(Request request) async {
     final Map<String, String> reqHeader = request.headers;
     logger.info('Header: $reqHeader');
+
+    // Listen to the pull request with 'autosubmit' label.
+    bool hasAutosubmit = false;
     final String rawBody = await request.readAsString();
+    final body = json.decode(rawBody) as Map<String, dynamic>;
+
+    if (!body.containsKey('pull_request') || !body['pull_request'].containsKey('labels')) {
+      return Response.ok(jsonEncode(<String, String>{}));
+    }
+
+    PullRequest pullRequest = PullRequest.fromJson(body['pull_request']);
+    hasAutosubmit = pullRequest.labels!.any((label) => label.name == 'autosubmit');
+
+    if (hasAutosubmit) {
+      // TODO(kristinbi): Check if PR can be submitted. https://github.com/flutter/flutter/issues/98707
+    }
 
     return Response.ok(
       rawBody,
--- auto_submit/test/requests/github_webhook_test.dart
+++ auto_submit/test/requests/github_webhook_test.dart
@@ -2,9 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:convert';
+
+import 'package:auto_submit/requests/github_webhook.dart';
+import 'package:github/github.dart';
 import 'package:shelf/shelf.dart';
 import 'package:test/test.dart';
-import 'package:auto_submit/requests/github_webhook.dart';
+
+import './github_webhook_test_data.dart';
 
 void main() {
   group('Check Webhook', () {
@@ -16,14 +21,17 @@ void main() {
           headers: {
             'header1': 'header value1',
           },
-          body: "{\"label1\": \"label1cotent\"}");
+          body: webhookEventMock);
       githubWebhook = GithubWebhook();
     });
 
     test('call handler to handle the post request', () async {
       Response response = await githubWebhook.post(req);
       final String resBody = await response.readAsString();
-      expect(resBody, "{\"label1\": \"label1cotent\"}");
+      final body = json.decode(resBody) as Map<String, dynamic>;
+      List<IssueLabel>? labels = PullRequest.fromJson(body['pull_request']).labels;
+      expect(labels![0].name, 'cla: yes');
+      expect(labels[1].name, 'autosubmit');
     });
   });
 }
