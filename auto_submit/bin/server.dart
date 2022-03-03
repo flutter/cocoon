eaee71c0ec3123d7889abcd7de34108920efa97b
Casey Hillers <chillers@google.com>
2022-03-03 11:39:00 -0800

    [autosubmit] Add GitHub app authentication

--- auto_submit/bin/server.dart
+++ auto_submit/bin/server.dart
@@ -7,13 +7,25 @@ import 'dart:async';
 import 'package:appengine/appengine.dart';
 import 'package:auto_submit/helpers.dart';
 import 'package:auto_submit/requests/github_webhook.dart';
+import 'package:auto_submit/service/config.dart';
+import 'package:neat_cache/neat_cache.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 Future<void> main() async {
   await withAppEngineServices(() async {
     useLoggingPackageAdaptor();
 
-    final Router router = Router()..post('/webhook', GithubWebhook().post);
+    final cache = Cache.inMemoryCacheProvider(1024);
+    final Config config = Config(
+      cache: cache,
+    );
+
+    final Router router = Router()
+      ..post(
+          '/webhook',
+          GithubWebhook(
+            config: config,
+          ).post);
     await serveHandler(router);
   });
 }
