24f2a14d86bff20b17a34d0911a3b90e5057abe5
Casey Hillers <chillers@google.com>
2022-03-14 10:29:16 -0700

    Remove release dashboard

--- release_dashboard/lib/logic/cherrypicks.dart
+++
@@ -1,8 +0,0 @@
-// Copyright 2014 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Converts the cherrypicks captured by input forms from string to a string array.
-List<String> cherrypickStringtoArray(String? cherrypickString) {
-  return cherrypickString == '' || cherrypickString == null ? <String>[] : cherrypickString.split(',');
-}
