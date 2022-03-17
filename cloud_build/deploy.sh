f5b61442d635dffe030171cc24c1a41144b254bc
godofredoc <godofredoc@google.com>
2022-03-17 10:30:33 -0700

    Revert "Temporarily remove auto-submit deployment. (#1670)" (#1671)
    This reverts commit 1efd941ed4516fca47897ce6b08583ea39c80673.

--- cloud_build/deploy.sh
+++ cloud_build/deploy.sh
@@ -15,3 +15,10 @@ gcloud app services set-traffic default --splits version-$2=1 --quiet
 # For daily builds, this will keep the 10 most recent versions, but wipe the rest.
 gcloud app versions list --format="value(version.id)" --sort-by="~version.createTime" | tail -n +11 | xargs -r gcloud app versions delete --quiet
 popd > /dev/null
+
+# auto_submit
+pushd auto_submit > /dev/null
+gcloud app deploy --project "$1" --version "version-$2" -q "$3" --no-stop-previous-version
+gcloud app services set-traffic default --splits version-$2=1 --quiet
+gcloud app versions list --format="value(version.id)" --sort-by="~version.createTime" | tail -n +20 | xargs -r gcloud app versions delete --quiet
+popd > /dev/null
