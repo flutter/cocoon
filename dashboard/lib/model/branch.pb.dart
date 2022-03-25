2366432e02aa56e8f28b8eeb3fe62c31fdcd984b
Xilai Zhang <xilaizhang@google.com>
2022-03-24 18:08:15 -0700

    format files

--- dashboard/lib/model/branch.pb.dart
+++ dashboard/lib/model/branch.pb.dart
@@ -13,11 +13,14 @@ import 'package:protobuf/protobuf.dart' as $pb;
 import 'key.pb.dart' as $0;
 
 class Branch extends $pb.GeneratedMessage {
-  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'Branch', createEmptyInstance: create)
-    ..aOM<$0.RootKey>(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'key', subBuilder: $0.RootKey.create)
-    ..aInt64(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'lastActivity', protoName: 'lastActivity')
-    ..hasRequiredFields = false
-  ;
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(
+      const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'Branch',
+      createEmptyInstance: create)
+    ..aOM<$0.RootKey>(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'key',
+        subBuilder: $0.RootKey.create)
+    ..aInt64(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'lastActivity',
+        protoName: 'lastActivity')
+    ..hasRequiredFields = false;
 
   Branch._() : super();
   factory Branch({
@@ -33,18 +36,19 @@ class Branch extends $pb.GeneratedMessage {
     }
     return _result;
   }
-  factory Branch.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
-  factory Branch.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
-  @$core.Deprecated(
-  'Using this can add significant overhead to your binary. '
-  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
-  'Will be removed in next major version')
+  factory Branch.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromBuffer(i, r);
+  factory Branch.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromJson(i, r);
+  @$core.Deprecated('Using this can add significant overhead to your binary. '
+      'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+      'Will be removed in next major version')
   Branch clone() => Branch()..mergeFromMessage(this);
-  @$core.Deprecated(
-  'Using this can add significant overhead to your binary. '
-  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
-  'Will be removed in next major version')
-  Branch copyWith(void Function(Branch) updates) => super.copyWith((message) => updates(message as Branch)) as Branch; // ignore: deprecated_member_use
+  @$core.Deprecated('Using this can add significant overhead to your binary. '
+      'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+      'Will be removed in next major version')
+  Branch copyWith(void Function(Branch) updates) =>
+      super.copyWith((message) => updates(message as Branch)) as Branch; // ignore: deprecated_member_use
   $pb.BuilderInfo get info_ => _i;
   @$core.pragma('dart2js:noInline')
   static Branch create() => Branch._();
@@ -57,7 +61,10 @@ class Branch extends $pb.GeneratedMessage {
   @$pb.TagNumber(1)
   $0.RootKey get key => $_getN(0);
   @$pb.TagNumber(1)
-  set key($0.RootKey v) { setField(1, v); }
+  set key($0.RootKey v) {
+    setField(1, v);
+  }
+
   @$pb.TagNumber(1)
   $core.bool hasKey() => $_has(0);
   @$pb.TagNumber(1)
@@ -68,10 +75,12 @@ class Branch extends $pb.GeneratedMessage {
   @$pb.TagNumber(2)
   $fixnum.Int64 get lastActivity => $_getI64(1);
   @$pb.TagNumber(2)
-  set lastActivity($fixnum.Int64 v) { $_setInt64(1, v); }
+  set lastActivity($fixnum.Int64 v) {
+    $_setInt64(1, v);
+  }
+
   @$pb.TagNumber(2)
   $core.bool hasLastActivity() => $_has(1);
   @$pb.TagNumber(2)
   void clearLastActivity() => clearField(2);
 }
-
--- dashboard/lib/model/branch.pbjson.dart
+++ dashboard/lib/model/branch.pbjson.dart
@@ -8,18 +8,20 @@
 import 'dart:core' as $core;
 import 'dart:convert' as $convert;
 import 'dart:typed_data' as $typed_data;
+
 @$core.Deprecated('Use branchDescriptor instead')
-const Branch$json = const {
+const Branch$json = {
   '1': 'Branch',
-  '2': const [
-    const {'1': 'key', '3': 1, '4': 1, '5': 11, '6': '.RootKey', '9': 0, '10': 'key', '17': true},
-    const {'1': 'lastActivity', '3': 2, '4': 1, '5': 3, '9': 1, '10': 'lastActivity', '17': true},
+  '2': [
+    {'1': 'key', '3': 1, '4': 1, '5': 11, '6': '.RootKey', '9': 0, '10': 'key', '17': true},
+    {'1': 'lastActivity', '3': 2, '4': 1, '5': 3, '9': 1, '10': 'lastActivity', '17': true},
   ],
-  '8': const [
-    const {'1': '_key'},
-    const {'1': '_lastActivity'},
+  '8': [
+    {'1': '_key'},
+    {'1': '_lastActivity'},
   ],
 };
 
 /// Descriptor for `Branch`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List branchDescriptor = $convert.base64Decode('CgZCcmFuY2gSHwoDa2V5GAEgASgLMgguUm9vdEtleUgAUgNrZXmIAQESJwoMbGFzdEFjdGl2aXR5GAIgASgDSAFSDGxhc3RBY3Rpdml0eYgBAUIGCgRfa2V5Qg8KDV9sYXN0QWN0aXZpdHk=');
+final $typed_data.Uint8List branchDescriptor = $convert.base64Decode(
+    'CgZCcmFuY2gSHwoDa2V5GAEgASgLMgguUm9vdEtleUgAUgNrZXmIAQESJwoMbGFzdEFjdGl2aXR5GAIgASgDSAFSDGxhc3RBY3Rpdml0eYgBAUIGCgRfa2V5Qg8KDV9sYXN0QWN0aXZpdHk=');
