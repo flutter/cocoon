ad4cd9758b5b88e23ed03cc7e5d53af0aab1f68f
Keyong Han <keyonghan@google.com>
2022-04-11 09:17:11 -0700

    deploy cronjob

--- cloud_build/deploy_app_dart.sh
+++ cloud_build/deploy_app_dart.sh
@@ -9,7 +9,6 @@ pushd app_dart > /dev/null
 set -e
 # app_dart
 gcloud app deploy --project "$1" --version "version-$2" -q "$3" --no-stop-previous-version
-gcloud app deploy --project "$1" cron.yaml
 gcloud app services set-traffic default --splits version-$2=1 --quiet
 # Google Cloud quota only allows 30 versions.
 # For daily builds, this will keep the 10 most recent versions, but wipe the rest.
