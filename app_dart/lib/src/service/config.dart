94d91aef9bd33bcd059ee89510cbec9a374ddf8e
Pierre-Louis <6655696+guidezpl@users.noreply.github.com>
2022-05-04 23:38:06 +0200

    Improve flutter-gold pending message (#1785)

--- app_dart/lib/src/service/config.dart
+++ app_dart/lib/src/service/config.dart
@@ -226,7 +226,7 @@ class Config {
       '(https://github.com/flutter/flutter/wiki/Tree-hygiene#how-to-review-code) '
       'and make sure this patch meets those guidelines before LGTMing.';
 
-  String get flutterGoldPending => 'Pending.';
+  String get flutterGoldPending => 'Waiting for all other checks to be successful before querying Gold.';
 
   String get flutterGoldSuccess => 'All golden file tests have passed.';
 
