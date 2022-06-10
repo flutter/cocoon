b178419b37f59088b5e6340a2a8cb32878707b43
Xilai Zhang <xilaizhang@google.com>
2022-06-10 12:58:11 -0700

    skeleton from dart create

---
+++ codesign/bin/codesign.dart
@@ -0,0 +1,5 @@
+import 'package:codesign/codesign.dart' as codesign;
+
+void main(List<String> arguments) {
+  print('Hello world: ${codesign.calculate()}!');
+}
---
+++ codesign/test/codesign_test.dart
@@ -0,0 +1,8 @@
+import 'package:codesign/codesign.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
