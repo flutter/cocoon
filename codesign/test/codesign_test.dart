66d940c8bd316fb8a47c3cb5d991a4dfdfa1e1d5
Xilai Zhang <xilaizhang@google.com>
2022-06-13 13:28:38 -0700

    add dummy tests

--- codesign/test/codesign_test.dart
+++ codesign/test/codesign_test.dart
@@ -2,4 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-void main() {}
+import 'package:test/test.dart';
+
+void main() {
+  test('placeholder', () {
+    expect(1, equals(1));
+  });
+}
