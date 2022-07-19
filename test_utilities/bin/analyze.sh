96aafe89ba9e570d85811dc1ea590dea29472ee8
Casey Hillers <chillers@google.com>
2022-07-19 10:32:38 -0700

    [test_utilities] Add analyze.dart

---
+++ test_utilities/bin/analyze.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Copyright 2020 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# General flutter/cocoon repo static analysis.
+
+set -ex
+
+dir=$(dirname $0)
+
+pushd $dir/../../analyze > /dev/null
+dart pub get
+dart analyze.dart
+
+popd > /dev/null
