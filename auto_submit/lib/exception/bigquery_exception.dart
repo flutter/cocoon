6f01f96595d109ca39b2ef7b75bd06b53adee92d
Ricardo Amador <ricardoamador@google.com>
2022-09-02 16:21:52 -0700

    Added copyright header to bigquery exception.

--- auto_submit/lib/exception/bigquery_exception.dart
+++ auto_submit/lib/exception/bigquery_exception.dart
@@ -1,4 +1,10 @@
+// Copyright 2022 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 class BigQueryException implements Exception {
+  /// Create a custom exception for Big Query Errors.
   BigQueryException(this.cause);
+  
   final String cause;
 }
