279fbca93fcf1c26ba3c92cff323628fca992ef1
Ricardo Amador <ricardoamador@google.com>
2022-09-09 15:11:06 -0700

    formatting

--- auto_submit/lib/model/big_query_revert_request_record.g.dart
+++ auto_submit/lib/model/big_query_revert_request_record.g.dart
@@ -6,19 +6,16 @@ part of 'big_query_revert_request_record.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-RevertRequestRecord _$RevertRequestRecordFromJson(Map<String, dynamic> json) =>
-    RevertRequestRecord(
+RevertRequestRecord _$RevertRequestRecordFromJson(Map<String, dynamic> json) => RevertRequestRecord(
       organization: json['organization'] as String?,
       repository: json['repository'] as String?,
       author: json['author'] as String?,
       prNumber: json['pr_number'] as int?,
       prCommit: json['pr_commit'] as String?,
-      prCreatedTimestamp: json['pr_created_timestamp'] == null
-          ? null
-          : DateTime.parse(json['pr_created_timestamp'] as String),
-      prLandedTimestamp: json['pr_landed_timestamp'] == null
-          ? null
-          : DateTime.parse(json['pr_landed_timestamp'] as String),
+      prCreatedTimestamp:
+          json['pr_created_timestamp'] == null ? null : DateTime.parse(json['pr_created_timestamp'] as String),
+      prLandedTimestamp:
+          json['pr_landed_timestamp'] == null ? null : DateTime.parse(json['pr_landed_timestamp'] as String),
       originalPrAuthor: json['original_pr_author'] as String?,
       originalPrNumber: json['original_pr_number'] as int?,
       originalPrCommit: json['original_pr_commit'] as String?,
@@ -30,9 +27,7 @@ RevertRequestRecord _$RevertRequestRecordFromJson(Map<String, dynamic> json) =>
           : DateTime.parse(json['original_pr_landed_timestamp'] as String),
     );
 
-Map<String, dynamic> _$RevertRequestRecordToJson(
-        RevertRequestRecord instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$RevertRequestRecordToJson(RevertRequestRecord instance) => <String, dynamic>{
       'pr_created_timestamp': instance.prCreatedTimestamp?.toIso8601String(),
       'pr_landed_timestamp': instance.prLandedTimestamp?.toIso8601String(),
       'organization': instance.organization,
@@ -43,8 +38,6 @@ Map<String, dynamic> _$RevertRequestRecordToJson(
       'original_pr_author': instance.originalPrAuthor,
       'original_pr_number': instance.originalPrNumber,
       'original_pr_commit': instance.originalPrCommit,
-      'original_pr_created_timestamp':
-          instance.originalPrCreatedTimestamp?.toIso8601String(),
-      'original_pr_landed_timestamp':
-          instance.originalPrLandedTimestamp?.toIso8601String(),
+      'original_pr_created_timestamp': instance.originalPrCreatedTimestamp?.toIso8601String(),
+      'original_pr_landed_timestamp': instance.originalPrLandedTimestamp?.toIso8601String(),
     };
