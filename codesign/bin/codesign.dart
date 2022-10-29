b93500a369882fdfab2a4cec483085b60e2223da
Xilai Zhang <xilaizhang@google.com>
2022-10-28 18:44:57 -0700

    trim comments

--- codesign/bin/codesign.dart
+++ codesign/bin/codesign.dart
@@ -16,9 +16,9 @@ const String kHelpFlag = 'help';
 const String kDryrunFlag = 'dryrun';
 const String kCodesignCertNameOption = 'CODESIGN-CERT-NAME';
 const String kCodesignUserNameOption = 'CODESIGN-USERNAME';
-const String kGcsDownloadPathOption = 'GCS-DOWNLOAD-PATH';
-const String kGcsUploadPathOption = 'GCS-UPLOAD-PATH';
-const String kCredentialsPathOption = 'PASSWORD-FILE-PATH';
+const String kGcsDownloadPathOption = 'gcs-download-path';
+const String kGcsUploadPathOption = 'gcs-upload-path';
+const String kCredentialsPathOption = 'password-file-path';
 
 /// Perform Mac code signing based on file paths.
 ///
@@ -32,21 +32,28 @@ const String kCredentialsPathOption = 'PASSWORD-FILE-PATH';
 /// For [kGcsDownloadPathOption] and [kGcsUploadPathOption], they are required parameter to specify the google cloud bucket paths.
 /// [kGcsDownloadPathOption] is the google cloud bucket prefix to download the remote artifacts,
 /// [kGcsUploadPathOption] is the cloud bucket prefix to upload codesigned artifact to.
-/// For example, supply '--GCS-DOWNLOAD-PATH=gs://flutter_infra_release/ios-usb-dependencies/unsigned/libimobiledevice/<commit>/libimobiledevice.zip',
-/// and code sign app will download the artifact at 'flutter_infra_release/ios-usb-dependencies/unsigned/libimobiledevice/<commit>/libimobiledevice.zip' on google cloud storage
+/// For example, supply
+/// '--gcs-download-path=gs://flutter_infra_release/ios-usb-dependencies/unsigned/libimobiledevice/<commit>/libimobiledevice.zip',
+/// and code sign app will download the artifact at
+/// 'flutter_infra_release/ios-usb-dependencies/unsigned/libimobiledevice/<commit>/libimobiledevice.zip'
+/// on google cloud storage
 ///
-/// For [kCredentialsPathOption], this is the file path of the password file in file system. The password file stores sensitive passwords.
+/// For [kCredentialsPathOption], this is the file path of the password file in file system.
+/// The password file stores sensitive passwords.
 /// sensitive passwords include <CODESIGN-APPSTORE-ID>, <CODESIGN-TEAM-ID>, and <APP-SPECIFIC-PASSWORD>.
-/// For example, if a user supplies --PASSWORD-FILE-PATH=/tmp/passwords.txt, then we would be expecting a password file located at /tmp/passwords.txt.
-/// The password file should provide the password value for each of the password name, deliminated by a single colon. The content of a password file would look similar to:
+/// For example, if a user supplies --password-file-path=/tmp/passwords.txt,
+/// then we would be expecting a password file located at /tmp/passwords.txt.
+/// The password file should provide the password value for each of the password name, deliminated by a single colon.
+/// The content of a password file would look similar to:
 /// CODESIGN-APPSTORE-ID:123
 /// CODESIGN-TEAM-ID:456
 /// APP-SPECIFIC-PASSWORD:789
 ///
 /// Usage:
 /// ```shell
-/// dart run bin/codesign.dart --[no-]dryrun --GCS-DOWNLOAD-PATH=gs://flutter_infra_release/flutter/<commit>/android-arm-profile/artifacts.zip
-/// --GCS-UPLOAD-PATH=gs://flutter_infra_release/flutter/<commit>/android-arm-profile/artifacts.zip
+/// dart run bin/codesign.dart --[no-]dryrun
+/// --gcs-download-path=gs://flutter_infra_release/flutter/<commit>/android-arm-profile/artifacts.zip
+/// --gcs-upload-path=gs://flutter_infra_release/flutter/<commit>/android-arm-profile/artifacts.zip
 /// ```
 Future<void> main(List<String> args) async {
   final ArgParser parser = ArgParser();
@@ -65,13 +72,13 @@ Future<void> main(List<String> args) async {
     ..addOption(
       kGcsDownloadPathOption,
       help: 'The google cloud bucket path to download the artifact from\n'
-          'e.g. supply `--GCS-DOWNLOAD-PATH=gs://flutter_infra_release/ios-usb-dependencies/unsigned/ios-deploy/<commit>/ios-deploy.zip`'
+          'e.g. supply `--gcs-download-path=gs://flutter_infra_release/ios-usb-dependencies/unsigned/ios-deploy/<commit>/ios-deploy.zip`'
           ' if you would like to codesign ios-deploy.zip, which has a google cloud bucket path of flutter_infra_release/ios-usb-dependencies/unsigned/ios-deploy/<commit>/ios-deploy.zip to be downloaded from \n',
     )
     ..addOption(
       kGcsUploadPathOption,
       help: 'The google cloud bucket path to upload the artifact to. \n'
-          'e.g. supply `--GCS-UPLOAD-PATH=gs://flutter_infra_release/ios-usb-dependencies/ios-deploy/<commit>/ios-deploy.zip`'
+          'e.g. supply `--gcs-upload-path=gs://flutter_infra_release/ios-usb-dependencies/ios-deploy/<commit>/ios-deploy.zip`'
           ' if you would like to codesign ios-deploy.zip, which has a google cloud bucket path of flutter_infra_release/ios-usb-dependencies/ios-deploy/<commit>/ios-deploy.zip to be uploaded to',
     )
     ..addOption(
