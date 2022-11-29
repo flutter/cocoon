036f72bbdcaa5a6d11cf15bf5fbd6aa783fadaf2
Godofredo Contreras <godofredoc@google.com>
2022-11-29 09:50:28 -0800

    Use buildbucket package name.

--- packages/buildbucket-dart/lib/buildbucket_pb.dart
+++ packages/buildbucket-dart/lib/buildbucket_pb.dart
@@ -2,6 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-library buildbucket_pb;
+library buildbucket;
 
 export 'src/generated/go.chromium.org/luci/buildbucket/proto/build.pb.dart' show Build;
--- packages/buildbucket-dart/test/buildbucket_pb_test.dart
+++ packages/buildbucket-dart/test/buildbucket_pb_test.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:buildbucket_pb/buildbucket_pb.dart';
+import 'package:buildbucket/buildbucket_pb.dart';
 import 'package:test/test.dart';
 
 Map<String, dynamic> buildJson = {
