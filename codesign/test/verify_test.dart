d72a7ba21d06680ee56c6a1af5ca07b52714073a
Christopher Fujino <christopherfujino@gmail.com>
2023-01-11 16:49:08 -0800

    update licenses

--- codesign/test/verify_test.dart
+++ codesign/test/verify_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2019 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:codesign/verify.dart';
 import 'package:file/file.dart';
 import 'package:file/memory.dart';
