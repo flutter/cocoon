69803e4fa09d693fc6bf0646c604735616674c8f
Ricardo Amador <ricardoamador@google.com>
2023-02-15 15:22:34 -0800

    Updated tests

--- auto_submit/test/validations/revert_test.dart
+++ auto_submit/test/validations/revert_test.dart
@@ -124,6 +124,7 @@ void main() {
       final Map<String, dynamic> queryResultJsonDecode =
           jsonDecode(queryResultRepositoryContributorJson) as Map<String, dynamic>;
       final QueryResult queryResult = QueryResult.fromJson(queryResultJsonDecode);
+      githubService.pullRequestMock = revertPullRequest;
       final ValidationResult validationResult = await revert.validate(queryResult, revertPullRequest);
       assert(!validationResult.result);
       assert(validationResult.action == Action.REMOVE_LABEL);
@@ -137,6 +138,7 @@ void main() {
       final Map<String, dynamic> queryResultJsonDecode =
           jsonDecode(queryResultRepositoryOwnerJson) as Map<String, dynamic>;
       final QueryResult queryResult = QueryResult.fromJson(queryResultJsonDecode);
+      githubService.pullRequestMock = revertPullRequest;
       final ValidationResult validationResult = await revert.validate(queryResult, revertPullRequest);
       assert(!validationResult.result);
       assert(validationResult.action == Action.REMOVE_LABEL);
@@ -153,6 +155,7 @@ void main() {
       final Map<String, dynamic> queryResultJsonDecode =
           jsonDecode(queryResultRepositoryOwnerJson) as Map<String, dynamic>;
       final QueryResult queryResult = QueryResult.fromJson(queryResultJsonDecode);
+      githubService.pullRequestMock = revertPullRequest;
       final ValidationResult validationResult = await revert.validate(queryResult, revertPullRequest);
       assert(!validationResult.result);
       assert(validationResult.action == Action.REMOVE_LABEL);
@@ -181,7 +184,7 @@ void main() {
 
       // Need to set the mock checkRuns for required CheckRun validation
       githubService.checkRunsData = ciyamlCheckRunNotComplete;
-
+      githubService.pullRequestMock = revertPullRequest;
       revert = Revert(
         config: config,
         retryOptions: const RetryOptions(
@@ -217,6 +220,8 @@ void main() {
       // Need to set the mock checkRuns for required CheckRun validation
       githubService.checkRunsData = ciyamlCheckRun;
 
+      githubService.pullRequestMock = revertPullRequest;
+
       final ValidationResult validationResult = await revert.validate(queryResult, revertPullRequest);
       assert(!validationResult.result);
       assert(validationResult.action == Action.REMOVE_LABEL);
@@ -245,6 +250,8 @@ void main() {
 
       // Need to set the mock checkRuns for required CheckRun validation
       githubService.checkRunsData = ciyamlCheckRun;
+
+      githubService.pullRequestMock = revertPullRequest;
 
       final ValidationResult validationResult = await revert.validate(queryResult, revertPullRequest);
       assert(validationResult.result);
