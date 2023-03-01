ee64c61df3e1c82f2f5260f92e21b939603dd2f3
Drew Roen <drewroen@google.com>
2023-03-01 11:23:38 -0600

    Update logging

--- cloud_build/get_docker_image_provenance.sh
+++ cloud_build/get_docker_image_provenance.sh
@@ -17,9 +17,10 @@ do
     COMMAND_RESULT=$?
     if [[ $COMMAND_RESULT -eq 0 ]]
     then
+        echo "Successfully obtained provenance and saved to $2"
         break
     else
-        echo "Failed to obtain provenance. Retrying in 30 seconds"
+        echo "Failed to obtain provenance."
         sleep 30
     fi
 done
