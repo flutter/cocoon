cc873c25b386833c609bb147d494e3f6223b117e
David Iglesias Teixeira <ditman@gmail.com>
2023-04-07 16:39:51 -0700

    dart format

--- dashboard/lib/widgets/sign_in_button/sign_in_button.dart
+++ dashboard/lib/widgets/sign_in_button/sign_in_button.dart
@@ -2,5 +2,4 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export 'sign_in_button_native.dart'
-  if (dart.library.js_util) 'sign_in_button_web.dart';
+export 'sign_in_button_native.dart' if (dart.library.js_util) 'sign_in_button_web.dart';
