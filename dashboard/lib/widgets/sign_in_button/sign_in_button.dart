56c84a3a80c2c2e7d5f1c6c434701aac05831895
David Iglesias Teixeira <ditman@gmail.com>
2023-04-07 16:19:19 -0700

    Add missing license

--- dashboard/lib/widgets/sign_in_button/sign_in_button.dart
+++ dashboard/lib/widgets/sign_in_button/sign_in_button.dart
@@ -1,2 +1,6 @@
+// Copyright (c) 2016, the Flutter project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 export 'sign_in_button_native.dart'
   if (dart.library.js_util) 'sign_in_button_web.dart';
