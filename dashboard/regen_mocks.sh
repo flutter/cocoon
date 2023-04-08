3d0497078f2a6883449955c3ea855e3a34ea9156
David Iglesias Teixeira <ditman@gmail.com>
2023-04-07 19:42:50 -0700

    Fix hashbang.

--- dashboard/regen_mocks.sh
+++ dashboard/regen_mocks.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Copyright 2019 The Flutter Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-flutter pub get
+dart pub get
 
 echo "(Re)generating mocks."
 
-flutter pub run build_runner build --delete-conflicting-outputs
+dart run build_runner build --delete-conflicting-outputs
