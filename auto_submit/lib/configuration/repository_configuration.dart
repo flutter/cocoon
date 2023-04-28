e0bedcab31f0b04f2af29c83eb8ea1d30a1ecc27
Ricardo Amador <ricardoamador@google.com>
2023-04-28 13:05:27 -0700

    Removed repositoryConfigurationBuilder class.

--- auto_submit/lib/configuration/repository_configuration.dart
+++ auto_submit/lib/configuration/repository_configuration.dart
@@ -5,56 +5,6 @@
 import 'package:auto_submit/exception/configuration_exception.dart';
 import 'package:yaml/yaml.dart';
 
-/// RepositoryConfigurationBuilder is used to build the RepositoryConfiguration
-/// object. It allows the default values or missing values to be configured for
-/// the configuration before sending it on for use.
-class RepositoryConfigurationBuilder {
-  bool? _allowConfigOverride = false;
-  String? _defaultBranch = '';
-  Set<String>? _autoApprovalAccounts = {};
-  int? _approvingReviews = 2;
-  String? _approvalGroup = '';
-  bool? _runCi = true;
-  bool? _supportNoReviewReverts = true;
-  Set<String>? _requiredCheckRunsOnRevert = {};
-
-  set allowConfigOverride(bool value) => _allowConfigOverride = value;
-
-  set defaultBranch(String value) => _defaultBranch = value;
-
-  set autoApprovalAccounts(Set<String>? value) {
-    if (value != null) {
-      _autoApprovalAccounts = value;
-    }
-  }
-
-  set approvingReviews(int? value) {
-    if (value != null) {
-      _approvingReviews = value;
-    }
-  }
-
-  set approvalGroup(String? value) => _approvalGroup = value;
-
-  set runCi(bool? value) {
-    if (value != null) {
-      _runCi = value;
-    }
-  }
-
-  set supportNoReviewReverts(bool? value) {
-    if (value != null) {
-      _supportNoReviewReverts = value;
-    }
-  }
-
-  set requiredCheckRunsOnRevert(Set<String>? value) {
-    if (value != null && value.isNotEmpty) {
-      _requiredCheckRunsOnRevert = value;
-    }
-  }
-}
-
 /// The RepositoryConfiguration stores the pertinent information that autosubmit
 /// will need when submiting and validating pull requests for a particular
 /// repository.
@@ -130,18 +80,7 @@ class RepositoryConfiguration {
   }
 
   static RepositoryConfiguration fromYaml(String yaml) {
-    final RepositoryConfigurationBuilder builder = RepositoryConfigurationBuilder();
-
     final dynamic yamlDoc = loadYaml(yaml);
-
-    if (yamlDoc[allowConfigOverrideKey] != null) {
-      builder.allowConfigOverride = yamlDoc[allowConfigOverrideKey];
-    }
-
-    // Default branch is required.
-    if (yamlDoc[defaultBranchKey] != null) {
-      builder.defaultBranch = yamlDoc[defaultBranchKey];
-    }
 
     final Set<String> autoApprovalAccounts = {};
     final YamlList? yamlAutoApprovalAccounts = yamlDoc[autoApprovalAccountsKey];
@@ -150,19 +89,10 @@ class RepositoryConfiguration {
         autoApprovalAccounts.add(element as String);
       }
     }
-    builder.autoApprovalAccounts = autoApprovalAccounts;
-
-    builder.approvingReviews = yamlDoc[approvingReviewsKey];
-
-    if (yamlDoc[approvalGroupKey] != null) {
-      builder.approvalGroup = yamlDoc[approvalGroupKey];
-    } else {
+    
+    if (yamlDoc[approvalGroupKey] == null) {
       throw ConfigurationException('The approval group is a required field.');
     }
-
-    builder.runCi = yamlDoc[runCiKey];
-
-    builder.supportNoReviewReverts = yamlDoc[supportNoReviewRevertKey];
 
     final Set<String> requiredCheckRunsOnRevert = {};
     final YamlList? yamlRequiredCheckRuns = yamlDoc[requiredCheckRunsOnRevertKey];
@@ -171,17 +101,16 @@ class RepositoryConfiguration {
         requiredCheckRunsOnRevert.add(element as String);
       }
     }
-    builder.requiredCheckRunsOnRevert = requiredCheckRunsOnRevert;
 
     return RepositoryConfiguration(
-      allowConfigOverride: builder._allowConfigOverride,
-      defaultBranch: builder._defaultBranch,
-      autoApprovalAccounts: builder._autoApprovalAccounts,
-      approvingReviews: builder._approvingReviews,
-      approvalGroup: builder._approvalGroup,
-      runCi: builder._runCi,
-      supportNoReviewReverts: builder._supportNoReviewReverts,
-      requiredCheckRunsOnRevert: builder._requiredCheckRunsOnRevert,
+      allowConfigOverride: yamlDoc[allowConfigOverrideKey] ?? false,
+      defaultBranch: yamlDoc[defaultBranchKey] ?? 'main',
+      autoApprovalAccounts: autoApprovalAccounts,
+      approvingReviews: yamlDoc[approvingReviewsKey] ?? 2,
+      approvalGroup: yamlDoc[approvalGroupKey],
+      runCi: yamlDoc[runCiKey] ?? true,
+      supportNoReviewReverts: yamlDoc[supportNoReviewRevertKey] ?? true,
+      requiredCheckRunsOnRevert: requiredCheckRunsOnRevert,
     );
   }
 }
