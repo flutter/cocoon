a2b9eecaff4e0db1e8570ff7d0b5e202390afd19
Ricardo Amador <ricardoamador@google.com>
2023-05-10 11:23:55 -0700

    Adding a license header to the pull request state class.

--- auto_submit/lib/model/pull_request_state.dart
+++ auto_submit/lib/model/pull_request_state.dart
@@ -1,3 +1,9 @@
+// Copyright 2023 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// This class holds values for the current states of the pull requests we
+/// process with the autosubmit service.
 class PullRequestState {
   static String get open => 'open';
   static String get closed => 'closed';
