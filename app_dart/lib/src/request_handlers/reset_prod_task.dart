c3accd9d9d3bf73d76801728dedb6f174ed4a30e
Casey Hillers <chillers@google.com>
2023-05-17 11:48:38 -0700

    wip reset all

--- app_dart/lib/src/request_handlers/reset_prod_task.dart
+++ app_dart/lib/src/request_handlers/reset_prod_task.dart
@@ -4,6 +4,7 @@
 
 import 'dart:async';
 
+import 'package:cocoon_service/src/service/build_status_provider.dart';
 import 'package:gcloud/db.dart';
 import 'package:github/github.dart';
 import 'package:meta/meta.dart';
@@ -44,7 +45,12 @@ class ResetProdTask extends ApiRequestHandler<Body> {
   static const String ownerParam = 'Owner';
   static const String repoParam = 'Repo';
   static const String commitShaParam = 'Commit';
-  static const String builderParam = 'Builder';
+
+  /// Name of the task to be retried.
+  /// 
+  /// If "all" is given, all failed tasks will be retried. This enables
+  /// oncalls to quickly recover a commit without the tedium of the UI.
+  static const String taskParam = 'Task';
 
   @override
   Future<Body> post() async {
@@ -55,20 +61,47 @@ class ResetProdTask extends ApiRequestHandler<Body> {
     final String? repo = requestData![repoParam] as String?;
     final String? sha = requestData![commitShaParam] as String?;
     final TokenInfo token = await tokenInfo(request!);
-    final String? taskName = requestData![builderParam] as String?;
+    final String? taskName = requestData![taskParam] as String?;
 
     RepositorySlug? slug;
-
     if (encodedKey != null && encodedKey.isNotEmpty) {
       // Check params required for dashboard.
       checkRequiredParameters(<String>[taskKeyParam]);
     } else {
       // Checks params required when this API is called with curl.
-      checkRequiredParameters(<String>[commitShaParam, builderParam, repoParam]);
+      checkRequiredParameters(<String>[commitShaParam, taskParam, repoParam]);
       slug = RepositorySlug(owner, repo!);
       gitBranch ??= Config.defaultBranch(slug);
     }
 
+    final bool retryAll = taskName == 'all';
+
+    if (retryAll) {
+      final BuildStatusService buildStatusService = BuildStatusService(datastore);
+      final List<CommitStatus> statuses = await buildStatusService.retrieveCommitStatus(slug: slug!, limit: 5).toList();
+      final CommitStatus status = statuses.firstWhere((CommitStatus status) => status.commit.sha == sha);
+      final List<Future<void>> futures = <Future<void>>[];
+      for (final Task task in status.stages.first.tasks) {
+        futures.add(rerun(datastore: datastore, gitBranch: gitBranch, sha: sha, taskName: task.name, slug: slug, email: token.email!,));
+      }
+      await Future.wait(futures);
+    } else {
+      await rerun(datastore: datastore, encodedKey: encodedKey, gitBranch: gitBranch, sha: sha, taskName: taskName, slug: slug, email: token.email!, ignoreChecks: true,);
+    }
+
+    return Body.empty;
+  }
+
+  Future<void> rerun({
+    required DatastoreService datastore,
+    String? encodedKey,
+    String? gitBranch,
+    String? sha,
+    String? taskName,
+    RepositorySlug? slug,
+    required String email,
+    bool ignoreChecks = false,
+  }) async {
     final Task task = await _getTaskFromNamedParams(
       datastore: datastore,
       encodedKey: encodedKey,
@@ -83,7 +116,7 @@ class ResetProdTask extends ApiRequestHandler<Body> {
     final Target target = ciYaml.postsubmitTargets.singleWhere((Target target) => target.value.name == task.name);
 
     final Map<String, List<String>> tags = <String, List<String>>{
-      'triggered_by': <String>[token.email!],
+      'triggered_by': <String>[email],
       'trigger_type': <String>['manual'],
     };
     final bool isRerunning = await luciBuildService.checkRerunBuilder(
@@ -92,13 +125,13 @@ class ResetProdTask extends ApiRequestHandler<Body> {
       target: target,
       datastore: datastore,
       tags: tags,
-      ignoreChecks: true,
+      ignoreChecks: ignoreChecks,
     );
-    if (isRerunning == false) {
-      throw InternalServerError('Failed to rerun ${task.name}');
-    }
 
-    return Body.empty;
+    // For human retries from the dashboard, notify if a task failed to rerun.
+    if (ignoreChecks && isRerunning == false) {
+      throw InternalServerError('Failed to rerun $taskName');
+    }
   }
 
   /// Retrieve [Task] from [DatastoreService] from either an encoded key or commit + task name info.
