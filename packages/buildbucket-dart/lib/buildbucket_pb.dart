e7f5248bb5cde418062ccb693ab68b3caf5b1cf5
Ricardo Amador <ricardoamador@google.com>
2023-06-26 16:36:42 -0700

    Forgot to expose the notification object and BuildV2 pubsub.

--- packages/buildbucket-dart/lib/buildbucket_pb.dart
+++ packages/buildbucket-dart/lib/buildbucket_pb.dart
@@ -5,3 +5,4 @@
 library buildbucket;
 
 export 'src/generated/go.chromium.org/luci/buildbucket/proto/build.pb.dart' show Build;
+export 'src/generated/go.chromium.org/luci/buildbucket/proto/notification.pb.dart' show NotificationConfig, BuildsV2PubSub;
