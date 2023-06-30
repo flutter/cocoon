06dce41b6fa671749d901c1f8cbe8259b4174d93
Ricardo Amador <32242716+ricardoamador@users.noreply.github.com>
2023-06-30 12:14:19 -0700

    Make filters insensitive (#2908)
    Simple fix to make the searches case insensitive in the dashboard.
    
    *List which issues are fixed by this PR. You must list at least one issue.*
    Fixes https://github.com/flutter/flutter/issues/102755
    
    *If you had to change anything in the [flutter/tests] repo, include a link to the migration guide as per the [breaking change policy].*
    
    Without case:
    ![image](https://github.com/flutter/cocoon/assets/32242716/7d1f3d02-bfbf-498c-96fe-145b05436cc8)

--- dashboard/lib/logic/task_grid_filter.dart
+++ dashboard/lib/logic/task_grid_filter.dart
@@ -44,9 +44,10 @@ class TaskGridFilter extends FilterPropertySource {
 
   final RegExpFilterProperty _taskProperty =
       RegExpFilterProperty(fieldName: 'taskFilter', label: 'Task Name', caseSensitive: false);
-  final RegExpFilterProperty _authorProperty = RegExpFilterProperty(fieldName: 'authorFilter', label: 'Commit Author');
+  final RegExpFilterProperty _authorProperty =
+      RegExpFilterProperty(fieldName: 'authorFilter', label: 'Commit Author', caseSensitive: false);
   final RegExpFilterProperty _messageProperty =
-      RegExpFilterProperty(fieldName: 'messageFilter', label: 'Commit Message');
+      RegExpFilterProperty(fieldName: 'messageFilter', label: 'Commit Message', caseSensitive: false);
   final RegExpFilterProperty _hashProperty = RegExpFilterProperty(fieldName: 'hashFilter', label: 'Commit Hash');
   final BoolFilterProperty _macProperty = BoolFilterProperty(fieldName: 'showMac', label: 'Mac');
   final BoolFilterProperty _windowsPorperty = BoolFilterProperty(fieldName: 'showWindows', label: 'Windows');
