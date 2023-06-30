7799c24bdeff8faedb91e87a8dd18399a93614f7
keyonghan <54558023+keyonghan@users.noreply.github.com>
2023-06-29 17:17:04 -0700

    Add date range for flaky statistics when updating flake bugs (#2902)
    Fixes: https://github.com/flutter/flutter/issues/115403

--- app_dart/lib/src/service/bigquery.dart
+++ app_dart/lib/src/service/bigquery.dart
@@ -36,7 +36,9 @@ select builder_name,
        string_agg(succeeded_builds, ', ') as succeeded_builds,
        array_agg(case when is_flaky = 1 then sha end IGNORE NULLS ORDER BY date DESC)[ordinal(1)] as recent_flaky_commit,
        array_agg(case when is_flaky = 1 then flaky_builds end IGNORE NULLS ORDER BY date DESC)[ordinal(1)] as flaky_build_of_recent_flaky_commit,
-       sum(is_flaky)/count(*) as flaky_ratio
+       sum(is_flaky)/count(*) as flaky_ratio,
+       min(date) as from_date,
+       max(date) as to_date
 from (select *, row_number() over (partition by builder_name order by time desc) as rank from `flutter-dashboard.datasite.luci_prod_build_status`)
 where date>=date_sub(current_date(), interval 30 day) and
       builder_name not like '%Drone' and
@@ -61,7 +63,9 @@ select builder_name,
        string_agg(succeeded_builds, ', ') as succeeded_builds,
        array_agg(case when is_flaky = 1 then sha end IGNORE NULLS ORDER BY date DESC)[ordinal(1)] as recent_flaky_commit,
        array_agg(case when is_flaky = 1 then flaky_builds end IGNORE NULLS ORDER BY date DESC)[ordinal(1)] as flaky_build_of_recent_flaky_commit,
-       sum(is_flaky)/count(*) as flaky_ratio
+       sum(is_flaky)/count(*) as flaky_ratio,
+       min(date) as from_date,
+       max(date) as to_date
 from (select *, row_number() over (partition by builder_name order by time desc) as rank from `flutter-dashboard.datasite.luci_staging_build_status`)
 where date>=date_sub(current_date(), interval 30 day) and
       builder_name not like '%Drone' and
@@ -148,6 +152,8 @@ class BigqueryService {
           flakyBuildOfRecentCommit: row.f![6].v as String?,
           flakyNumber: int.parse(row.f![1].v as String),
           totalNumber: int.parse(row.f![2].v as String),
+          fromDate: row.f![8].v as String?,
+          toDate: row.f![9].v as String?,
         ),
       );
     }
@@ -225,6 +231,8 @@ class BuilderStatistic {
     this.succeededBuilds,
     this.recentCommit,
     this.flakyBuildOfRecentCommit,
+    this.fromDate,
+    this.toDate,
   });
 
   final String name;
@@ -235,4 +243,6 @@ class BuilderStatistic {
   final String? flakyBuildOfRecentCommit;
   final int flakyNumber;
   final int totalNumber;
+  final String? fromDate;
+  final String? toDate;
 }
