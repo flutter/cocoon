40047e5ad9a6d9a773aac1617b783152ca81bb62
Ricardo Amador <ricardoamador@google.com>
2023-07-06 16:46:45 -0700

    Add a license.

--- auto_submit/lib/validations/mergeable.dart
+++ auto_submit/lib/validations/mergeable.dart
@@ -6,7 +6,9 @@ import 'package:auto_submit/model/auto_submit_query_result.dart';
 import 'package:auto_submit/validations/validation.dart';
 import 'package:github/github.dart' as github;
 
+/// Validates that a pull request is in a mergeable state.
 class Mergeable extends Validation {
+
   Mergeable({required super.config});
 
   @override
