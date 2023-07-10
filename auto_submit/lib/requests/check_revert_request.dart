2f307f39ef937b13596513f6d4a60963760633e3
Ricardo Amador <ricardoamador@google.com>
2023-07-10 15:09:41 -0700

    Adding a license to to check_revert_request.dart

--- auto_submit/lib/requests/check_revert_request.dart
+++ auto_submit/lib/requests/check_revert_request.dart
@@ -1,3 +1,7 @@
+// Copyright 2023 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:auto_submit/request_handling/pubsub.dart';
 import 'package:auto_submit/requests/check_request.dart';
 import 'package:auto_submit/service/approver_service.dart';
