916cbac98cf82cebee75beed753789b3e2e2d412
Ricardo Amador <ricardoamador@google.com>
2023-07-12 15:34:27 -0700

    Readd the license.

--- auto_submit/lib/service/validation_service.dart
+++ auto_submit/lib/service/validation_service.dart
@@ -1,3 +1,8 @@
+
+// Copyright 2023 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:auto_submit/exception/bigquery_exception.dart';
 import 'package:auto_submit/exception/retryable_exception.dart';
 import 'package:auto_submit/model/auto_submit_query_result.dart';
