b76215ce97432637b50dd725dbbb81527348b795
Ricardo Amador <ricardoamador@google.com>
2023-07-12 15:20:52 -0700

    Add missing licenses.

--- auto_submit/lib/service/pull_request_validation_service.dart
+++ auto_submit/lib/service/pull_request_validation_service.dart
@@ -1,3 +1,7 @@
+// Copyright 2023 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:auto_submit/configuration/repository_configuration.dart';
 import 'package:auto_submit/model/auto_submit_query_result.dart';
 import 'package:auto_submit/model/pull_request_data_types.dart';
