bb1a4b81c0432466334735194abe508071b534cf
Drew Roen <drewroen@google.com>
2023-09-13 12:24:03 -0500

    Add issue to TODO

--- app_dart/lib/src/request_handlers/dart_internal_subscription.dart
+++ app_dart/lib/src/request_handlers/dart_internal_subscription.dart
@@ -66,6 +66,7 @@ class DartInternalSubscription extends SubscriptionHandler {
 
     // Only publish the parent release_builder builds to the datastore.
     // TODO(drewroengoogle): Remove this regex in favor of supporting *all* dart-internal build results.
+    // Issue: https://github.com/flutter/flutter/issues/134674
     final regex =
         RegExp(r'(Linux|Mac|Windows)\s+(engine_release_builder|packaging_release_builder|flutter_release_builder)');
     if (!regex.hasMatch(builder)) {
