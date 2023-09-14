45159e26e2517a8e1f381d32e8f2df18d50e30fe
Ricardo Amador <ricardoamador@google.com>
2023-09-14 14:55:08 -0700

    Added missing licenses.

--- auto_submit/test/src/validations/fake_required_check_runs.dart
+++ auto_submit/test/src/validations/fake_required_check_runs.dart
@@ -1,3 +1,7 @@
+// Copyright 2023 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:auto_submit/validations/required_check_runs.dart';
 import 'package:auto_submit/model/auto_submit_query_result.dart' as auto;
 
--- auto_submit/test/src/validations/fake_validation_filter.dart
+++ auto_submit/test/src/validations/fake_validation_filter.dart
@@ -1,3 +1,7 @@
+// Copyright 2023 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:auto_submit/validations/validation.dart';
 import 'package:auto_submit/validations/validation_filter.dart';
 
