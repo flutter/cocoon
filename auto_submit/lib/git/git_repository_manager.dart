31f6f444f7a5d4cdebee54b492ba760911d4a522
Ricardo Amador <32242716+ricardoamador@users.noreply.github.com>
2023-09-15 14:08:49 -0700

    Automatic Revert (Part 1): Base services needed for the automated revert. (#3083)
    This is the first part of the changes for Automated revert. These are the base classes that actually perform the revert. 
    
    There are two methods that can be used. Unfortunately the github graphql method does not currently work. I have an open issue with github but included the code here anyway.
    
    The git command line tools currently perform the revert. It checks out the repo, reverts the commit on the command line and then pushes the commit to github. It then creates a branch with the change which will be auto approved by the fluttergithubbot. Then ultimately merged.
    
    *List which issues are fixed by this PR. You must list at least one issue.*
    Part of https://github.com/flutter/flutter/issues/113867
    
    *If you had to change anything in the [flutter/tests] repo, include a link to the migration guide as per the [breaking change policy].*

---
+++ auto_submit/lib/git/git_repository_manager.dart
@@ -0,0 +1,100 @@
+// Copyright 2023 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io';
+import 'package:auto_submit/service/log.dart';
+import 'package:github/github.dart';
+import 'git_cli.dart';
+import 'utilities.dart';
+
+class GitRepositoryManager {
+  final RepositorySlug slug;
+  final String workingDirectory;
+  String? cloneToDirectory;
+  final GitCli gitCli;
+
+  late String targetCloneDirectory;
+
+  /// RepositoryManager will perform clone, revert and delete on the repository
+  /// in the working directory that is cloned to [cloneToDirectory].
+  ///
+  /// If the clonedToDirectory is not provided then the name of the repository
+  /// will be used as the cloneToDirectory.
+  GitRepositoryManager({
+    required this.slug,
+    //path/to/working/directory
+    required this.workingDirectory,
+    //reponame_commitSha
+    this.cloneToDirectory,
+    required this.gitCli,
+  }) {
+    cloneToDirectory ??= slug.name;
+    targetCloneDirectory = '$workingDirectory/$cloneToDirectory';
+  }
+
+  /// Clone the repository identified by the slug.
+  ///
+  /// Throw out rather than decomposing the return codes from the ProcessResult.
+  Future<void> cloneRepository() async {
+    if (Directory(targetCloneDirectory).existsSync()) {
+      // Blow the directory away instead of trying to update it.
+      Directory(targetCloneDirectory).deleteSync(recursive: true);
+    }
+
+    // Checking out a sparse copy will not checkout source files but will still
+    // allow a revert since we only care about the commitSha.
+    // Source: https://git-scm.com/docs/git-clone
+    await gitCli.cloneRepository(
+      slug: slug,
+      workingDirectory: workingDirectory,
+      targetDirectory: targetCloneDirectory,
+      options: ['--sparse'],
+    );
+  }
+
+  Future<void> setupConfig() async {
+    await gitCli.setupUserConfig(slug: slug, workingDirectory: targetCloneDirectory);
+    await gitCli.setupUserEmailConfig(slug: slug, workingDirectory: targetCloneDirectory);
+  }
+
+  /// Revert a commit in the current repository.
+  ///
+  /// The [baseBranchName] is the branch we want to branch from. In this case it
+  /// will almost always be the default branch name. The target branch is
+  /// preformatted with the commitSha.
+  Future<void> revertCommit(String baseBranchName, String commitSha, RepositorySlug slug, String token) async {
+    final GitRevertBranchName revertBranchName = GitRevertBranchName(commitSha);
+    // Working directory for these must be repo checkout directory.
+    await gitCli.createBranch(
+      newBranchName: revertBranchName.branch,
+      workingDirectory: targetCloneDirectory,
+      useCheckout: true,
+    );
+
+    await gitCli.setUpstream(
+      slug: slug,
+      workingDirectory: targetCloneDirectory,
+      branchName: revertBranchName.branch,
+      token: token,
+    );
+
+    await gitCli.revertChange(
+      commitSha: commitSha,
+      workingDirectory: targetCloneDirectory,
+    );
+
+    await gitCli.pushBranch(
+      branchName: revertBranchName.branch,
+      workingDirectory: targetCloneDirectory,
+    );
+  }
+
+  /// Delete the repository managed by this instance.
+  Future<void> deleteRepository() async {
+    log.info('Deleting clone directory $targetCloneDirectory');
+    if (Directory(targetCloneDirectory).existsSync()) {
+      Directory(targetCloneDirectory).deleteSync(recursive: true);
+    }
+  }
+}
---
+++ auto_submit/lib/service/revert_issue_body_formatter.dart
@@ -0,0 +1,48 @@
+// Copyright 2023 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:github/github.dart';
+
+class RevertIssueBodyFormatter {
+  RevertIssueBodyFormatter({
+    required this.slug,
+    required this.originalPrNumber,
+    required this.initiatingAuthor,
+    required this.originalPrTitle,
+    required this.originalPrBody,
+  });
+
+  // Possible format for the revert issue
+  final RepositorySlug slug;
+  final String initiatingAuthor;
+  final int originalPrNumber;
+  final String originalPrTitle;
+  final String originalPrBody;
+
+  late String? revertPrTitle;
+  late String? revertPrBody;
+  late String? revertPrLink;
+
+  RevertIssueBodyFormatter get format {
+    // Create the title for the revert issue.
+    revertPrTitle = 'Reverts "$originalPrTitle"';
+
+    // create the reverts Link for the body. Looks like Reverts flutter/cocoon#123 but will render as a link.
+    revertPrLink = 'Reverts ${slug.fullName}#$originalPrNumber';
+
+    // Create the body for the revert issue.
+    revertPrBody = '''
+$revertPrLink
+Initiated by: $initiatingAuthor
+This change reverts the following previous change:
+$originalPrBody
+''';
+
+    return this;
+  }
+
+  String? get formattedRevertPrTitle => revertPrTitle;
+
+  String? get formattedRevertPrBody => revertPrBody;
+}
