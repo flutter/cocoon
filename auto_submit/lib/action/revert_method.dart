ee1dca099d2073f9703e6d8cc394c07462f95909
Ricardo Amador <32242716+ricardoamador@users.noreply.github.com>
2023-09-25 11:27:18 -0700

    Updated to remove hardcoded test strings (#3101)
    Had some left over test strings that I unfortunately missed when submitting. This has caused some failures in flutter as the default branch there is master.
    
    *List which issues are fixed by this PR. You must list at least one issue.*
    
    *If you had to change anything in the [flutter/tests] repo, include a link to the migration guide as per the [breaking change policy].*

--- auto_submit/lib/action/revert_method.dart
+++ auto_submit/lib/action/revert_method.dart
@@ -7,5 +7,5 @@ import 'package:github/github.dart' as github;
 
 abstract class RevertMethod {
   // Allows substitution of the method of creating the revert request.
-  Future<Object?> createRevert(Config config, github.PullRequest pullRequest);
+  Future<Object?> createRevert(Config config, String initiatingAuthor, github.PullRequest pullRequest);
 }
