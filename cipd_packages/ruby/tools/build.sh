f2698f1ec7064aa4e62cea648c324cf1af46dd4d
Godofredo Contreras <godofredoc@google.com>
2023-10-11 10:01:20 -0700

    Update license for tools/build.sh.

--- cipd_packages/ruby/tools/build.sh
+++ cipd_packages/ruby/tools/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Copyright 2019 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 # Script to build a self contained ruby cipd packages.
 
