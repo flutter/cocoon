499b4e939eaf2404e87462df2b049e68d259030e
Todd Volkert <tvolkert@users.noreply.github.com>
2023-10-31 15:16:39 -0700

    Move cellSize from constant to inherited value (#3200)
    This allows us to use variable cell sizes and still have the tests pass.

--- dashboard/lib/widgets/task_box.dart
+++ dashboard/lib/widgets/task_box.dart
@@ -2,13 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 typedef ShowSnackBarCallback = ScaffoldFeatureController<SnackBar, SnackBarClosedReason> Function(SnackBar snackBar);
 
-class TaskBox {
-  /// How big to make each square in the grid.
-  static const double cellSize = 36;
+class TaskBox extends StatelessWidget {
+  const TaskBox({super.key, this.cellSize, required this.child});
+
+  final Widget child;
+  final double? cellSize;
+
+  static const double _kDefaultCellSize = 20;
+  static const double _kWebCellSize = 36;
 
   /// Status messages that map to [TaskStatus] enums.
   // TODO(chillers): Remove these and use TaskStatus enum when available. https://github.com/flutter/cocoon/issues/441
@@ -34,4 +40,37 @@ class TaskBox {
     statusInfraFailure: Colors.purple,
     statusInProgress: Colors.yellow,
   };
+
+  /// Returns the cell size of the nearest task box, or null if there is no
+  /// nearest task box.
+  static double? maybeOf(BuildContext context) {
+    final _TaskBox? box = context.dependOnInheritedWidgetOfExactType<_TaskBox>();
+    return box?.size;
+  }
+
+  /// Returns the cell size of the nearest task box.
+  static double of(BuildContext context) {
+    return maybeOf(context) ?? _kDefaultCellSize;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double size = cellSize ?? (kIsWeb ? _kWebCellSize : _kDefaultCellSize);
+    return _TaskBox(
+      size: TaskBox.maybeOf(context) ?? size,
+      child: child,
+    );
+  }
+}
+
+class _TaskBox extends InheritedWidget {
+  const _TaskBox({
+    required this.size,
+    required super.child,
+  });
+
+  final double size;
+
+  @override
+  bool updateShouldNotify(covariant _TaskBox oldWidget) => size != oldWidget.size;
 }
