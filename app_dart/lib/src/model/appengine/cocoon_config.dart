d6aa965c36a5469f8b1f04798213a55f05712472
Casey Hillers <chillers@google.com>
2023-12-20 13:18:59 -0800

    [app_dart] Refactor server to run locally

--- app_dart/lib/src/model/appengine/cocoon_config.dart
+++ app_dart/lib/src/model/appengine/cocoon_config.dart
@@ -6,6 +6,14 @@ import 'package:gcloud/db.dart';
 
 @Kind(name: 'CocoonConfig', idType: IdType.String)
 class CocoonConfig extends Model<String> {
+  CocoonConfig();
+
+  /// Helper function only for local and test environments.
+  CocoonConfig.fake(Key<String>? key, this.value) {
+    parentKey = key?.parent;
+    id = key?.id;
+  }
+
   @StringProperty(propertyName: 'ParameterValue')
   late String value;
 }
