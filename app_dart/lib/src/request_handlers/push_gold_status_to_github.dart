28618c81050e9b629c2fcb381c4b81b709c5a556
Matan Lurey <matan@lurey.org>
2024-02-21 09:49:15 -0800

    Fix contains check.

--- app_dart/lib/src/request_handlers/push_gold_status_to_github.dart
+++ app_dart/lib/src/request_handlers/push_gold_status_to_github.dart
@@ -125,14 +125,14 @@ class PushGoldStatusToGithub extends ApiRequestHandler<Body> {
             'linux_host_engine',
             'mac_host_engine',
             'linux_web_engine',
-          ].any(name.contains)) {
+          ].any((String shard) => shard.contains(name))) {
             runsGoldenFileTests = true;
           }
         } else if (slug == Config.flutterSlug) {
           if (const <String>[
             'framework',
             'misc',
-          ].any(name.contains)) {
+          ].any((String shard) => shard.contains(name))) {
             runsGoldenFileTests = true;
           }
         }
