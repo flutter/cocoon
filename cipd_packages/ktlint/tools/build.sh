e553f67326379b528bf261f610fd47d3386fe037
Gray Mackall <34871572+gmackall@users.noreply.github.com>
2024-02-29 13:15:00 -0800

    Ktlint in cipd (#3522)
    Creates a [ktlint](https://github.com/pinterest/ktlint/) cipd package, for use in https://github.com/flutter/flutter/pull/143478.

---
+++ cipd_packages/ktlint/tools/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Copyright 2019 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Script to build a self contained ktlint cipd package.
+
+set -e
+set -x
+
+# Get the directory of this script.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Get versions from config files.
+KTLINT_MAJOR_VERSION=$(cat $DIR/../ktlint_metadata.txt | cut -d ',' -f 1)
+KTLINT_FILE_NAME=$(cat $DIR/../ktlint_metadata.txt | cut -d ',' -f 2)
+
+# Create the package structure.
+rm -rf $DIR/../build && mkdir -p $DIR/../build
+mkdir -p $DIR/../build/ktlint
+
+curl https://github.com/pinterest/ktlint/releases/download/"$KTLINT_MAJOR_VERSION"/"$KTLINT_FILE_NAME" -o $DIR/../build/ktlint
