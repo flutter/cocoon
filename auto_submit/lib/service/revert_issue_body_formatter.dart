04129ed8da0ac37bf5067ff7114664f23a83f2c8
Ricardo Amador <ricardoamador@google.com>
2024-02-29 16:37:49 -0800

    Add semicolons to fields and remove some unneeded text.

--- auto_submit/lib/service/revert_issue_body_formatter.dart
+++ auto_submit/lib/service/revert_issue_body_formatter.dart
@@ -38,14 +38,14 @@ class RevertIssueBodyFormatter {
     revertPrTitle = 'Reverts "$prToRevertTitle (#$prToRevertNumber)"';
 
     // create the reverts Link for the body. Looks like Reverts flutter/cocoon#123 but will render as a link.
-    revertPrLink = 'Reverts ${slug.fullName}#$prToRevertNumber';
+    revertPrLink = '${slug.fullName}#$prToRevertNumber';
 
     prToRevertBody ??= 'No description provided.';
 
     // Create the body for the revert issue.
     revertPrBody = '''
 <!-- start_original_pr_link -->
-$revertPrLink
+Reverts: $revertPrLink
 <!-- end_original_pr_link -->
 <!-- start_initiating_author -->
 Initiated by: $initiatingAuthor
@@ -63,7 +63,6 @@ Reviewed By: ${SetBase.setToString(prToRevertReviewers)}
 
 <!-- start_revert_body -->
 This change reverts the following previous change:
-Original Description:
 $prToRevertBody
 <!-- end_revert_body -->
 ''';
