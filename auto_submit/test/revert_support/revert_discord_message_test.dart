5eb9760e6ec4073ce163ab861d25a1217ef12875
Ricardo Amador <ricardoamador@google.com>
2024-02-29 16:11:53 -0800

    Adding missing license.

--- auto_submit/test/revert_support/revert_discord_message_test.dart
+++ auto_submit/test/revert_support/revert_discord_message_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2024 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:auto_submit/revert_support/revert_discord_message.dart';
 import 'package:test/test.dart';
 
