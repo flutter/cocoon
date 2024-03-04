f87b561ab920f2097c076d581ef32f7f8c3cba16
Gray Mackall <mackall@google.com>
2024-03-04 11:56:46 -0800

    fix the outpit directory for ktlint

--- cipd_packages/ktlint/tools/build.sh
+++ cipd_packages/ktlint/tools/build.sh
@@ -19,4 +19,4 @@ KTLINT_FILE_NAME=$(cat $DIR/../ktlint_metadata.txt | cut -d ',' -f 2)
 rm -rf $DIR/../build && mkdir -p $DIR/../build
 mkdir -p $DIR/../build/ktlint
 
-curl https://github.com/pinterest/ktlint/releases/download/"$KTLINT_MAJOR_VERSION"/"$KTLINT_FILE_NAME" -o $DIR/../build/ktlint
+curl https://github.com/pinterest/ktlint/releases/download/"$KTLINT_MAJOR_VERSION"/"$KTLINT_FILE_NAME" -o $DIR/../build/ktlint/ktlint
