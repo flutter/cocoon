9d3d9272bbd6efe5dc1fded46cabcf23f215df75
Ricardo Amador <ricardoamador@google.com>
2024-03-05 14:54:09 -0800

    Rename directory for clarity

---
+++ auto_submit/lib/revert/revert_discord_message.dart
@@ -0,0 +1,30 @@
+// Copyright 2024 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:auto_submit/model/discord_message.dart';
+
+class RevertDiscordMessage extends Message {
+  static const String _username = 'Revert bot';
+  static const int discordMessageLength = 2000;
+  static const int elipsesOffset = 3;
+
+  RevertDiscordMessage({super.content, super.username, super.avatarUrl});
+
+  static RevertDiscordMessage generateMessage(
+    String originalPrUrl,
+    String revertPrUrl,
+    String initiatingAuthor,
+    String reasonForRevert,
+  ) {
+    final String content = '''
+Pull Request $originalPrUrl has been reverted by $initiatingAuthor here: $revertPrUrl.
+Reason for Revert: $reasonForRevert''';
+
+    final String truncatedContent = content.length <= discordMessageLength
+        ? content
+        : '${content.substring(0, discordMessageLength - elipsesOffset)}...';
+
+    return RevertDiscordMessage(content: truncatedContent, username: _username);
+  }
+}
---
+++ auto_submit/lib/revert/revert_issue_body_formatter.dart
@@ -0,0 +1,76 @@
+// Copyright 2023 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:github/github.dart';
+import 'dart:collection';
+
+class RevertIssueBodyFormatter {
+  RevertIssueBodyFormatter({
+    required this.slug,
+    required this.prToRevertNumber,
+    required this.initiatingAuthor,
+    required this.revertReason,
+    required this.prToRevertAuthor,
+    required this.prToRevertReviewers,
+    required this.prToRevertTitle,
+    required this.prToRevertBody,
+  });
+
+  // Possible format for the revert issue
+  RepositorySlug slug;
+  String initiatingAuthor;
+  int prToRevertNumber;
+  // These two fields can be null in the original pull request.
+  String? prToRevertTitle;
+  String? prToRevertBody;
+  String? revertReason;
+  String? prToRevertAuthor;
+  Set<String> prToRevertReviewers;
+
+  late String? revertPrTitle;
+  late String? revertPrBody;
+  late String? revertPrLink;
+
+  RevertIssueBodyFormatter get format {
+    // Create the title for the revert issue.
+    prToRevertTitle ??= 'No title provided.';
+    revertPrTitle = 'Reverts "$prToRevertTitle (#$prToRevertNumber)"';
+
+    // create the reverts Link for the body. Looks like Reverts flutter/cocoon#123 but will render as a link.
+    revertPrLink = '${slug.fullName}#$prToRevertNumber';
+
+    prToRevertBody ??= 'No description provided.';
+
+    // Create the body for the revert issue.
+    revertPrBody = '''
+<!-- start_original_pr_link -->
+Reverts: $revertPrLink
+<!-- end_original_pr_link -->
+<!-- start_initiating_author -->
+Initiated by: $initiatingAuthor
+<!-- end_initiating_author -->
+<!-- start_revert_reason -->
+Reason for reverting: $revertReason
+<!-- end_revert_reason -->
+<!-- start_original_pr_author -->
+Original PR Author: $prToRevertAuthor
+<!-- end_original_pr_author -->
+
+<!-- start_reviewers -->
+Reviewed By: ${SetBase.setToString(prToRevertReviewers)}
+<!-- end_reviewers -->
+
+<!-- start_revert_body -->
+This change reverts the following previous change:
+$prToRevertBody
+<!-- end_revert_body -->
+''';
+
+    return this;
+  }
+
+  String? get formattedRevertPrTitle => revertPrTitle;
+
+  String? get formattedRevertPrBody => revertPrBody;
+}
