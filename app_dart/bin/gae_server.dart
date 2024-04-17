2d086c4ef62dab795b943b4bc16a2f2e0096ec09
Ricardo Amador <ricardoamador@google.com>
2024-04-16 19:19:57 -0700

    Adding server classes with buildbucket v2 support.

--- app_dart/bin/gae_server.dart
+++ app_dart/bin/gae_server.dart
@@ -7,7 +7,11 @@ import 'dart:io';
 import 'package:appengine/appengine.dart';
 import 'package:cocoon_service/cocoon_service.dart';
 import 'package:cocoon_service/server.dart';
+import 'package:cocoon_service/src/service/build_bucket_v2_client.dart';
 import 'package:cocoon_service/src/service/commit_service.dart';
+import 'package:cocoon_service/src/service/github_checks_service_v2.dart';
+import 'package:cocoon_service/src/service/luci_build_service_v2.dart';
+import 'package:cocoon_service/src/service/scheduler_v2.dart';
 import 'package:gcloud/db.dart';
 
 Future<void> main() async {
@@ -18,7 +22,12 @@ Future<void> main() async {
     final Config config = Config(dbService, cache);
     final AuthenticationProvider authProvider = AuthenticationProvider(config: config);
     final AuthenticationProvider swarmingAuthProvider = SwarmingAuthenticationProvider(config: config);
+
     final BuildBucketClient buildBucketClient = BuildBucketClient(
+      accessTokenService: AccessTokenService.defaultProvider(config),
+    );
+
+    final BuildBucketV2Client buildBucketV2Client = BuildBucketV2Client(
       accessTokenService: AccessTokenService.defaultProvider(config),
     );
 
@@ -27,11 +36,23 @@ Future<void> main() async {
       config: config,
       cache: cache,
       buildBucketClient: buildBucketClient,
+      buildBucketV2Client: buildBucketV2Client,
+      pubsub: const PubSub(),
+    );
+
+    final LuciBuildServiceV2 luciBuildServiceV2 = LuciBuildServiceV2(
+      config: config,
+      cache: cache,
+      buildBucketV2Client: buildBucketV2Client,
       pubsub: const PubSub(),
     );
 
     /// Github checks api service used to provide luci test execution status on the Github UI.
     final GithubChecksService githubChecksService = GithubChecksService(
+      config,
+    );
+
+    final GithubChecksServiceV2 githubChecksServiceV2 = GithubChecksServiceV2(
       config,
     );
 
@@ -44,6 +65,13 @@ Future<void> main() async {
       config: config,
       githubChecksService: githubChecksService,
       luciBuildService: luciBuildService,
+    );
+
+    final SchedulerV2 schedulerV2 = SchedulerV2(
+      cache: cache,
+      config: config,
+      githubChecksService: githubChecksServiceV2,
+      luciBuildService: luciBuildServiceV2,
     );
 
     final BranchService branchService = BranchService(
@@ -59,10 +87,14 @@ Future<void> main() async {
       authProvider: authProvider,
       branchService: branchService,
       buildBucketClient: buildBucketClient,
+      buildBucketV2Client: buildBucketV2Client,
       gerritService: gerritService,
       scheduler: scheduler,
+      schedulerV2: schedulerV2,
       luciBuildService: luciBuildService,
+      luciBuildServiceV2: luciBuildServiceV2,
       githubChecksService: githubChecksService,
+      githubChecksServiceV2: githubChecksServiceV2,
       commitService: commitService,
       swarmingAuthProvider: swarmingAuthProvider,
     );
