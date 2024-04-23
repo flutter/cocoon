2eeeddd500394e172a3d6a8df9e256d2cf03b87b
Ricardo Amador <32242716+ricardoamador@users.noreply.github.com>
2024-04-23 09:37:27 -0700

    Add BuildBucket v2 migration service file changes (Part 3) (#3665)
    Adds the service class changes to support the v2 build bucket api migration. These classes support the rest endpoint classes and facilitate scheduling and are the main changes needed for scheduling and requesting builds vi the v2 api.
    
    Includes files from https://github.com/flutter/cocoon/pull/3664.
    
    *List which issues are fixed by this PR. You must list at least one issue.*
    Part 3 of https://github.com/flutter/flutter/issues/135934
    
    *If you had to change anything in the [flutter/tests] repo, include a link to the migration guide as per the [breaking change policy].*

--- app_dart/bin/gae_server.dart
+++ app_dart/bin/gae_server.dart
@@ -7,7 +7,11 @@ import 'dart:io';
 import 'package:appengine/appengine.dart';
 import 'package:cocoon_service/cocoon_service.dart';
 import 'package:cocoon_service/server.dart';
+import 'package:cocoon_service/src/service/build_bucket_v2_client.dart';
 import 'package:cocoon_service/src/service/commit_service.dart';
+// import 'package:cocoon_service/src/service/github_checks_service_v2.dart';
+// import 'package:cocoon_service/src/service/luci_build_service_v2.dart';
+// import 'package:cocoon_service/src/service/scheduler_v2.dart';
 import 'package:gcloud/db.dart';
 
 Future<void> main() async {
@@ -18,7 +22,12 @@ Future<void> main() async {
     final Config config = Config(dbService, cache);
     final AuthenticationProvider authProvider = AuthenticationProvider(config: config);
     final AuthenticationProvider swarmingAuthProvider = SwarmingAuthenticationProvider(config: config);
+
     final BuildBucketClient buildBucketClient = BuildBucketClient(
+      accessTokenService: AccessTokenService.defaultProvider(config),
+    );
+
+    final BuildBucketV2Client buildBucketV2Client = BuildBucketV2Client(
       accessTokenService: AccessTokenService.defaultProvider(config),
     );
 
@@ -27,13 +36,25 @@ Future<void> main() async {
       config: config,
       cache: cache,
       buildBucketClient: buildBucketClient,
+      buildBucketV2Client: buildBucketV2Client,
       pubsub: const PubSub(),
     );
+
+    // final LuciBuildServiceV2 luciBuildServiceV2 = LuciBuildServiceV2(
+    //   config: config,
+    //   cache: cache,
+    //   buildBucketV2Client: buildBucketV2Client,
+    //   pubsub: const PubSub(),
+    // );
 
     /// Github checks api service used to provide luci test execution status on the Github UI.
     final GithubChecksService githubChecksService = GithubChecksService(
       config,
     );
+
+    // final GithubChecksServiceV2 githubChecksServiceV2 = GithubChecksServiceV2(
+    //   config,
+    // );
 
     // Gerrit service class to communicate with GoB.
     final GerritService gerritService = GerritService(config: config);
@@ -45,6 +66,13 @@ Future<void> main() async {
       githubChecksService: githubChecksService,
       luciBuildService: luciBuildService,
     );
+
+    // final SchedulerV2 schedulerV2 = SchedulerV2(
+    //   cache: cache,
+    //   config: config,
+    //   githubChecksService: githubChecksServiceV2,
+    //   luciBuildService: luciBuildServiceV2,
+    // );
 
     final BranchService branchService = BranchService(
       config: config,
