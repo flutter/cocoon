343ebefa906cf42a4e0c6d4b447a7fe3866a9fbc
Christopher Fujino <christopherfujino@gmail.com>
2024-10-31 16:23:47 -0700

    refactor bash to give better error reporting

--- dev/provision_salt.sh
+++ dev/provision_salt.sh
@@ -11,21 +11,24 @@ MINION_PLIST_PATH=/Library/LaunchDaemons/com.saltstack.salt.minion.plist
 # Installs salt minion.
 # Pins the version to 2019.2.0 and Python 2 to be compatible with Fuchsia salt master.
 function install_salt() {
-  if [[ "$(uname)" == 'Darwin' ]]; then
+  OS="$(uname)"
+  if [[ "$OS" == 'Darwin' ]]; then
     curl https://packages.broadcom.com/artifactory/saltproject-generic/macos/3006.9/salt-3006.9-py3-x86_64.pkg -o /tmp/salt.pkg
     sudo installer -pkg /tmp/salt.pkg -target /
-  elif [[ "$(lsb_release -is)" == 'Debian' ]]; then
-    wget -O - https://repo.saltproject.io/py3/debian/10/amd64/3002/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-    echo 'deb http://repo.saltproject.io/py3/debian/10/amd64/3002 buster main' | sudo tee /etc/apt/sources.list.d/saltstack.list
-    # Also provision debian backports for m2crypto
-    echo 'deb http://deb.debian.org/debian buster-backports main' | sudo tee /etc/apt/sources.list.d/backports.list
-    sudo apt update
-    sudo apt install salt-minion
-  elif [[ "$(lsb_release -is)" == 'Ubuntu' ]]; then
-    sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest/salt-archive-keyring.gpg
-    echo 'deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/20.04/amd64/3005/ focal main' | sudo tee /etc/apt/sources.list.d/salt.list
-    sudo apt update
-    sudo apt install salt-minion
+  elif [[ "$OS" == 'Linux' ]]; then
+    DISTRO="$(lsb_release -is)"
+    if [[ "$DISTRO" == 'Ubuntu' ]]; then
+      sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest/salt-archive-keyring.gpg
+      echo 'deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/20.04/amd64/3005/ focal main' | sudo tee /etc/apt/sources.list.d/salt.list
+      sudo apt update
+      sudo apt install salt-minion
+    else
+      echo "Unsupported Linux distribution: $DISTRO" >&2
+      exit 1
+    fi
+  else
+    echo "Unsupported operating system $OS" >&2
+    exit 1
   fi
 }
 
@@ -45,7 +48,7 @@ function config_minion() {
 }
 
 function set_deviceos_grains() {
-  if [ ! -z "$1" ]; then
+  if [ -n "$1" ]; then
     sudo /opt/salt/bin/salt-call grains.set 'device_os' "$1"
   fi
 }
