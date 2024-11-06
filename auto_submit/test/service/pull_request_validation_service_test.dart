f299decdad43b88a6e7c8a53f0d23235a4f94854
Yegor Jbanov <yjbanov@google.com>
2024-11-05 16:45:08 -0800

    use minimal number of arguments to enqueue PRs

--- auto_submit/test/service/pull_request_validation_service_test.dart
+++ auto_submit/test/service/pull_request_validation_service_test.dart
@@ -473,9 +473,7 @@ This is the second line in a paragraph.''');
       expect(
         mutationOptions,
         {
-          'clientMutationId': null,
           'pullRequestId': '1',
-          'expectedHeadOid': '6dcb09b5b57875f334f61aebed695e2e4193db5e',
           'jump': false,
         },
       );
@@ -512,9 +510,7 @@ This is the second line in a paragraph.''');
       expect(
         mutationOptions,
         {
-          'clientMutationId': null,
           'pullRequestId': '1',
-          'expectedHeadOid': '6dcb09b5b57875f334f61aebed695e2e4193db5e',
           'jump': false,
         },
       );
@@ -555,9 +551,7 @@ This is the second line in a paragraph.''');
       expect(
         mutationOptions,
         {
-          'clientMutationId': null,
           'pullRequestId': '1',
-          'expectedHeadOid': '6dcb09b5b57875f334f61aebed695e2e4193db5e',
           'jump': true,
         },
       );
