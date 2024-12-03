4be5513d8d2fdc944eb2a3d9f63705d75e0b167a
John McDole <codefu@google.com>
2024-12-03 15:15:27 -0800

    line length

--- app_dart/bin/local_server.dart
+++ app_dart/bin/local_server.dart
@@ -49,7 +49,6 @@ Future<void> main() async {
   // Gerrit service class to communicate with GoB.
   final GerritService gerritService = GerritService(config: config);
 
-
   /// Cocoon scheduler service to manage validating commits in presubmit and postsubmit.
   final Scheduler scheduler = Scheduler(
     cache: cache,
