fb1b2358e94b205777f450f7d8250643311492c6
Matan Lurey <matan@lurey.org>
2025-03-11 17:22:23 -0700

    ++

--- dashboard/test/utils/golden.dart
+++ dashboard/test/utils/golden.dart
@@ -32,7 +32,7 @@ Future<void> expectGoldenMatches(
     actual,
     matchesGoldenFile(goldenPath),
     reason: reason,
-    skip: skip is String || skip == true || !Platform.isLinux,
+    skip: skip is String || skip == true || kIsWeb || !Platform.isLinux,
   );
 }
 
@@ -48,7 +48,7 @@ class CocoonFileComparator extends LocalFileComparator {
 
     if (!result.passed && result.diffPercent > _kGoldenDiffTolerance) {
       final error = await generateFailureOutput(result, golden, basedir);
-      if (Platform.environment.containsKey('LUCI_CONTEXT')) {
+      if (!kIsWeb && Platform.environment.containsKey('LUCI_CONTEXT')) {
         stderr.writeln(
           '$golden has failed. For your convenience CI provides it as a base64 encoded image below. #[IMAGE]:',
         );
@@ -57,7 +57,7 @@ class CocoonFileComparator extends LocalFileComparator {
       }
       throw FlutterError(error);
     }
-    if (!result.passed) {
+    if (!result.passed && !kIsWeb) {
       stderr.writeln(
         'A tolerable difference of ${result.diffPercent * 100}% was found when '
         'comparing $golden.',
