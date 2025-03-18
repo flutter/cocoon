dfac74f5ed4b0adee41c010a27e50d193356ff76
Matan Lurey <matan@lurey.org>
2025-03-18 13:25:04 -0700

    Remove unused files, add .gitattributes to make codereview better.

--- format.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Copyright 2020 The Flutter Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-set -e
-
-dart format --set-exit-if-changed .
