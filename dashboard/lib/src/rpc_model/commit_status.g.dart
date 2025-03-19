2246068464f2960e41093e505290bf784c7184ea
Matan Lurey <matan@lurey.org>
2025-03-18 18:43:52 -0700

    Format.

--- dashboard/lib/src/rpc_model/commit_status.g.dart
+++ dashboard/lib/src/rpc_model/commit_status.g.dart
@@ -7,20 +7,20 @@ part of 'commit_status.dart';
 // **************************************************************************
 
 CommitStatus _$CommitStatusFromJson(Map<String, dynamic> json) =>
-    $checkedCreate(
-      'CommitStatus',
-      json,
-      ($checkedConvert) {
-        final val = CommitStatus(
-          commit: $checkedConvert('commit',
-              (v) => CommitStatus._commitFromJson(v as Map<String, Object?>)),
-          tasks: $checkedConvert(
-              'tasks', (v) => CommitStatus._tasksFromJson(v as List)),
-          branch: $checkedConvert('branch', (v) => v as String),
-        );
-        return val;
-      },
-    );
+    $checkedCreate('CommitStatus', json, ($checkedConvert) {
+      final val = CommitStatus(
+        commit: $checkedConvert(
+          'commit',
+          (v) => CommitStatus._commitFromJson(v as Map<String, Object?>),
+        ),
+        tasks: $checkedConvert(
+          'tasks',
+          (v) => CommitStatus._tasksFromJson(v as List),
+        ),
+        branch: $checkedConvert('branch', (v) => v as String),
+      );
+      return val;
+    });
 
 Map<String, dynamic> _$CommitStatusToJson(CommitStatus instance) =>
     <String, dynamic>{
