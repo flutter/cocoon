542699a45994999cf83a87988c196c8d0ef0c2b5
Matan Lurey <matan@lurey.org>
2025-03-25 12:46:06 -0700

    ++

--- app_dart/lib/src/model/firestore/base.dart
+++ app_dart/lib/src/model/firestore/base.dart
@@ -1,3 +1,7 @@
+// Copyright 2019 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'dart:convert';
 
 import 'package:googleapis/firestore/v1.dart' as g;
