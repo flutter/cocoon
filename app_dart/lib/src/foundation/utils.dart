2233d1b642de7b488fd6fa64fa359ce692847e81
Jason Simmons <jason-simmons@users.noreply.github.com>
2025-03-28 17:25:55 +0000

    Disable the file-based check for whether a flutter/flutter commit is a Fusion/monorepo commit (#4406)
    This check recently had errors due to a GitHub outage (see https://github.com/flutter/flutter/issues/166110).  This caused Cocoon's isFusion cache to be populated with incorrect results for various commits.
    
    The check is now obsolete because there will not be any more release branches that use the legacy pre-monorepo layout.

--- app_dart/lib/src/foundation/utils.dart
+++ app_dart/lib/src/foundation/utils.dart
@@ -9,7 +9,6 @@ import 'dart:io';
 import 'package:cocoon_server/logging.dart';
 import 'package:github/github.dart';
 import 'package:googleapis/bigquery/v2.dart';
-import 'package:http/http.dart' as http;
 import 'package:retry/retry.dart';
 import 'package:yaml/yaml.dart';
 
@@ -64,18 +63,7 @@ Duration twoSecondLinearBackoff(int attempt) {
   return const Duration(seconds: 2) * (attempt + 1);
 }
 
-http.Client _defaultHttpClientProvider() => http.Client();
-
 class FusionTester {
-  final HttpClientProvider _httpClientProvider;
-
-  /// This is a lightweight in memory cache for commit-sha to isFusion
-  final _isFusionMap = <String, bool>{};
-
-  FusionTester({
-    http.Client Function() httpClientProvider = _defaultHttpClientProvider,
-  }) : _httpClientProvider = httpClientProvider;
-
   /// Tests if the [sha] is in flutter/flutter and engine assets are available.
   Future<bool> isFusionBasedRef(
     RepositorySlug slug,
@@ -83,70 +71,7 @@ class FusionTester {
     Duration timeout = _githubTimeout,
     RetryOptions retryOptions = _githubRetryOptions,
   }) async {
-    final cacheKey = '${slug.fullName}/$sha';
-    final cacheHit = _isFusionMap[cacheKey];
-    if (cacheHit != null) {
-      log.info('isFusionRef: cache hit for $cacheKey = $cacheHit');
-      return cacheHit;
-    }
-    final isFusion =
-        _isFusionMap[cacheKey] = await _isFusionBasedRefReal(
-          slug,
-          sha,
-          timeout,
-          retryOptions,
-        );
-    return isFusion;
-  }
-
-  Future<bool> _isFusionBasedRefReal(
-    RepositorySlug slug,
-    String sha,
-    Duration timeout,
-    RetryOptions retryOptions,
-  ) async {
-    if (slug != Config.flutterSlug) {
-      log.debug('isFusionRef: not a fusion ref - wrong slug($slug)');
-      return false;
-    }
-    try {
-      final files = await Future.wait([
-        githubFileContent(
-          slug,
-          'DEPS',
-          httpClientProvider: _httpClientProvider,
-          ref: sha,
-          timeout: timeout,
-          retryOptions: retryOptions,
-        ),
-        githubFileContent(
-          slug,
-          'engine/src/.gn',
-          httpClientProvider: _httpClientProvider,
-          ref: sha,
-          timeout: timeout,
-          retryOptions: retryOptions,
-        ),
-      ]);
-      if (files.any((contents) => contents.isEmpty)) {
-        log.debug(
-          'isFusionRef: not a fusion ref - DEPS or engine/src/.gn is empty',
-        );
-        return false;
-      }
-
-      log.debug('isFusionRef: fusion ref - ');
-      return true;
-    } on NotFoundException catch (e) {
-      log.debug(
-        "isFusionRef: 'DEPS' or 'engine/src/.gn' not found a fusion ref.",
-        e,
-      );
-      return false;
-    } catch (e) {
-      log.warn('isFusionRef: unknown error while testing', e);
-      rethrow;
-    }
+    return slug == Config.flutterSlug;
   }
 }
 
