affd69dd753cf0f409c2d9ddca5f878770e34499
Matan Lurey <matan@lurey.org>
2025-04-01 16:02:57 -0700

    Run dart format.

--- dashboard/lib/src/rpc_model/commit.g.dart
+++ dashboard/lib/src/rpc_model/commit.g.dart
@@ -7,54 +7,49 @@ part of 'commit.dart';
 // **************************************************************************
 
 Commit _$CommitFromJson(Map<String, dynamic> json) => $checkedCreate(
-      'Commit',
-      json,
-      ($checkedConvert) {
-        final val = Commit(
-          timestamp:
-              $checkedConvert('CreateTimestamp', (v) => (v as num).toInt()),
-          sha: $checkedConvert('Sha', (v) => v as String),
-          author: $checkedConvert('Author',
-              (v) => CommitAuthor.fromJson(v as Map<String, dynamic>)),
-          message: $checkedConvert('Message', (v) => v as String),
-          repository:
-              $checkedConvert('FlutterRepositoryPath', (v) => v as String),
-          branch: $checkedConvert('Branch', (v) => v as String),
-        );
-        return val;
-      },
-      fieldKeyMap: const {
-        'timestamp': 'CreateTimestamp',
-        'sha': 'Sha',
-        'author': 'Author',
-        'message': 'Message',
-        'repository': 'FlutterRepositoryPath',
-        'branch': 'Branch'
-      },
+  'Commit',
+  json,
+  ($checkedConvert) {
+    final val = Commit(
+      timestamp: $checkedConvert('CreateTimestamp', (v) => (v as num).toInt()),
+      sha: $checkedConvert('Sha', (v) => v as String),
+      author: $checkedConvert(
+        'Author',
+        (v) => CommitAuthor.fromJson(v as Map<String, dynamic>),
+      ),
+      message: $checkedConvert('Message', (v) => v as String),
+      repository: $checkedConvert('FlutterRepositoryPath', (v) => v as String),
+      branch: $checkedConvert('Branch', (v) => v as String),
     );
+    return val;
+  },
+  fieldKeyMap: const {
+    'timestamp': 'CreateTimestamp',
+    'sha': 'Sha',
+    'author': 'Author',
+    'message': 'Message',
+    'repository': 'FlutterRepositoryPath',
+    'branch': 'Branch',
+  },
+);
 
 Map<String, dynamic> _$CommitToJson(Commit instance) => <String, dynamic>{
-      'CreateTimestamp': instance.timestamp,
-      'Sha': instance.sha,
-      'Author': instance.author,
-      'Message': instance.message,
-      'FlutterRepositoryPath': instance.repository,
-      'Branch': instance.branch,
-    };
+  'CreateTimestamp': instance.timestamp,
+  'Sha': instance.sha,
+  'Author': instance.author,
+  'Message': instance.message,
+  'FlutterRepositoryPath': instance.repository,
+  'Branch': instance.branch,
+};
 
 CommitAuthor _$CommitAuthorFromJson(Map<String, dynamic> json) =>
-    $checkedCreate(
-      'CommitAuthor',
-      json,
-      ($checkedConvert) {
-        final val = CommitAuthor(
-          login: $checkedConvert('Login', (v) => v as String),
-          avatarUrl: $checkedConvert('avatar_url', (v) => v as String),
-        );
-        return val;
-      },
-      fieldKeyMap: const {'login': 'Login', 'avatarUrl': 'avatar_url'},
-    );
+    $checkedCreate('CommitAuthor', json, ($checkedConvert) {
+      final val = CommitAuthor(
+        login: $checkedConvert('Login', (v) => v as String),
+        avatarUrl: $checkedConvert('avatar_url', (v) => v as String),
+      );
+      return val;
+    }, fieldKeyMap: const {'login': 'Login', 'avatarUrl': 'avatar_url'});
 
 Map<String, dynamic> _$CommitAuthorToJson(CommitAuthor instance) =>
     <String, dynamic>{
--- dashboard/lib/src/rpc_model/commit_status.g.dart
+++ dashboard/lib/src/rpc_model/commit_status.g.dart
@@ -7,25 +7,21 @@ part of 'commit_status.dart';
 // **************************************************************************
 
 CommitStatus _$CommitStatusFromJson(Map<String, dynamic> json) =>
-    $checkedCreate(
-      'CommitStatus',
-      json,
-      ($checkedConvert) {
-        final val = CommitStatus(
-          commit: $checkedConvert(
-              'Commit', (v) => Commit.fromJson(v as Map<String, dynamic>)),
-          tasks: $checkedConvert(
-              'Tasks',
-              (v) => (v as List<dynamic>)
-                  .map((e) => Task.fromJson(e as Map<String, dynamic>))),
-        );
-        return val;
-      },
-      fieldKeyMap: const {'commit': 'Commit', 'tasks': 'Tasks'},
-    );
+    $checkedCreate('CommitStatus', json, ($checkedConvert) {
+      final val = CommitStatus(
+        commit: $checkedConvert(
+          'Commit',
+          (v) => Commit.fromJson(v as Map<String, dynamic>),
+        ),
+        tasks: $checkedConvert(
+          'Tasks',
+          (v) => (v as List<dynamic>).map(
+            (e) => Task.fromJson(e as Map<String, dynamic>),
+          ),
+        ),
+      );
+      return val;
+    }, fieldKeyMap: const {'commit': 'Commit', 'tasks': 'Tasks'});
 
 Map<String, dynamic> _$CommitStatusToJson(CommitStatus instance) =>
-    <String, dynamic>{
-      'Commit': instance.commit,
-      'Tasks': instance.tasks,
-    };
+    <String, dynamic>{'Commit': instance.commit, 'Tasks': instance.tasks};
