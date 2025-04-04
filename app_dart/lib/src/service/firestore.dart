144131c8d554bdab2015243aa51ab334ad282c06
Matan Lurey <matan@lurey.org>
2025-04-04 14:09:13 -0700

    Rewrite vacuum-stale-tasks to be Firestore first.

--- app_dart/lib/src/service/firestore.dart
+++ app_dart/lib/src/service/firestore.dart
@@ -9,6 +9,7 @@ import 'package:cocoon_server/google_auth_provider.dart';
 import 'package:github/github.dart';
 import 'package:googleapis/firestore/v1.dart';
 import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
 
 import '../../cocoon_service.dart';
 import '../model/firestore/commit.dart';
@@ -146,8 +147,14 @@ class FirestoreService {
   }
 
   /// Returns all tasks running against the speificed [commitSha].
-  Future<List<Task>> queryCommitTasks(String commitSha) async {
-    final filterMap = <String, Object>{'${Task.fieldCommitSha} =': commitSha};
+  Future<List<Task>> queryCommitTasks(
+    String commitSha, {
+    String? status,
+  }) async {
+    final filterMap = <String, Object>{
+      '${Task.fieldCommitSha} =': commitSha,
+      if (status != null) '${Task.fieldStatus} = ': status,
+    };
     final orderMap = <String, String>{
       Task.fieldCreateTimestamp: kQueryOrderDescending,
     };
@@ -157,6 +164,26 @@ class FirestoreService {
       orderMap: orderMap,
     );
     return [...documents.map(Task.fromDocument)];
+  }
+
+  /// Queries the last [commitLimit] commits, and returns the commit and tasks.
+  ///
+  /// For tasks with multiple attempts, only the most recent task is returned.
+  ///
+  /// If [status] is provided, only tasks matching the status are returned.
+  Future<List<CommitAndTasks>> queryRecentCommitsAndTasks(
+    RepositorySlug slug, {
+    required int commitLimit,
+    String? status,
+  }) async {
+    final commits = await queryRecentCommits(slug: slug, limit: commitLimit);
+    return [
+      for (final commit in commits)
+        CommitAndTasks(
+          commit,
+          await queryCommitTasks(commit.sha, status: status),
+        )._withMostRecentTaskOnly(),
+    ];
   }
 
   /// Queries the last updated Gold status for the [slug] and [prNumber].
@@ -373,4 +400,36 @@ List<Write> documentsToWrites(List<Document> documents, {bool? exists}) {
         ),
       )
       .toList();
+}
+
+/// A pairing of a [Commit] and [Task]s associated with that commit.
+@immutable
+final class CommitAndTasks {
+  /// Creates a [CommitAndTasks] with the provided commit and tasks.
+  CommitAndTasks(this.commit, Iterable<Task> tasks)
+    : tasks = List.unmodifiable(tasks);
+
+  /// Commit from Firestore.
+  final Commit commit;
+
+  /// Tasks where [Task.commitSha] is the same as [Commit.sha].
+  ///
+  /// This list is unmodifiable.`
+  final List<Task> tasks;
+
+  /// Returns a copy of `this` with only the most recent task per builder.
+  ///
+  /// For example, if a task `Linux foo` was run 3 times, only the most recent
+  /// task (`Linux foo`, `attempt = 3`) is retained in the accompanying [tasks]
+  /// list, and the rest of the tasks are removed.
+  @useResult
+  CommitAndTasks _withMostRecentTaskOnly() {
+    final mostRecent = <String, Task>{};
+    for (final task in tasks) {
+      mostRecent.update(task.taskName, (current) {
+        return current.currentAttempt > task.createTimestamp ? current : task;
+      }, ifAbsent: () => task);
+    }
+    return CommitAndTasks(commit, mostRecent.values);
+  }
 }
