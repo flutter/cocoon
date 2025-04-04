23a0b5988531c2cdf55bf57fef533c5241b7bed9
Matan Lurey <matan@lurey.org>
2025-04-04 11:36:53 -0700

    ++

--- app_dart/test/src/service/fake_firestore_service_test.dart
+++ app_dart/test/src/service/fake_firestore_service_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:cocoon_server_test/test_logging.dart';
 import 'package:cocoon_service/src/service/firestore.dart';
 import 'package:googleapis/firestore/v1.dart' as g;
 import 'package:test/test.dart';
@@ -9,6 +10,8 @@ import 'package:test/test.dart';
 import 'fake_firestore_service.dart';
 
 void main() {
+  useTestLoggerPerTest();
+
   late FakeFirestoreService firestore;
 
   setUp(() {
