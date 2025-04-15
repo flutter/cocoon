1234c2c2bc3768cb5a921b62925cd455ca1f8dc5
Matan Lurey <matanlurey@users.noreply.github.com>
2025-04-15 08:26:50 -0700

    Properly record a new task for a new `dart_internal` build ID. (#4513)
    I think this has been a bug for a while, but could have also been caused by the Firestore migration.
    
    After this PR, `dart_internal` should show re-runs and previous builds like expected.
    
    Closes https://github.com/flutter/flutter/issues/166935.
    
    /cc @reidbaker

--- app_dart/lib/src/service/luci_build_service.dart
+++ app_dart/lib/src/service/luci_build_service.dart
@@ -1108,8 +1108,7 @@ class LuciBuildService {
     fs.Task task,
   ) async {
     // Update task status in Firestore.
-    final newAttempt = task.currentAttempt + 1;
-    task.resetAsRetry(attempt: newAttempt);
+    task.resetAsRetry();
     task.setStatus(fs.Task.statusInProgress);
 
     final firestore = await _config.createFirestoreService();
@@ -1132,11 +1131,11 @@ class LuciBuildService {
       name: task.taskName,
     );
     dsExistingTask
-      ..attempts = newAttempt
+      ..attempts = task.currentAttempt
       ..status = Task.statusInProgress;
     await datastore.insert([dsExistingTask]);
 
-    return newAttempt;
+    return task.currentAttempt;
   }
 
   /// Check if a builder should be rerun.
