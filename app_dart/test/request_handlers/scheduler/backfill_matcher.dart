144a24e90c90fb10ae2c738c627498de833684a1
Matan Lurey <matanlurey@users.noreply.github.com>
2025-04-17 14:33:59 -0700

    No prod impact: complete (and test) `DefaultBackfillStrategy` (#4537)
    Towards https://github.com/flutter/flutter/issues/167222.
    
    Adds:
    - Replaces `(Super, Awkward, Tuple)` with `BackfillGrid` data structure, and tests.
    - Updates `DefaultBackfillStrategy` and adds tests

---
+++ app_dart/test/request_handlers/scheduler/backfill_matcher.dart
@@ -0,0 +1,223 @@
+// Copyright 2019 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:cocoon_service/src/request_handlers/scheduler/backfill_grid.dart';
+import 'package:cocoon_service/src/service/luci_build_service/opaque_commit.dart';
+import 'package:test/expect.dart';
+
+import '../../src/model/ci_yaml_matcher.dart';
+
+/// Returns a matcher that asserts the state of [BackfillGrid.targets].
+Matcher hasGridTargetsMatching(
+  Iterable<(TargetMatcher, List<OpaqueTaskMatcher>)> targets,
+) {
+  return _BackfillGridMatcher([...targets]);
+}
+
+// The default Dart matchers do not handle tuples well, so this matcher does
+// roughly what you'd want to do when comparing the shape of BackfillGrid to
+// an expected output.
+final class _BackfillGridMatcher extends Matcher {
+  const _BackfillGridMatcher(this._expected);
+  final List<(TargetMatcher, List<OpaqueTaskMatcher>)> _expected;
+
+  @override
+  Description describe(Description description) {
+    return description
+        .add('matches the grid shape ')
+        .addDescriptionOf(_expected);
+  }
+
+  @override
+  bool matches(Object? item, Map matchState) {
+    if (item is! BackfillGrid) {
+      return false;
+    }
+    final actual = item.targets.toList();
+    if (actual.length != _expected.length) {
+      return false;
+    }
+    var i = 0;
+    for (final (actualTarget, actualTasks) in item.targets) {
+      final (expectedTarget, expectedTasks) = _expected[i];
+      if (!expectedTarget.matches(actualTarget, {})) {
+        return false;
+      }
+      if (actualTasks.length != expectedTasks.length) {
+        return false;
+      }
+      for (var n = 0; n < actualTasks.length; n++) {
+        if (!expectedTasks[n].matches(actualTasks[n], {})) {
+          return false;
+        }
+      }
+      i++;
+    }
+    return true;
+  }
+}
+
+const isBackfillTask = BackfillTaskMatcher._(TypeMatcher());
+
+final class BackfillTaskMatcher extends Matcher {
+  const BackfillTaskMatcher._(this._delegate);
+  final TypeMatcher<BackfillTask> _delegate;
+
+  BackfillTaskMatcher hasTask(Object matcherOr) {
+    return BackfillTaskMatcher._(
+      _delegate.having((t) => t.task, 'task', matcherOr),
+    );
+  }
+
+  BackfillTaskMatcher hasTarget(Object matcherOr) {
+    return BackfillTaskMatcher._(
+      _delegate.having((t) => t.target, 'target', matcherOr),
+    );
+  }
+
+  BackfillTaskMatcher hasCommit(Object matcherOr) {
+    return BackfillTaskMatcher._(
+      _delegate.having((t) => t.commit, 'commit', matcherOr),
+    );
+  }
+
+  BackfillTaskMatcher hasPriority(Object matcherOr) {
+    return BackfillTaskMatcher._(
+      _delegate.having((t) => t.priority, 'priority', matcherOr),
+    );
+  }
+
+  @override
+  bool matches(Object? item, Map matchState) {
+    if (item is! BackfillTask) {
+      return false;
+    }
+
+    return _delegate.matches(item, matchState);
+  }
+
+  @override
+  Description describe(Description description) {
+    return _delegate.describe(description);
+  }
+
+  @override
+  Description describeMismatch(
+    Object? item,
+    Description mismatchDescription,
+    Map matchState,
+    _,
+  ) {
+    return _delegate.describeMismatch(
+      item,
+      mismatchDescription,
+      matchState,
+      false,
+    );
+  }
+}
+
+const isOpaqueCommit = OpaqueCommitMatcher._(TypeMatcher());
+
+final class OpaqueCommitMatcher extends Matcher {
+  const OpaqueCommitMatcher._(this._delegate);
+  final TypeMatcher<OpaqueCommit> _delegate;
+
+  OpaqueCommitMatcher hasSha(Object matcherOr) {
+    return OpaqueCommitMatcher._(
+      _delegate.having((c) => c.sha, 'sha', matcherOr),
+    );
+  }
+
+  OpaqueCommitMatcher hasBranch(Object matcherOr) {
+    return OpaqueCommitMatcher._(
+      _delegate.having((c) => c.branch, 'branch', matcherOr),
+    );
+  }
+
+  OpaqueCommitMatcher hasSlug(Object matcherOr) {
+    return OpaqueCommitMatcher._(
+      _delegate.having((c) => c.slug, 'slug', matcherOr),
+    );
+  }
+
+  @override
+  bool matches(Object? item, _) {
+    if (item is! OpaqueCommit) {
+      return false;
+    }
+
+    return _delegate.matches(item, {});
+  }
+
+  @override
+  Description describe(Description description) {
+    return _delegate.describe(description);
+  }
+
+  @override
+  Description describeMismatch(
+    Object? item,
+    Description mismatchDescription,
+    _,
+    _,
+  ) {
+    return _delegate.describeMismatch(item, mismatchDescription, {}, false);
+  }
+}
+
+const isOpaqueTask = OpaqueTaskMatcher._(TypeMatcher());
+
+final class OpaqueTaskMatcher extends Matcher {
+  const OpaqueTaskMatcher._(this._delegate);
+  final TypeMatcher<OpaqueTask> _delegate;
+
+  OpaqueTaskMatcher hasName(Object matcherOr) {
+    return OpaqueTaskMatcher._(
+      _delegate.having((t) => t.name, 'name', matcherOr),
+    );
+  }
+
+  OpaqueTaskMatcher hasCommitSha(Object matcherOr) {
+    return OpaqueTaskMatcher._(
+      _delegate.having((t) => t.commitSha, 'commitSha', matcherOr),
+    );
+  }
+
+  OpaqueTaskMatcher hasCurrentAttempt(Object matcherOr) {
+    return OpaqueTaskMatcher._(
+      _delegate.having((t) => t.currentAttempt, 'currentAttempt', matcherOr),
+    );
+  }
+
+  OpaqueTaskMatcher hasStatus(Object matcherOr) {
+    return OpaqueTaskMatcher._(
+      _delegate.having((t) => t.status, 'status', matcherOr),
+    );
+  }
+
+  @override
+  bool matches(Object? item, _) {
+    if (item is! OpaqueTask) {
+      return false;
+    }
+
+    return _delegate.matches(item, {});
+  }
+
+  @override
+  Description describe(Description description) {
+    return _delegate.describe(description);
+  }
+
+  @override
+  Description describeMismatch(
+    Object? item,
+    Description mismatchDescription,
+    _,
+    _,
+  ) {
+    return _delegate.describeMismatch(item, mismatchDescription, {}, false);
+  }
+}
---
+++ app_dart/test/request_handlers/scheduler/backfill_strategy_test.dart
@@ -0,0 +1,180 @@
+// Copyright 2019 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:math';
+
+import 'package:cocoon_server_test/test_logging.dart';
+import 'package:cocoon_service/src/model/firestore/task.dart' as fs;
+import 'package:cocoon_service/src/request_handlers/scheduler/backfill_grid.dart';
+import 'package:cocoon_service/src/request_handlers/scheduler/backfill_strategy.dart';
+import 'package:cocoon_service/src/service/luci_build_service/opaque_commit.dart';
+import 'package:test/test.dart';
+
+import '../../src/utilities/entity_generators.dart';
+import 'backfill_matcher.dart';
+
+void main() {
+  useTestLoggerPerTest();
+
+  group('DefaultBackfillStrategy', () {
+    // Pick a deterministic seed, because shuffling is involved.
+    final random = Random(0);
+    final strategy = DefaultBackfillStrategy(random);
+
+    final commits = [
+      for (var i = 0; i < 10; i++)
+        OpaqueCommit.fromFirestore(generateFirestoreCommit(i)),
+    ];
+
+    final targets = [
+      for (var i = 0; i < 10; i++) generateTarget(i, name: 'Linux TASK_$i'),
+    ];
+
+    OpaqueTask taskSucceeded(int commit, int index) {
+      return OpaqueTask.fromFirestore(
+        generateFirestoreTask(
+          index,
+          commitSha: commits[commit].sha,
+          status: fs.Task.statusSucceeded,
+          name: 'Linux TASK_$index',
+        ),
+      );
+    }
+
+    OpaqueTask taskNew(int commit, int index) {
+      return OpaqueTask.fromFirestore(
+        generateFirestoreTask(
+          index,
+          commitSha: commits[commit].sha,
+          status: fs.Task.statusNew,
+          name: 'Linux TASK_$index',
+        ),
+      );
+    }
+
+    OpaqueTask taskFailed(int commit, int index) {
+      return OpaqueTask.fromFirestore(
+        generateFirestoreTask(
+          index,
+          commitSha: commits[commit].sha,
+          status: fs.Task.statusFailed,
+          name: 'Linux TASK_$index',
+        ),
+      );
+    }
+
+    OpaqueTask taskInProgress(int commit, int index) {
+      return OpaqueTask.fromFirestore(
+        generateFirestoreTask(
+          index,
+          commitSha: commits[commit].sha,
+          status: fs.Task.statusInProgress,
+          name: 'Linux TASK_$index',
+        ),
+      );
+    }
+
+    late BackfillGrid grid;
+
+    tearDown(() {
+      printOnFailure(
+        'Grid contents on failure: ${grid.targets.toList().toString()}',
+      );
+    });
+
+    // INPUT:
+    // 🧑‍💼 ⬜ ⬜
+    // 🧑‍💼 🟩 🟨
+    //
+    // OUTPUT:
+    // 🧑‍💼 🟨 ⬜
+    // 🧑‍💼 🟩 🟨
+    test('skips tasks for targets where a task is already in progress', () {
+      // dart format off
+      grid = BackfillGrid.from([
+        //           Linux TASK_0           Linux TASK_1
+        (commits[0], [taskNew       (0, 0),        taskNew (0, 1)]),
+        (commits[1], [taskSucceeded (1, 0), taskInProgress (1, 1)]),
+      ], tipOfTreeTargets: [
+        targets[0],
+        targets[1],
+      ]);
+      // dart format on
+
+      expect(strategy.determineBackfill(grid), [
+        isBackfillTask.hasCommit(commits[0]).hasTarget(targets[0]),
+      ]);
+    });
+
+    // INPUT:
+    // 🧑‍💼 ⬜ ⬜
+    // 🧑‍💼 ⬜ ⬜
+    //
+    // OUTPUT:
+    // 🧑‍💼 🟨 🟨
+    // 🧑‍💼 ⬜ ⬜
+    test('only schedules one task per target', () {
+      // dart format off
+      grid = BackfillGrid.from([
+        //           Linux TASK_0            Linux TASK_1
+        (commits[0], [taskNew       (0, 0),  taskNew (0, 1)]),
+        (commits[1], [taskNew       (1, 0),  taskNew (1, 1)]),
+      ], tipOfTreeTargets: [
+        targets[0],
+        targets[1],
+      ]);
+      // dart format on
+
+      expect(strategy.determineBackfill(grid), [
+        isBackfillTask.hasCommit(commits[0]).hasTarget(targets[0]),
+        isBackfillTask.hasCommit(commits[0]).hasTarget(targets[1]),
+      ]);
+    });
+
+    // INPUT:
+    // 🧑‍💼 ⬜ ⬜ ⬜ kBatchSize = 6
+    // 🧑‍💼 ⬜ ⬜ ⬜ < 1
+    // 🧑‍💼 ⬜ ⬜ ⬜ < 2
+    // 🧑‍💼 ⬜ ⬜ ⬜ < 3
+    // 🧑‍💼 ⬜ ⬜ ⬜ < 4
+    // 🧑‍💼 ⬜ ⬜ 🟥 < 5
+    // 🧑‍💼 ⬜ 🟥 ⬜ < 6
+    // 🧑‍💼 🟥 ⬜ ⬜ < 7
+    //
+    // OUTPUT:
+    // 🧑‍💼 3️⃣ 1️⃣ 2️⃣ kBatchSize = 6
+    // 🧑‍💼 ⬜ ⬜ ⬜ < 1
+    // 🧑‍💼 ⬜ ⬜ ⬜ < 2
+    // 🧑‍💼 ⬜ ⬜ ⬜ < 3
+    // 🧑‍💼 ⬜ ⬜ ⬜ < 4
+    // 🧑‍💼 ⬜ ⬜ 🟥 < 5
+    // 🧑‍💼 ⬜ 🟥 ⬜ < 6
+    // 🧑‍💼 🟥 ⬜ ⬜ < 7
+    test('places previously failing within kBatchSize as high priority', () {
+      // dart format off
+      grid = BackfillGrid.from([
+        //           Linux TASK_0            Linux TASK_1          Linux TASK_2
+        (commits[0], [taskNew       (0, 0),  taskNew    (0, 1),    taskNew    (0, 2)]), // kBatchSize = 6
+        (commits[1], [taskNew       (1, 0),  taskNew    (1, 1),    taskNew    (1, 2)]), // < 1
+        (commits[1], [taskNew       (2, 0),  taskNew    (2, 1),    taskNew    (2, 2)]), // < 2
+        (commits[1], [taskNew       (3, 0),  taskNew    (3, 1),    taskNew    (3, 2)]), // < 4
+        (commits[1], [taskNew       (4, 0),  taskNew    (4, 1),    taskNew    (4, 2)]), // < 4
+        (commits[1], [taskNew       (5, 0),  taskNew    (5, 1),    taskFailed (5, 2)]), // < 5
+        (commits[1], [taskNew       (6, 0),  taskFailed (6, 1),    taskNew    (6, 2)]), // < 6
+        (commits[1], [taskFailed    (7, 0),  taskNew    (7, 1),    taskNew    (7, 2)]), // < 7
+      ], tipOfTreeTargets: [
+        targets[0],
+        targets[1],
+        targets[2],
+      ]);
+      // dart format on
+
+      expect(strategy.determineBackfill(grid), [
+        isBackfillTask.hasCommit(commits[0]).hasTarget(targets[1]), // 1️⃣
+        isBackfillTask.hasCommit(commits[0]).hasTarget(targets[2]), // 2️⃣
+        isBackfillTask.hasCommit(commits[0]).hasTarget(targets[0]), // 3️⃣
+      ]);
+    });
+  });
+}
