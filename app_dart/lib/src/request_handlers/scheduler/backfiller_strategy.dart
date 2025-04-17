e5348d67f7541524c709797ea349bfbd315738b4
Matan Lurey <matanlurey@users.noreply.github.com>
2025-04-17 09:27:03 -0700

    Rewrite (no prod impact) `BatchBackfiller` logic as `DefaultBackfillerStrategy` (#4529)
    This is incremental work towards:
    
    - Getting `BatchBackfiller` to use Firestore-first 
      (https://github.com/flutter/flutter/issues/167222)
    - Eventually improving our backfilling algorithm 
      (https://github.com/flutter/flutter/issues/166106)
    
    If you like this approach, a future PR will add tests and change `batch_backfiller` to use it.

---
+++ app_dart/lib/src/request_handlers/scheduler/backfiller_strategy.dart
@@ -0,0 +1,126 @@
+// Copyright 2019 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:math';
+
+import 'package:meta/meta.dart';
+
+import '../../model/ci_yaml/target.dart';
+import '../../model/firestore/task.dart' as fs;
+import '../../service/luci_build_service.dart';
+import '../../service/luci_build_service/opaque_commit.dart';
+import '../../service/scheduler/policy.dart';
+
+/// Defines an interface for determining what tasks to backfill in which order.
+///
+/// _Backfilling_ is the act of incrementally, and in batch via a cron job,
+/// (slowly) scheduling tasks marked [fs.Task.statusNew] (waiting for backfill),
+/// potentially skipping certain tasks.
+///
+/// For example, in the following dashboard "grid":
+/// ```txt
+/// 🧑‍💼 🟩 🟩 🟩 🟩 🟩 ⬜ ⬜ ⬜ ⬜
+/// 🧑‍💼 ⬜ ⬜ ⬜ 🟨 🟨 🟥 🟩 🟩 🟩
+/// ```
+///
+/// A backfilling strategy would decide which `⬜` boxes to schedule (turning
+/// them to `🟨` ),
+@immutable
+abstract interface class BackfillerStrategy {
+  /// Given a grid of (\~50) commits to (\~20) tasks, returns tasks to backfill.
+  ///
+  /// Each commit reflects something like the following:
+  /// ```txt
+  /// 🧑‍💼 ⬜ ⬜ ⬜ 🟨 🟨 🟥 🟩 🟩 🟩
+  /// ```
+  ///
+  /// The returned list of tasks are `⬜` tasks that should be prioritized, in
+  /// order of most important to least important. That is, implementations may
+  /// only (due to capacity) backfill the top `N` tasks returned by this method.
+  List<(OpaqueTask, int)> determineBackfill(
+    List<Target> targets,
+    List<(OpaqueCommit, List<OpaqueTask>)> recent,
+  );
+}
+
+/// The "original" backfiller strategy ported from `BatchBackfiller`.
+///
+/// It prioritizes targets with the following rules:
+/// - [LuciBuildService.kRerunPriority] prioritizes tasks that recently[^1] failed;
+/// - [LuciBuildService.kRerunPriority] > other priorities;
+/// - Both high and low priority targets are shuffled (using [List.shuffle])
+///
+/// [^1]: As determined by [BatchPolicy.kBatchSize] tasks after a given task
+final class DefaultBackfillerStrategy implements BackfillerStrategy {
+  /// Creates a default backfiller strategy.
+  ///
+  /// If [Random] is provided, it used for [List.shuffle] determinism.
+  const DefaultBackfillerStrategy(@visibleForTesting this._random);
+  final Random? _random;
+
+  @override
+  List<(OpaqueTask, int)> determineBackfill(
+    List<Target> targets,
+    List<(OpaqueCommit, List<OpaqueTask>)> recent,
+  ) {
+    // First, index the tasks by "column": Map<{Name}, List<OpaqueTask>.
+    final tasksByName = <String, List<OpaqueTask>>{};
+    for (final (_, tasks) in recent) {
+      for (final task in tasks) {
+        (tasksByName[task.name] ??= []).add(task);
+      }
+    }
+
+    // Next index the targets as well.
+    final targetsByName = {for (final target in targets) target.name: target};
+
+    // Next, remove entire columns where any of the following is true.
+    tasksByName.removeWhere((_, tasks) {
+      // 1. The target is not supposed to be run by the backfilling process.
+      if (targetsByName[tasks.first.name]?.schedulerPolicy is! BatchPolicy) {
+        return true;
+      }
+
+      // 2. At least one task in progress;
+      if (!tasks.any((t) => t.status == fs.Task.statusInProgress)) {
+        return true;
+      }
+
+      // Retain.
+      return false;
+    });
+
+    // Now, create two lists: high priority (recent failure) and regular.
+    final hadRecentFailure = <OpaqueTask>[];
+    final noRecentFailures = <OpaqueTask>[];
+
+    for (final column in tasksByName.values) {
+      for (var i = 0; i < column.length; i++) {
+        final row = column[i];
+        if (row.status != fs.Task.statusNew) {
+          continue;
+        }
+
+        // Determine whether it is high priority or regular priority.
+        if (column
+            .getRange(i, min(i + BatchPolicy.kBatchSize, column.length))
+            .any((t) => fs.Task.taskFailStatusSet.contains(t.status))) {
+          hadRecentFailure.add(row);
+        } else {
+          noRecentFailures.add(row);
+        }
+      }
+    }
+
+    // Shuffle both sublists.
+    hadRecentFailure.shuffle(_random);
+    noRecentFailures.shuffle(_random);
+
+    // Return both of these lists, concatted.
+    return [
+      ...hadRecentFailure.map((t) => (t, LuciBuildService.kRerunPriority)),
+      ...noRecentFailures.map((t) => (t, LuciBuildService.kBackfillPriority)),
+    ];
+  }
+}
--- app_dart/lib/src/service/luci_build_service/opaque_commit.dart
+++ app_dart/lib/src/service/luci_build_service/opaque_commit.dart
@@ -6,7 +6,9 @@ import 'package:github/github.dart';
 import 'package:meta/meta.dart';
 
 import '../../model/appengine/commit.dart' as ds;
+import '../../model/appengine/task.dart' as ds;
 import '../../model/firestore/commit.dart' as fs;
+import '../../model/firestore/task.dart' as fs;
 
 /// Represents components of a GitHub commit without specifying the backend.
 ///
@@ -52,4 +54,50 @@ final class _DatastoreCommit implements OpaqueCommit {
 
   @override
   RepositorySlug get slug => _commit.slug;
+}
+
+/// Represents components of a backend task without specifying the backend.
+///
+/// This is used transitory to migrate from Datastore -> Firestore.
+@immutable
+abstract final class OpaqueTask {
+  factory OpaqueTask.fromFirestore(fs.Task task) = _FirestoreTask;
+  factory OpaqueTask.fromDatastore(ds.Task task) = _DatastoreTask;
+
+  /// Name of the task.
+  String get name;
+
+  /// Status of the task.
+  String get status;
+
+  /// Commit the task belongs to.
+  String get commitSha;
+}
+
+final class _FirestoreTask implements OpaqueTask {
+  const _FirestoreTask(this._task);
+  final fs.Task _task;
+
+  @override
+  String get name => _task.taskName;
+
+  @override
+  String get status => _task.status;
+
+  @override
+  String get commitSha => _task.commitSha;
+}
+
+final class _DatastoreTask implements OpaqueTask {
+  const _DatastoreTask(this._task);
+  final ds.Task _task;
+
+  @override
+  String get name => _task.builderName!;
+
+  @override
+  String get status => _task.status;
+
+  @override
+  String get commitSha => _task.commitKey!.id!.split('/').last;
 }
