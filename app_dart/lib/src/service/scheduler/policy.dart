f49c4b03fe33edcf9f77f2dc2705e96475a5be97
Matan Lurey <matanlurey@users.noreply.github.com>
2025-04-18 16:27:23 -0700

    Fix `policy.triggerPriority` (#4551)
    Oops!
    
    This is a mistype that queries by the full document name instead of the name of the task!

--- app_dart/lib/src/service/scheduler/policy.dart
+++ app_dart/lib/src/service/scheduler/policy.dart
@@ -75,7 +75,7 @@ final class BatchPolicy implements SchedulerPolicy {
       (Task t) => t.taskName == taskName && t.commitSha == commitSha,
     );
     if (recentTasks.length < kBatchSize) {
-      log.warn(
+      log.debug(
         '$taskName has less than $kBatchSize, skip scheduling to wait for '
         'ci.yaml roll.',
       );
