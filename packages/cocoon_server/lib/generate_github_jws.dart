eaed233558c0a0f586aa2a65d7920675a60dd1da
John McDole <codefu@google.com>
2025-04-28 11:00:06 -0700

    Consolidate to one library for JWT / JWS
    - Moves nearly identical Json Web Signature code to shared folder
    - Adds tests
    - Behind the scenes: udpate secrets manager with the correctly formated
      PEM.

---
+++ packages/cocoon_server/lib/generate_github_jws.dart
@@ -0,0 +1,33 @@
+import 'package:jose_plus/jose.dart';
+import 'package:meta/meta.dart';
+
+/// Generates a Json Web Signature token to prove to GitHub we are [githubAppId].
+String generateGitHubJws({
+  required String privateKeyPem,
+  required String githubAppId,
+  @visibleForTesting DateTime? now,
+}) {
+  // Load the signing key
+  final signingKey = JsonWebKey.fromPem(privateKeyPem);
+
+  // Calculate times used for claims.
+  now ??= DateTime.now();
+  final iat =
+      now.millisecondsSinceEpoch ~/
+      1000; // Issued at time (seconds since epoch)
+  final exp =
+      now.add(const Duration(minutes: 10)).millisecondsSinceEpoch ~/
+      1000; // Expiration time (seconds since epoch)
+
+  // Get the builder and add claims
+  final builder = JsonWebSignatureBuilder();
+  builder.jsonContent = JsonWebTokenClaims.fromJson({
+    'iss': githubAppId,
+    'iat': iat,
+    'exp': exp,
+  });
+
+  // Sign it
+  builder.addRecipient(signingKey);
+  return builder.build().toCompactSerialization();
+}
