7f15a76bfffdb82c596131df13dd2f7a86a7d075
Matan Lurey <matanl@google.com>
2025-05-20 10:59:52 -0700

    Opt out bin/internal/engine.version from most presubmits.

--- app_dart/test/service/scheduler/files_changed_optimization_test.dart
+++ app_dart/test/service/scheduler/files_changed_optimization_test.dart
@@ -152,6 +152,26 @@ void main() {
     );
   });
 
+  test('include bin/internal/engine.version as ignored', () async {
+    final optimizer = FilesChangedOptimizer(
+      getFilesChanged: filesChanged([
+        'README.md',
+        'CONTRIBUTING.md',
+        'bin/internal/engine.version',
+        'packages/flutter_tools/lib/src/engine/NOT_THE_ENGINE.md',
+      ]),
+      ciYamlFetcher: ciYamlFetcher(slug: Config.flutterSlug),
+      config: config(maxFilesChangedForSkippingEnginePhase: 100),
+    );
+
+    await expectLater(
+      optimizer.checkPullRequest(
+        generatePullRequest(repo: 'flutter', changedFilesCount: 4, number: 123),
+      ),
+      completion(FilesChangedOptimization.skipPresubmitAllExceptFlutterAnalyze),
+    );
+  });
+
   test('only non-engine files', () async {
     final optimizer = FilesChangedOptimizer(
       getFilesChanged: filesChanged(['packages/flutter/lib/flutter.dart']),
