e3b089eacc738e60fdc0b27a41876f1d1cb401b4
John McDole <codefu@google.com>
2025-12-17 12:27:24 -0800

    Add config for new feature: test suppression

--- app_dart/lib/src/service/flags/dynamic_config.g.dart
+++ app_dart/lib/src/service/flags/dynamic_config.g.dart
@@ -25,6 +25,7 @@ DynamicConfig _$DynamicConfigFromJson(Map<String, dynamic> json) =>
           : ConsolidatedCheckRunFlow.fromJson(
               json['consolidatedCheckRunFlow'] as Map<String, dynamic>?,
             ),
+      treeStatusSuppression: json['treeStatusSuppression'] as bool?,
     );
 
 Map<String, dynamic> _$DynamicConfigToJson(DynamicConfig instance) =>
@@ -34,4 +35,5 @@ Map<String, dynamic> _$DynamicConfigToJson(DynamicConfig instance) =>
       'ciYaml': instance.ciYaml.toJson(),
       'closeMqGuardAfterPresubmit': instance.closeMqGuardAfterPresubmit,
       'consolidatedCheckRunFlow': instance.consolidatedCheckRunFlow.toJson(),
+      'treeStatusSuppression': instance.treeStatusSuppression,
     };
