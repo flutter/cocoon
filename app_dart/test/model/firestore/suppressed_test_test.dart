5348e89976fd26c251c0664f5411ca1d9164106b
John McDole <codefu@google.com>
2025-12-17 17:11:41 -0800

    Fix errors

--- app_dart/test/model/firestore/suppressed_test_test.dart
+++ app_dart/test/model/firestore/suppressed_test_test.dart
@@ -2,12 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:cocoon_server_test/test_logging.dart';
 import 'package:cocoon_service/cocoon_service.dart';
 import 'package:cocoon_service/src/model/firestore/suppressed_test.dart';
 import 'package:googleapis/firestore/v1.dart';
 import 'package:test/test.dart';
 
+
 void main() {
+  useTestLoggerPerTest();
+
   group('SuppressedTest', () {
     test('creates from document correctly', () {
       final doc = Document(
