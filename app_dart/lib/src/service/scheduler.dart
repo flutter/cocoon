e2fb142d1882557391e2f936163b928d569dd4e5
Dmitry Grand <dmgr@google.com>
2026-06-23 16:29:19 -0700

    refactor: extract pull request sha variable and add logging when existing try builds are detected

--- app_dart/lib/src/service/scheduler.dart
+++ app_dart/lib/src/service/scheduler.dart
@@ -340,10 +340,12 @@ class Scheduler {
     // after presubmits have started, check if builds already exist for the
     // current SHA. If builds already exist, there is no need to cancel them
     // and create new ones.
-    final tryBuilds = await _luciBuildService.getTryBuildsBySha(
-      sha: pullRequest.head!.sha!,
-    );
+    final sha = pullRequest.head!.sha!;
+    final tryBuilds = await _luciBuildService.getTryBuildsBySha(sha: sha);
     if (tryBuilds.isNotEmpty) {
+      log.info(
+        'CICD label was already applied to pr:${pullRequest.number} for sha:$sha and ${tryBuilds.length} jobs have been already scheduled, skipping.',
+      );
       return;
     }
 
@@ -364,11 +366,11 @@ class Scheduler {
     // the PR and to the merge group, and so it must be completed in both cases.
     final lock = await lockMergeGroupChecks(
       slug,
-      pullRequest.head!.sha!,
+      sha,
       // Override details url of merge queue guard check for users with unified
       // check run flow enabled
       detailsUrl: isUnifiedCheckRun
-          ? 'https://flutter-dashboard.appspot.com/#/presubmit?repo=${slug.name}&sha=${pullRequest.head!.sha}'
+          ? 'https://flutter-dashboard.appspot.com/#/presubmit?repo=${slug.name}&sha=$sha'
           : null,
       isUnifiedCheckRun: isUnifiedCheckRun,
     );
@@ -385,7 +387,6 @@ class Scheduler {
     final isPackages = slug == Config.packagesSlug;
     do {
       try {
-        final sha = pullRequest.head!.sha!;
         if (!isFusion && !(isPackages && isUnifiedCheckRun)) {
           unlockMergeGroup = true;
         }
@@ -532,10 +533,10 @@ class Scheduler {
     // there are situations (see code above) when it needs to be unlocked
     // immediately.
     if (unlockMergeGroup) {
-      await unlockMergeQueueGuard(slug, pullRequest.head!.sha!, lock);
+      await unlockMergeQueueGuard(slug, sha, lock);
     }
     log.info(
-      'Finished triggering builds for: pr ${pullRequest.number}, commit ${pullRequest.base!.sha}, branch ${pullRequest.head!.ref} and slug $slug}',
+      'Finished triggering builds for: pr ${pullRequest.number}, commit $sha, branch ${pullRequest.head!.ref} and slug $slug}',
     );
   }
 
