f9377f5fd1cfed33b36c0244746fb21fcb7dccd3
Dmitry Grand (dmgr) <dmgr@google.com>
2026-09-04 17:28:05 -0700

    fix license header

--- app_dart/test/request_handlers/presubmit_luci_subscription_test.dart
+++ app_dart/test/request_handlers/presubmit_luci_subscription_test.dart
@@ -1,8 +1,8 @@
 // Copyright 2019 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import 'package:archive/archive.dart';
 
+import 'package:archive/archive.dart';
 import 'package:buildbucket/buildbucket_pb.dart' as bbv2;
 import 'package:cocoon_common/task_status.dart';
 import 'package:cocoon_integration_test/testing.dart';
