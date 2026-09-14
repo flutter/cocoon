224c590a83d98b024fb1e9b3a7bc262b3f95a6bc
Dmitry Grand (dmgr) <dmgr@google.com>
2026-09-14 14:41:31 -0700

    Added `checkSuiteFlow` flag. Removed `unifiedCheckRunFlow` flag as no longer needed.

---
+++ app_dart/lib/src/service/flags/check_suite_flow.dart
@@ -0,0 +1,52 @@
+// Copyright 2026 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta/meta.dart';
+
+part 'check_suite_flow.g.dart';
+
+/// Flag that controls whether to use checks suite flow.
+@JsonSerializable()
+@immutable
+final class CheckSuiteFlow {
+  /// Default configuration for [CheckSuiteFlow].
+  static const defaultInstance = CheckSuiteFlow._(
+    useForAll: false,
+    useForUsers: [],
+  );
+
+  /// Whether to use checks suite flow for all users.
+  @JsonKey()
+  final bool useForAll;
+
+  /// List of users to use checks suite flow.
+  @JsonKey()
+  final List<String> useForUsers;
+
+  const CheckSuiteFlow._({
+    required this.useForAll, //
+    required this.useForUsers, //
+  });
+
+  /// Creates [CheckSuiteFlow] flags from the provided fields.
+  ///
+  /// Any omitted fields default to the values in [defaultInstance].
+  factory CheckSuiteFlow({bool? useForAll, List<String>? useForUsers}) {
+    return CheckSuiteFlow._(
+      useForAll: useForAll ?? defaultInstance.useForAll,
+      useForUsers: useForUsers ?? defaultInstance.useForUsers,
+    );
+  }
+
+  /// Creates [CheckSuiteFlow] flags from a [json] object.
+  ///
+  /// Any omitted fields default to the values in [defaultInstance].
+  factory CheckSuiteFlow.fromJson(Map<String, Object?>? json) {
+    return _$CheckSuiteFlowFromJson(json ?? {});
+  }
+
+  /// The inverse operation of [CheckSuiteFlow.fromJson].
+  Map<String, Object?> toJson() => _$CheckSuiteFlowToJson(this);
+}
--- app_dart/lib/src/service/flags/check_suite_flow.g.dart
+++ app_dart/lib/src/service/flags/check_suite_flow.g.dart
@@ -2,23 +2,22 @@
 
 // ignore_for_file: always_specify_types, implicit_dynamic_parameter
 
-part of 'unified_check_run_flow_flags.dart';
+part of 'check_suite_flow.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-UnifiedCheckRunFlow _$UnifiedCheckRunFlowFromJson(Map<String, dynamic> json) =>
-    UnifiedCheckRunFlow(
+CheckSuiteFlow _$CheckSuiteFlowFromJson(Map<String, dynamic> json) =>
+    CheckSuiteFlow(
       useForAll: json['useForAll'] as bool?,
       useForUsers: (json['useForUsers'] as List<dynamic>?)
           ?.map((e) => e as String)
           .toList(),
     );
 
-Map<String, dynamic> _$UnifiedCheckRunFlowToJson(
-  UnifiedCheckRunFlow instance,
-) => <String, dynamic>{
-  'useForAll': instance.useForAll,
-  'useForUsers': instance.useForUsers,
-};
+Map<String, dynamic> _$CheckSuiteFlowToJson(CheckSuiteFlow instance) =>
+    <String, dynamic>{
+      'useForAll': instance.useForAll,
+      'useForUsers': instance.useForUsers,
+    };
